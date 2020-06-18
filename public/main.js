0a1cfee89f394c451b4b3c0bbc0556ac6f93bd3d
Luis Olmedo <olmedoluis012@gmail.com>
2020-06-17 21:23:40 -0300

    refactor(main.js):Add scripts elements by javascript

--- public/main.js
+++ public/main.js
@@ -1,1 +1,15 @@
-console.log("hola");
+function Script(props) {
+  const scriptContainer = document.createElement("div");
+  scriptContainer.classList = ["script-option"];
+
+  const scriptButton = document.createElement("button");
+  const text = document.createTextNode("script test");
+  scriptButton.appendChild(text);
+
+  scriptContainer.appendChild(scriptButton);
+
+  return scriptContainer;
+}
+
+const root = document.getElementById("scripts");
+root.appendChild(Script());

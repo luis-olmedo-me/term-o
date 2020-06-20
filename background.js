16d09a0c7dcefd2805632a57cf99227854aa97d9
Luis Olmedo <olmedoluis012@gmail.com>
2020-06-19 23:13:01 -0300

    refactor(background.js):Comment storage setter

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 // chrome.tabs.create({ url: "./public/main.html" });
-const value = "console.log('funco?')";
-chrome.storage.local.set({ myKey: value }, function () {
-  console.log("Value is set to" + value);
-});
+// const value = "console.log('funco?')";
+// chrome.storage.local.set({ scriptsBagKey: value }, function () {
+//   console.log("Value is set to" + value);
+// });

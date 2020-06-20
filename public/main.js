fa4d4f6eef5d385993d17d804a9e755f713ce888
Luis Olmedo <olmedoluis012@gmail.com>
2020-06-20 19:07:30 -0300

    refactor(main.js):Use components script

--- public/main.js
+++ public/main.js
@@ -1,55 +1,8 @@
-function SubOptions({ options }) {
-  const scriptContainer = document.createElement("div");
-  scriptContainer.classList = ["script-sub-options closed"];
-
-  options.forEach(({ text, callback }) => {
-    const scriptButton = document.createElement("button");
-    scriptButton.onclick = callback;
-
-    const textNode = document.createTextNode(text);
-    scriptButton.appendChild(textNode);
-
-    scriptContainer.appendChild(scriptButton);
-  });
-
-  return scriptContainer;
-}
-
-function Script({ text = "script", callback, options }) {
-  const hasOptions = Boolean(options);
-
-  const scriptContainer = document.createElement("div");
-  scriptContainer.classList = ["script-option"];
-
-  const scriptButton = document.createElement("button");
-  scriptButton.onclick = callback;
-
-  const textNode = document.createTextNode(text);
-  scriptButton.appendChild(textNode);
-
-  scriptContainer.appendChild(scriptButton);
-
-  if (hasOptions) {
-    const subOptions = SubOptions({ options });
-
-    scriptButton.onclick = () => {
-      const isClosed = subOptions.className.includes("closed");
-
-      subOptions.className = `script-sub-options ${isClosed ? "" : "closed"}`;
-
-      callback && callback();
-    };
-
-    scriptContainer.appendChild(subOptions);
-  }
-
-  return scriptContainer;
-}
-
 const codeCoder = document.getElementById("code-coder");
 const codeName = document.getElementById("code-name");
 const scripts = document.getElementById("scripts");
 let currentScripts = [];
+const { Script } = Components;
 
 function saveScripts(scripts) {
   chrome.storage.local.set({ scriptsBagKey: JSON.stringify(scripts) });

110201701c2f2db893be8150267bbf48a459e80f
Luis Olmedo <olmedoluis012@gmail.com>
2020-06-27 02:46:59 -0300

    refactor(content.js):Wrap function execution in try catch

--- content.js
+++ content.js
@@ -1,5 +1,11 @@
 chrome.runtime.onMessage.addListener(function (message, callback) {
   if (message.message == "EXECUTE_SCRIPT_BAG") {
-    eval(message.customCode);
+    try {
+      const script = new Function(message.customCode);
+
+      script();
+    } catch (error) {
+      console.error("Got an error executing..");
+    }
   }
 });

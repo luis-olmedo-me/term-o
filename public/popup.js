88439707f22a04ea30cf8cd6918e919a351b0316
Luis Olmedo <olmedoluis012@gmail.com>
2020-06-27 03:18:32 -0300

    refactor(popup.js):Recieve error from content script

--- public/popup.js
+++ public/popup.js
@@ -30,3 +30,9 @@ chrome.storage.local.get(["scriptsBagKey"], function ({ scriptsBagKey }) {
     });
   });
 });
+
+chrome.runtime.onMessage.addListener(function ({ type, error }) {
+  if (type == "ERROR_SCRIPT_BAG") {
+    console.log("chan chan", error);
+  }
+});

b1533ba9c6633fea5629eb31002ab88b942d6448
Luis Olmedo <olmedoluis012@gmail.com>
2020-06-27 03:18:02 -0300

    feat(content.js):send error name to popup

--- content.js
+++ content.js
@@ -1,11 +1,16 @@
-chrome.runtime.onMessage.addListener(function (message, callback) {
+chrome.runtime.onMessage.addListener(function (message, sender) {
   if (message.message == "EXECUTE_SCRIPT_BAG") {
     try {
       const script = new Function(message.customCode);
 
       script();
     } catch (error) {
-      console.error("Got an error executing..");
+      console.log(error);
+      // sender({ message: "ERROR_SCRIPT_BAG", error });
+      chrome.runtime.sendMessage({
+        type: "ERROR_SCRIPT_BAG",
+        error: error.name,
+      });
     }
   }
 });

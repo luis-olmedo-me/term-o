7cb07feadb582cd1573e975c8005189915d2ab5d
Luis Olmedo <olmedoluis012@gmail.com>
2020-08-29 16:43:50 -0300

    refactor(content.ui.js):Properly follow scroll

--- content.ui.js
+++ content.ui.js
@@ -5,7 +5,18 @@
 
   const page = document.getElementsByTagName("body")[0];
 
-  // const webBotsContents = document.createElement("div");
+  const webBotsContents = document.createElement("div");
+  const webBotsContentsStyle = webBotsContents.style;
+
+  webBotsContents.id = "web-bots-contents";
+  webBotsContentsStyle.width = "100vw";
+  webBotsContentsStyle.height = "100vh";
+  webBotsContentsStyle.position = "absolute";
+  webBotsContentsStyle.zIndex = 10000000;
+  webBotsContentsStyle.top = 0;
+  webBotsContentsStyle.pointerEvents = "none";
+
+  page.insertBefore(webBotsContents, page.firstChild);
 
   const createSnackbar = ({ newTitle, newMessage, theme }) => {
     const snackBarWrapper = document.createElement("div");
@@ -13,11 +24,11 @@
 
     snackBarWrapperStyle.color = "#fff";
     snackBarWrapperStyle.backgroundColor = theme;
-    snackBarWrapperStyle.position = "sticky";
+    snackBarWrapperStyle.position = "absolute";
     snackBarWrapperStyle.width = "max-content";
     snackBarWrapperStyle.padding = "10px 50px 10px 20px";
     snackBarWrapperStyle.borderRadius = "0 5px 5px 0";
-    snackBarWrapperStyle.marginTop = "50px";
+    snackBarWrapperStyle.top = "50px";
     snackBarWrapperStyle.boxShadow = "0px 0px 8px 1px #d6d6d6";
 
     const titleText = document.createTextNode(newTitle);
@@ -47,6 +58,8 @@
     error: "#ff212c",
   };
 
+  const $webBots = $("#web-bots-contents");
+
   global.contentSnackBarAPI = {
     setMessage: (title, message, theme) => {
       const snackbar = createSnackbar({
@@ -55,7 +68,11 @@
         newMessage: message,
       });
 
-      page.appendChild(snackbar);
+      webBotsContents.appendChild(snackbar);
+
+      window.addEventListener("scroll", (event) => {
+        $webBots.css("top", scrollY);
+      });
 
       setTimeout(() => snackbar.remove(), 5000);
     },

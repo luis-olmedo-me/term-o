8f823790c361f53841eea3f950d7674934aa6fe5
Luis Olmedo <olmedoluis012@gmail.com>
2020-11-14 19:44:52 -0300

    refactor(background.js):Remove unnessary comments

--- background.js
+++ background.js
@@ -1,13 +1,3 @@
-// chrome.tabs.create({ url: "./public/main.html" });
-
-// const value = [
-//   { name: "My great script", script: "console.log('hola')" },
-//   { name: "My best script", script: "console.log('carola')" },
-// ];
-// chrome.storage.local.set({ scriptsBagKey: JSON.stringify(value) }, function () {
-//   console.log("Value is set to" + value);
-// });
-
 class Broker {
   constructor() {
     this.keysListening = [];

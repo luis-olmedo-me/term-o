efa4060ac4251f03923f742aa37aaa76813d98bc
Luis Olmedo <olmedoluis012@gmail.com>
2020-12-12 12:35:16 -0300

    refactor(content.js):Expect for message event to execute scripts

--- rebuild/src/extension/content/content.js
+++ rebuild/src/extension/content/content.js
@@ -1,1 +1,22 @@
-console.log("Hello world from content!");
+chrome.runtime.onMessage.addListener(function ({ message, customCode, query }) {
+  if (message == "EXECUTE_SCRIPT_BAG") {
+    const options = JSON.parse(query);
+    const environment = Object.keys(options).reduce((environment, name) => {
+      const env = options[name];
+
+      return { ...environment, [name]: env.value || env.defaultValue };
+    }, {});
+
+    const scope = { window, contentSnackBarAPI, environment };
+
+    try {
+      (function webBot(code) {
+        eval(code);
+      }.call(scope, customCode));
+    } catch ({ stack, message }) {
+      const stackFirstPart = stack.split("\n")[0];
+
+      contentSnackBarAPI.setMessage(stackFirstPart, message, "error");
+    }
+  }
+});

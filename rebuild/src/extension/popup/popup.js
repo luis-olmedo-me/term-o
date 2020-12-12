bdbb3290c41bf9e1cae55557a67f3eb237e5dd6d
Luis Olmedo <olmedoluis012@gmail.com>
2020-12-12 02:19:20 -0300

    refactor(popup.js):Properly load template for popup

--- rebuild/src/extension/popup/popup.js
+++ rebuild/src/extension/popup/popup.js
@@ -1,1 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 console.log("Hello world from popup!");
+
+const ExtensionApp = () => {
+  console.log("Hello World from popup.js inside the ExtensionApp!");
+
+  return <p>Hi</p>;
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ExtensionApp />
+  </React.StrictMode>,
+  document.getElementById("root")
+);

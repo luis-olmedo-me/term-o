43eec77c5d8b249aa482805b23947978c4281350
Luis Olmedo <olmedoluis012@gmail.com>
2020-12-12 21:55:39 -0300

    fix(webpack.config.js):Update renamed enty points directories

--- rebuild/webpack.config.js
+++ rebuild/webpack.config.js
@@ -3,9 +3,9 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: {
-    background: "./src/extension/background/background.js",
-    content: "./src/extension/content/content.js",
-    popup: "./src/extension/popup/popup.js",
+    background: "./src/projects/background/background.js",
+    content: "./src/projects/content/content.js",
+    popup: "./src/projects/popup/popup.js",
   },
   output: {
     path: path.resolve("extension"),

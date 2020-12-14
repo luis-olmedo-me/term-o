35710067563f3a25a06f9c7fe9c6a5f01b1ea6ea
Luis Olmedo <olmedoluis012@gmail.com>
2020-12-14 19:56:29 -0300

    refactor(Content.component.js):Follow user scroll

--- rebuild/src/projects/content/components/Content/Content.component.js
+++ rebuild/src/projects/content/components/Content/Content.component.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { EASY_DOM_CONTENT_ID } from "../../content.constants";
 
 import styles from "./Content.styles.scss";
 
 export const Content = () => {
+  const [topPosition, setTopPosition] = useState(0);
+
+  useEffect(function updateTop() {
+    let animationEventId = null;
+
+    const updatePosition = () => {
+      setTopPosition(window.scrollY);
+
+      animationEventId = window.requestAnimationFrame(updatePosition);
+    };
+
+    animationEventId = window.requestAnimationFrame(updatePosition);
+
+    return () => window.cancelAnimationFrame(animationEventId);
+  }, []);
+
   return (
-    <div id={EASY_DOM_CONTENT_ID} className={`${styles.content_wrapper}`}></div>
+    <div
+      id={EASY_DOM_CONTENT_ID}
+      className={styles.content_wrapper}
+      style={{ top: topPosition }}
+    >
+      content
+    </div>
   );
 };

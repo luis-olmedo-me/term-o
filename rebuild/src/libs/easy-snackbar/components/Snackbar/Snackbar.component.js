1a73dedaa9c7d0dbb1d6dc99f3f0ac1ac81256e7
Luis Olmedo <olmedoluis012@gmail.com>
2020-12-17 22:34:12 -0300

    refactor(Snackbar.component.js):Show data from custom hook

--- rebuild/src/libs/easy-snackbar/components/Snackbar/Snackbar.component.js
+++ rebuild/src/libs/easy-snackbar/components/Snackbar/Snackbar.component.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { useEasySnackbar } from "../../hooks/useEasySnackbar.hook";
 
 export const Snackbar = () => {
-  return <div>Snackbar</div>;
+  const [theme, title, message] = useEasySnackbar();
+  console.log("update", [theme, title, message]);
+
+  return (
+    <div>
+      <h3>{title}</h3>
+      <p>{message}</p>
+      <p>{theme}</p>
+    </div>
+  );
 };

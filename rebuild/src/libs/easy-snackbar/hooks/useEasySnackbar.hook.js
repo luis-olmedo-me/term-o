9766ae24cbe1e19a0f10946d107e310d6afafe52
Luis Olmedo <olmedoluis012@gmail.com>
2020-12-17 22:33:47 -0300

    fix(useEasySnackbar.hook.js):Properly set up event for hook

--- rebuild/src/libs/easy-snackbar/hooks/useEasySnackbar.hook.js
+++ rebuild/src/libs/easy-snackbar/hooks/useEasySnackbar.hook.js
@@ -1,18 +1,16 @@
-import { useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { SNACKBAR_UPDATE } from "../constants/eventsKeys.constants";
 
 export const useEasySnackbar = () => {
-  const [values, setValues] = useState({});
+  const [values, setValues] = useState(["", "", ""]);
 
-  useEffect(function updateWhenEventIsTriggered() {
-    const updateValues = ({ details: { theme, title, message } }) => {
-      setValues({ theme, title, message });
-    };
+  const updateValues = useCallback(({ detail: { theme, title, message } }) => {
+    setValues([theme, title, message]);
 
-    addEventListener(SNACKBAR_UPDATE, updateValues);
-
-    return removeEventListener(SNACKBAR_UPDATE, updateValues);
+    window.removeEventListener(SNACKBAR_UPDATE, updateValues);
   }, []);
+
+  window.addEventListener(SNACKBAR_UPDATE, updateValues);
 
   return values;
 };

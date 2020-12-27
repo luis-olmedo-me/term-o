f2aa5171d741363d2a401ff22f4badfd85198613
Luis Olmedo <olmedoluis012@gmail.com>
2020-12-27 17:04:33 -0300

    refactor(Snackbar.component.js):Show many messages

--- rebuild/src/libs/easy-snackbar/components/Snackbar/Snackbar.component.js
+++ rebuild/src/libs/easy-snackbar/components/Snackbar/Snackbar.component.js
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useEasySnackbar } from "../../hooks/useEasySnackbar.hook";
 
-export const Snackbar = () => {
-  const [theme, title, message] = useEasySnackbar();
-  console.log("update", [theme, title, message]);
+import styles from "./Snackbar.styles.scss";
 
-  return (
-    <div>
-      <h3>{title}</h3>
-      <p>{message}</p>
-      <p>{theme}</p>
-    </div>
+export const Snackbar = () => {
+  const [messages, setMessages] = useState([]);
+  const snackbarMessage = useEasySnackbar();
+
+  useEffect(
+    function addMessageToList() {
+      setMessages((oldSnackbarMessages) =>
+        snackbarMessage.length
+          ? [...oldSnackbarMessages, snackbarMessage]
+          : oldSnackbarMessages
+      );
+    },
+    [snackbarMessage]
   );
+
+  console.log("messages", messages);
+
+  return messages.map((messageFromHook, index) => {
+    const [theme, title, message] = messageFromHook;
+    const styleTheme = styles[theme] || styles.default;
+
+    const removeMessageFromList = () => {
+      setMessages(
+        messages.filter((messageCycled) => messageCycled !== messageFromHook)
+      );
+    };
+
+    const key = title.replace(" ", "_").toLowerCase();
+
+    return (
+      <div
+        key={`${key}_${index}`}
+        className={`${styles.snackbar__wrapper} ${styleTheme}`}
+        onClick={removeMessageFromList}
+      >
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.message}>{message}</p>
+      </div>
+    );
+  });
 };

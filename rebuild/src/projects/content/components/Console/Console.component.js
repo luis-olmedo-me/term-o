ae0fa10ee4b99007b88018161fa0d48fb1837fc7
Luis Olmedo <olmedoluis012@gmail.com>
2021-02-16 15:28:38 -0300

    feat(Console.component.js):Apply styles for console wrapper

--- rebuild/src/projects/content/components/Console/Console.component.js
+++ rebuild/src/projects/content/components/Console/Console.component.js
@@ -2,5 +2,11 @@ import React from "react";
 import styles from "./Console.styles.scss";
 
 export const Console = ({ isOpen }) => {
-  return isOpen && <div>Console</div>;
+  return (
+    isOpen && (
+      <div className={styles.console_wrapper}>
+        <div className={styles.console}></div>
+      </div>
+    )
+  );
 };

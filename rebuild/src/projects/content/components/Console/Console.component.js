d5e0d849a68fa49a5bef9a40ae386fd2a7adcd4d
Luis Olmedo <olmedoluis012@gmail.com>
2021-02-16 15:55:03 -0300

    feat(Console):Apply styles for button

--- rebuild/src/projects/content/components/Console/Console.component.js
+++ rebuild/src/projects/content/components/Console/Console.component.js
@@ -5,7 +5,13 @@ export const Console = ({ isOpen }) => {
   return (
     isOpen && (
       <div className={styles.console_wrapper}>
-        <div className={styles.console}></div>
+        <div className={styles.console}>
+          <div className={styles.console_command_input_wrapper}>
+            <input className={styles.console_command_input} type="text" placeholder="Write your commands here!"/>
+
+            <button className={styles.console_command_button}>Save</button>
+          </div>
+        </div>
       </div>
     )
   );

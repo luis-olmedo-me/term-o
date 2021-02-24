fafd6a688a58ba51dd30673bf53338fd141ed34d
Luis Olmedo <olmedoluis012@gmail.com>
2021-02-23 21:45:39 -0300

    feat(css.command.js):Create css command

---
+++ src/projects/content/components/Console/Commands/css.command.js
@@ -0,0 +1,38 @@
+const parseStylesInArray = (arrayStyles) => {
+  let newStyles = {};
+
+  for (let index = 0; index < arrayStyles.length; index++) {
+    const styleName = arrayStyles[index];
+
+    index++;
+    newStyles[styleName] = arrayStyles[index];
+  }
+
+  return newStyles;
+};
+
+const callback = ([elements, ...styles]) => {
+  const parsedStyles = parseStylesInArray(styles);
+
+  if (elements?.length) {
+    return [{ label: "Error: Not valid elements to apply styles", value: {} }];
+  }
+
+  elements.forEach(({ value }) => {
+    Object.keys(parsedStyles).forEach((styleName) => {
+      const styleValue = parsedStyles[styleName];
+
+      if (value) {
+        value.style[styleName] = styleValue;
+      }
+    });
+  });
+
+  return [{ label: "Styles Applied", value: parsedStyles }];
+};
+
+export const css = {
+  css: {
+    callback,
+  },
+};

02fde7b439308e89828468e3e68c7fd9bb91edd6
Luis Olmedo <olmedoluis012@gmail.com>
2021-02-28 13:17:22 -0300

    feat(TextInterpreter.component.js):Use styles

--- src/projects/content/modules/Console/components/TextInterpreter.component.js
+++ src/projects/content/modules/Console/components/TextInterpreter.component.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+import styles from "./TextInterpreter.styles.scss";
+
 export const TextInterpreter = ({ texts }) => {
   console.log("texts", texts);
   return (
-    <div>
+    <div className={styles.interpreter_wrapper}>
       {texts.map((text, index) => {
-        return <p key={index}>{text.map(({ label }) => label)}</p>;
+        return <p key={index}>{text.map(({ label }) => label).join(" ")}</p>;
       })}
     </div>
   );

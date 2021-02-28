fdb2cc0cea9a62ae2f1bf8b7f53b219a99be236b
Luis Olmedo <olmedoluis012@gmail.com>
2021-02-28 14:13:32 -0300

    fix(HistoryInterpreter.component.js):Properly import styles

--- src/projects/content/modules/Console/components/HistoryInterpreter/HistoryInterpreter.component.js
+++ src/projects/content/modules/Console/components/HistoryInterpreter/HistoryInterpreter.component.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import styles from "./TextInterpreter.styles.scss";
+import styles from "./HistoryInterpreter.styles.scss";
 
 export const HistoryInterpreter = ({ texts }) => {
   console.log("texts", texts);

716d121fbd2dbb94cb3d0a81bfcc022532a79d03
Luis Olmedo <olmedoluis012@gmail.com>
2021-03-06 19:17:15 -0300

    feat(ElementLabel.component.js):Implement styles for wrapper

--- src/projects/content/modules/Console/components/HistoryInterpreter/sub-components/ElementLabel/ElementLabel.component.js
+++ src/projects/content/modules/Console/components/HistoryInterpreter/sub-components/ElementLabel/ElementLabel.component.js
@@ -2,13 +2,18 @@ import React from "react";
 
 export const ElementLabel = ({
   value,
-  theme: { tag: tagStyle, class: classStyle, id: idStyle },
+  theme: {
+    tag: tagStyle,
+    class: classStyle,
+    id: idStyle,
+    wrapper: wrapperStyle,
+  },
 }) => {
   const { localName, classList, id } = value;
   const classNames = classList && Array.from(classList).join(".");
 
   return (
-    <span>
+    <span style={wrapperStyle}>
       <span style={tagStyle}>{localName}</span>
       <span style={classStyle}>{classNames && `.${classNames}`}</span>
       <span style={idStyle}>{id && `#${id}`}</span>

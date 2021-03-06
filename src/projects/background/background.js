af0717f2cd6ff940bff0d729d85c780cc1213285
Luis Olmedo <olmedoluis012@gmail.com>
2021-03-06 15:00:07 -0300

    refactor(background.js):Replace deprecated function

--- src/projects/background/background.js
+++ src/projects/background/background.js
@@ -75,7 +75,7 @@ chrome.storage.local.get([SCRIPTS_STORE], function (result) {
 });
 
 chrome.commands.onCommand.addListener(function (command) {
-  chrome.tabs.getSelected(null, ({ id }) => {
+  chrome.tabs.query({ active: true }, ([{ id }]) => {
     broker.send(NEW_COMMAND, { command }, null, id);
   });
 });

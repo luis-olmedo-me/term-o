695f0870c72e1559e19eeb548e83138343ca10b4
olmedoluis <olmedoluis@gmail.com>
2022-01-27 00:16:22 -0300

    feat: add styles for each element

--- src/libs/easy-commander/components/Element/Element.styles.js
+++ src/libs/easy-commander/components/Element/Element.styles.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
 export const ElementWrapper = styled.span`
-  padding: 2px 5px;
+  padding: 2px 10px;
   background-color: #9e86c8;
   color: #d6d6d6;
-  border-radius: 5px;
+  border-radius: 4px;
 `

508022c7d10dac4d7a83aa606563b75509db4c9b
olmedoluis <olmedoluis@gmail.com>
2022-01-27 22:17:47 -0300

    feat: Show hidden and hovered elements

--- src/libs/easy-commander/components/Element/Element.component.js
+++ src/libs/easy-commander/components/Element/Element.component.js
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { ElementWrapper } from './Element.styles'
 
 export const Element = ({ htmlElement }) => {
-  return <ElementWrapper>{htmlElement.tagName.toLowerCase()}</ElementWrapper>
+  const orinalBoxShadow = useMemo(
+    () => htmlElement.style.boxShadow,
+    [htmlElement]
+  )
+
+  const isHidden =
+    htmlElement.style.visibility === 'hidden' ||
+    htmlElement.style.display === 'none'
+
+  const highlightElement = () => {
+    htmlElement.style.boxShadow = '0 0 0 3px #ff0 inset'
+  }
+
+  const unhighlightElement = () => {
+    htmlElement.style.boxShadow = orinalBoxShadow
+  }
+
+  return (
+    <ElementWrapper
+      onMouseEnter={highlightElement}
+      onMouseLeave={unhighlightElement}
+      isHidden={isHidden}
+    >
+      {htmlElement.tagName.toLowerCase()}
+    </ElementWrapper>
+  )
 }
--- src/libs/easy-commander/components/Element/Element.styles.js
+++ src/libs/easy-commander/components/Element/Element.styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const ElementWrapper = styled.span`
   padding: 2px 10px;
-  background-color: #9e86c8;
+  background-color: ${(props) => (props.isHidden ? '#6e5698' : '#9e86c8')};
   color: #d6d6d6;
   border-radius: 4px;
 `

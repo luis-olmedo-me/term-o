669a47f5d2e84a8da3fdc19556bfa0dff8c91583
olmedoluis <olmedoluis@gmail.com>
2022-02-12 18:19:52 -0300

    feat: add ascii icon to errors

--- src/libs/easy-commander/components/LogWrapper/LogWrapper.component.js
+++ src/libs/easy-commander/components/LogWrapper/LogWrapper.component.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { parameterTypes } from '../../easyCommander.constants'
 import { Hash, Log } from './LogWrapper.styles'
 
+const preIconsByVariants = {
+  [parameterTypes.COMMAND]: '＄',
+  [parameterTypes.ERROR]: '✖'
+}
+
 export const LogWrapper = ({ children, variant }) => {
+  const icon = preIconsByVariants[variant]
   return (
     <Log className={variant}>
-      {variant === 'command' ? <Hash>$</Hash> : null}
+      {icon && <Hash>{icon}</Hash>}
       {children}
     </Log>
   )

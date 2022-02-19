13ca99a024636cc6264052b175da4743135eab9e
olmedoluis <olmedoluis@gmail.com>
2022-02-19 03:36:55 -0300

    feat: Allow to resize left side

--- src/projects/content/view/Content.styles.js
+++ src/projects/content/view/Content.styles.js
@@ -3,11 +3,28 @@ import styled from 'styled-components'
 export const ContentWrapper = styled.div`
   position: fixed;
   z-index: 1000000;
-  opacity: ${(props) => props.opacity || 0};
-  right: 0;
-  left: 0;
-  top: 0;
-  bottom: 0;
+  opacity: ${(props) => props.opacity};
+  right: ${(props) => `${props.right}px`};
+  left: ${(props) => `${props.left}px`};
+  top: ${(props) => `${props.top}px`};
+  bottom: ${(props) => `${props.bottom}px`};
   display: flex;
   flex-flow: column;
+`
+
+export const ResizerLeft = styled.button`
+  border: none;
+  background-color: transparent;
+  border-radius: 0;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: 2px;
+
+  &&:hover {
+    cursor: col-resize;
+    background-color: #aaa;
+    width: 8px;
+  }
 `
--- src/projects/content/view/Content.view.js
+++ src/projects/content/view/Content.view.js
@@ -1,14 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 
 import { Console } from '../modules/Console/Console.component'
 
-import {
-  ContentWrapper,
-  ResizerLeft,
-  ResizerRight,
-  ResizerTop,
-  ResizerBottom
-} from './Content.styles.js'
+import { ContentWrapper, ResizerLeft } from './Content.styles.js'
 import {
   eventTypes,
   extensionKeyEvents
@@ -18,6 +12,15 @@ import { resizeTypes } from './hooks/useResize/useResize.constants'
 
 export const Content = () => {
   const [isConsoleOpen, setIsConsoleOpen] = useState(false)
+  const wrapperReference = useRef(null)
+  const [isResizing, setIsResizing] = useState(false)
+  const [resizingFrom, setResizingFrom] = useState('')
+  const [resizeData, setResizeData] = useState({
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0
+  })
 
   const isContentActive = isConsoleOpen
 
@@ -39,16 +42,56 @@ export const Content = () => {
     return () => chrome.extension.onMessage.removeListener(toggleTerminal)
   }, [])
 
+  useEffect(
+    function setUpResizeEvent() {
+      if (!isResizing) return
+
+      const mouseHandler = (event) => {
+        let newResizeData = {}
+
+        switch (resizingFrom) {
+          case 'left':
+            const wrapperOffsetLeft = wrapperReference.current.offsetLeft
+            const newDistance = event.clientX - wrapperOffsetLeft
+            newResizeData = { left: newDistance + wrapperOffsetLeft }
+            console.log('newDistance', newDistance)
+            console.log('wrapperOffsetLeft', wrapperOffsetLeft)
+            console.log('newResizeData.left', newResizeData.left)
+            break
+        }
+
+        setResizeData((oldResizeData) => ({
+          ...oldResizeData,
+          ...newResizeData
+        }))
+      }
+
+      addEventListener('mousemove', mouseHandler)
+
+      return () => removeEventListener('mousemove', mouseHandler)
+    },
+    [isResizing]
+  )
+
   const resizeConsole = (newResizingFrom) => {
     setResizingFrom(newResizingFrom)
+    setIsResizing(true)
   }
 
   const stopResizeConsole = () => {
-    setResizingFrom('')
+    setIsResizing(false)
   }
 
   return (
-    <ContentWrapper opacity={isContentActive ? 1 : 0}>
+    <ContentWrapper
+      ref={wrapperReference}
+      opacity={isContentActive ? 1 : 0}
+      {...resizeData}
+    >
+      <ResizerLeft
+        onMouseDown={() => resizeConsole('left')}
+        onMouseUp={stopResizeConsole}
+      />
       <Console isOpen={isConsoleOpen} />
     </ContentWrapper>
   )

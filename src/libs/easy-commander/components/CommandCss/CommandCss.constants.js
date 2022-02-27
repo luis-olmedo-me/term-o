184590dcc1e4ac6f09fa7c3ddb73b3bc6623b5c1
olmedoluis <olmedoluis@gmail.com>
2022-02-27 12:40:51 -0300

    feat: Isolate css config in constant file

--- src/libs/easy-commander/components/CommandCss/CommandCss.constants.js
+++ src/libs/easy-commander/components/CommandCss/CommandCss.constants.js
@@ -1,8 +1,32 @@
+import React from 'react'
+
+import { CommandCss } from './CommandCss.component'
+
 import { kebabize, validStyleKeys } from '../../easyCommander.promises'
 
-export const cssProps = validStyleKeys.reduce((props, key) => {
+const cssProps = validStyleKeys.reduce((props, key) => {
   return {
     ...props,
     [kebabize(key)]: { key, type: 'string', defaultValue: '', aliases: [] }
   }
 }, {})
+
+export const cssConfig = {
+  props: {
+    'manual-styles': {
+      key: 'manualStyles',
+      internal: true,
+      type: 'object',
+      groupProps: cssProps,
+      defaultValue: {},
+      aliases: []
+    },
+    styles: {
+      key: 'styles',
+      type: 'string',
+      defaultValue: '',
+      aliases: ['s']
+    }
+  },
+  output: (props) => <CommandCss key={props.id} {...props} />
+}

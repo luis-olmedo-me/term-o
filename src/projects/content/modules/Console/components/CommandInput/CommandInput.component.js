ad20cfe813a25bb3b47be830eaceb5f45302ceb5
olmedoluis <63429352+olmedoluis@users.noreply.github.com>
2022-03-01 18:33:37 -0300

    [WBS-55] Add auto completition (#49)
    * feat: Rename dom command component
    
    * feat: Rename css command component
    
    * feat: Isolate css config in constant file
    
    * feat: Isolate dom config in constant file
    
    * feat: Isolate event config in constant file
    
    * feat: Isolate oON config in constant file
    
    * feat: Isolate oON config in constant file
    
    * feat: Add suggestion section
    
    * feat: Show suggestions
    
    * feat: Save all props config data
    
    * feat: filter command name
    
    * feat: Filter props
    
    * feat: Select option
    
    * feat: validate selected id
    
    * feat: Update new suggestions
    
    * set last suggestion as selected by default
    
    * refactor: remove unused stuff
    
    * feat: properly hide suggestions
    
    * feat: Properly show suggestions
    
    * feat: Properly reset after hitting ente
    
    * feat: Properly reset after hitting ente
    
    * feat: Add styles for selected state
    
    * feat: add sanity check
    
    * feat: show aliases
    
    * feat: add - into aliases
    
    * feat: Hide suggestions when theres no command
    
    * feat: Filter suggestions properly
    
    * feat: handle | characters
    
    * feat: trim from left
    
    Co-authored-by: olmedoluis <olmedoluis@gmail.com>

--- src/projects/content/modules/Console/components/CommandInput/CommandInput.component.js
+++ src/projects/content/modules/Console/components/CommandInput/CommandInput.component.js
@@ -1,31 +1,118 @@
 import React, { useState } from 'react'
-import { Hash, Input, InputWrapper } from './CommandInput.styles'
+import {
+  Hash,
+  Input,
+  InputWrapper,
+  Suggestion,
+  Suggestions
+} from './CommandInput.styles'
+import { commander } from 'libs/easy-commander/easyCommander.service'
 
 export const CommandInput = ({ inputReference, handleOnEnter }) => {
   const [command, setCommand] = useState('')
+  const [suggestions, setSuggestions] = useState([])
+  const [selectedSuggestionId, setSelectedSuggestionId] = useState(0)
 
   const handleCommandChange = ({ target: { value: newValue } }) => {
+    const newSuggestions = commander.getSuggestions(newValue)
+
     setCommand(newValue)
+    setSuggestions(newValue ? newSuggestions : [])
+    setSelectedSuggestionId(0)
   }
 
-  const handleKeyPressed = ({ key }) => {
+  const handleKeyPressed = (event) => {
+    const { key } = event
+
     if (key === 'Enter') {
-      handleOnEnter(command)
-      setCommand('')
+      if (!shouldShowSuggestions) {
+        handleOnEnter(command)
+        setCommand('')
+        setSuggestions([])
+        setSelectedSuggestionId(0)
+      } else {
+        const selectedSuggestion = suggestions[selectedSuggestionId]
+
+        const args = command.split(' ')
+        const firstArguments = args.splice(0, args.length - 1)
+
+        const newCommand = firstArguments.length
+          ? `${firstArguments.join(' ')} ${selectedSuggestion.value} `
+          : `${selectedSuggestion.value} `
+
+        const newSuggestions = commander.getSuggestions(newCommand)
+
+        setCommand(newCommand)
+        setSuggestions(newSuggestions)
+      }
+    } else if (key === 'ArrowUp') {
+      event.preventDefault()
+
+      setSelectedSuggestionId((indexId) => {
+        const nextId = Number(indexId) - 1
+        const validatedId = nextId < 0 ? suggestions.length - 1 : nextId
+
+        return validatedId
+      })
+    } else if (key === 'ArrowDown') {
+      event.preventDefault()
+
+      setSelectedSuggestionId((indexId) => {
+        const nextId = Number(indexId) + 1
+        const validatedId = nextId > suggestions.length - 1 ? 0 : nextId
+
+        return validatedId
+      })
+    } else if (key === '|') {
+      event.preventDefault()
+
+      const newCommand = `${command}| `
+      const newSuggestions = commander.getSuggestions(newCommand)
+
+      setCommand(newCommand)
+      setSuggestions(newCommand ? newSuggestions : [])
+      setSelectedSuggestionId(0)
     }
   }
 
-  return (
-    <InputWrapper ref={inputReference}>
-      <Hash>$</Hash>
+  const [lastCommand] = command.split('|').reverse()
+  const lastArgs = lastCommand.trimLeft().split(' ')
 
-      <Input
-        type='text'
-        onChange={handleCommandChange}
-        onKeyDown={handleKeyPressed}
-        value={command}
-        autoFocus
-      />
+  const areSuggestionsAvailable =
+    lastArgs.at(-1).startsWith('-') || lastArgs.length === 1
+  const shouldShowSuggestions =
+    Boolean(suggestions.length) && areSuggestionsAvailable
+
+  return (
+    <InputWrapper>
+      {shouldShowSuggestions && (
+        <Suggestions>
+          {suggestions.map((suggestion, index) => {
+            const isSelected = selectedSuggestionId === index
+            const aliases = suggestion.aliases || []
+
+            return (
+              <Suggestion key={suggestion.value} selected={isSelected}>
+                <span>{suggestion.value}</span>
+                <span>{aliases.join(', ')}</span>
+              </Suggestion>
+            )
+          })}
+        </Suggestions>
+      )}
+
+      <div>
+        <Hash>$</Hash>
+
+        <Input
+          ref={inputReference}
+          type='text'
+          onChange={handleCommandChange}
+          onKeyDown={handleKeyPressed}
+          value={command}
+          autoFocus
+        />
+      </div>
     </InputWrapper>
   )
 }

ae6b3fdbec5462a81d824d233ae54f59058b7515
olmedoluis <olmedoluis@gmail.com>
2022-03-01 19:19:59 -0300

    feat: push url

--- src/libs/easy-commander/components/CommandHistory/CommandHistory.component.js
+++ src/libs/easy-commander/components/CommandHistory/CommandHistory.component.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { LogWrapper } from '../LogWrapper/LogWrapper.component'
 
 import { parameterTypes } from '../../easyCommander.constants'
 
-export const CommandHistory = ({ command }) => {
+export const CommandHistory = ({ command, props: { goto, protocol } }) => {
+  useEffect(
+    function pushIntoURL() {
+      goto.forEach((url) => {
+        const formattedUrl = url.startsWith('www') ? url : `www.${url}`
+
+        console.log('Pushing into URL: ', url)
+        window.open(`${protocol}://${formattedUrl}`, '_blank')
+      })
+    },
+    [goto, protocol]
+  )
+
   return (
     <>
       <LogWrapper variant={parameterTypes.COMMAND}>{command}</LogWrapper>
--- src/libs/easy-commander/components/CommandHistory/CommandHistory.constants.js
+++ src/libs/easy-commander/components/CommandHistory/CommandHistory.constants.js
@@ -3,6 +3,19 @@ import React from 'react'
 import { CommandHistory } from './CommandHistory.component'
 
 export const historyConfig = {
-  props: {},
+  props: {
+    goto: {
+      key: 'goto',
+      type: 'array',
+      defaultValue: [],
+      aliases: ['g']
+    },
+    protocol: {
+      key: 'protocol',
+      type: 'string',
+      defaultValue: 'https',
+      aliases: ['p']
+    }
+  },
   output: (props) => <CommandHistory key={props.id} {...props} />
 }

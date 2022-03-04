9f3a8d634c8abc0e63661f50595ef19c0ba0791b
olmedoluis <olmedoluis@gmail.com>
2022-03-04 13:09:12 -0300

    feat: suggest aliases

--- src/libs/easy-commander/easyCommander.service.js
+++ src/libs/easy-commander/easyCommander.service.js
@@ -30,15 +30,23 @@ class Commander {
   }
 
   getSuggestions(command) {
-    const [lastCommand] = command.split('|').reverse()
+    const [lastCommand] = this.getCommandWithAliases(command)
+      .split('|')
+      .reverse()
     const [commandName, ...commandArgs] = lastCommand.trim().split(' ')
     const commandNames = Object.keys(this.commands)
+    const aliasNames = Object.keys(this.aliases)
 
-    const defaultProps = commandNames.reduce((finalProps, name) => {
+    const aliasAsProps = aliasNames.reduce((finalProps, name) => {
       const isMatch = name.includes(commandName)
 
       return isMatch ? [...finalProps, { value: name }] : finalProps
     }, [])
+    const defaultProps = commandNames.reduce((finalProps, name) => {
+      const isMatch = name.includes(commandName)
+
+      return isMatch ? [...finalProps, { value: name }] : finalProps
+    }, aliasAsProps)
 
     const knownCommand = this.commands[commandName]
     const { _: _values, ...props } = parseArgsIntoCommands(commandArgs)

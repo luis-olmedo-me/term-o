b7e86b4127532dd1c47fec68774397e220712061
olmedoluis <olmedoluis@gmail.com>
2022-03-24 14:15:53 -0300

    feat: change message type

--- src/libs/easy-commander/components/CommandDom/CommandDom.messages.js
+++ src/libs/easy-commander/components/CommandDom/CommandDom.messages.js
@@ -3,6 +3,6 @@ import { parameterTypes } from '../../constants/commands.constants'
 export const domMessages = {
   noElementsFound: {
     message: 'No elements where found in DOM for: "{patterns}".',
-    type: parameterTypes.INFO
+    type: parameterTypes.ERROR
   }
 }

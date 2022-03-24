fec21948226d95952d360ca49151265fe89adc1e
olmedoluis <olmedoluis@gmail.com>
2022-03-24 14:27:26 -0300

    fix: create message file for event command

---
+++ src/libs/easy-commander/components/CommandEvent/CommandEvent.messages.js
@@ -0,0 +1,12 @@
+import { parameterTypes } from '../../constants/commands.constants'
+
+export const eventMessages = {
+  invalidEventIds: {
+    message: 'The following ids were not found: {invalidIds}',
+    type: parameterTypes.ERROR
+  },
+  eventDeleteSuccess: {
+    message: 'Event(s) deleted successfully',
+    type: parameterTypes.SUCCESS
+  }
+}

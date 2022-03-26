6f46771aa846b8b43fabf30cad1df1265f4c3d8c
olmedoluis <olmedoluis@gmail.com>
2022-03-25 23:25:54 -0300

    refactor: remove unexpected action type

--- src/libs/commander/components/CommandHistory/CommandHistory.helpers.js
+++ src/libs/commander/components/CommandHistory/CommandHistory.helpers.js
@@ -1,7 +1,6 @@
 import { actionTypes } from '../../constants/commands.constants'
 
 export const getActionType = ({ goto, protocol }) => {
-  if (goto.length) return actionTypes.REDIRECT
-  else if (protocol) return actionTypes.SHOW_LIST
+  if (goto.length || protocol) return actionTypes.REDIRECT
   else return actionTypes.NONE
 }

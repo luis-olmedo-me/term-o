0e1d6eaf0caeb1091e60c4ab6a04d4be85a192a2
olmedoluis <olmedoluis@gmail.com>
2022-03-27 00:49:31 -0300

    feat: show success message after config reset

--- src/libs/commander/components/CommandClear/CommandClear.component.js
+++ src/libs/commander/components/CommandClear/CommandClear.component.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { resetConfiguration } from 'src/helpers/event.helpers.js'
 import { actionTypes } from '../../constants/commands.constants'
 import { getActionType } from './CommandClear.helpers'
+import { clearMessages } from './CommandClear.messages'
 
 export const CommandClear = ({ props, terminal: { clearTerminal } }) => {
   const actionType = getActionType(props)
@@ -15,6 +16,8 @@ export const CommandClear = ({ props, terminal: { clearTerminal } }) => {
 
         case actionTypes.CLEAR_CONFIG:
           resetConfiguration()
+            .catch(() => setMessageData(clearMessages.unexpectedError))
+            .then(() => setMessageData(clearMessages.configurationResetSuccess))
           break
 
         default:

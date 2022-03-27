a0cbea3ed638fd4589780254cbe7720bca8358c1
olmedoluis <olmedoluis@gmail.com>
2022-03-27 01:58:36 -0300

    feat: generalize error message

--- src/libs/commander/components/CommandDom/CommandDom.messages.js
+++ src/libs/commander/components/CommandDom/CommandDom.messages.js
@@ -2,7 +2,7 @@ import { parameterTypes } from '../../constants/commands.constants'
 
 export const domMessages = {
   noElementsFound: {
-    message: 'No elements where found in DOM for: "{patterns}".',
+    message: 'No elements where found in DOM.',
     type: parameterTypes.ERROR
   }
 }

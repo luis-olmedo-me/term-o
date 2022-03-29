94bb53a6e1cd0d14612cedd1bb719e189ac9d6a6
olmedoluis <olmedoluis@gmail.com>
2022-03-29 00:09:06 -0300

    feat: highlight elements on hover

--- src/modules/components/Overlay/Overlay.component.js
+++ src/modules/components/Overlay/Overlay.component.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Portal } from '../Portal/Portal.component'
-import { OverlayWrapper } from './Overlay.styles'
+import { HighlightedElement, OverlayWrapper } from './Overlay.styles'
 
 export const Overlay = ({ highlitedElement }) => {
+  const { top, left, height, width } = useMemo(() => {
+    return highlitedElement?.getBoundingClientRect() || {}
+  }, [highlitedElement])
+
   return (
     <Portal>
-      <OverlayWrapper isHighlighting={!!highlitedElement} />
+      <OverlayWrapper isHighlighting={!!highlitedElement}>
+        {highlitedElement && (
+          <HighlightedElement style={{ top, left, height, width }} />
+        )}
+      </OverlayWrapper>
     </Portal>
   )
 }
--- src/modules/components/Overlay/Overlay.styles.js
+++ src/modules/components/Overlay/Overlay.styles.js
@@ -4,13 +4,21 @@ export const OverlayWrapper = styled.div`
   all: initial;
   position: fixed;
   z-index: 100000;
-  font-family: 'Coda', monospace;
-  opacity: 0.5;
-  background-color: #000;
-  height: fill;
+  background-color: ${(props) =>
+    props.isHighlighting ? '#00000033' : 'transparent'};
   inset: 0;
   width: 100%;
-  opacity: ${(props) => (props.isHighlighting ? 0.2 : 0)};
   pointer-events: none;
-  transition: opacity 0.2s ease-in-out;
+  transition: background-color 0.2s ease-in-out;
+`
+
+export const HighlightedElement = styled.div`
+  position: fixed;
+  z-index: 100001;
+  opacity: 0.5;
+  background-color: transparent;
+  inset: 0;
+  width: 100%;
+  border: 2px dashed #9cf;
+  box-sizing: border-box;
 `

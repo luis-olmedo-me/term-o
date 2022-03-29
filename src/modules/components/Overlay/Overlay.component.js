baa75ad32efd5e3dbff639163772db809c9bf85f
olmedoluis <olmedoluis@gmail.com>
2022-03-28 23:17:04 -0300

    feat: set up overlay styles

--- src/modules/components/Overlay/Overlay.component.js
+++ src/modules/components/Overlay/Overlay.component.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Portal } from '../Portal/Portal.component'
+import { OverlayWrapper } from './Overlay.styles'
 
 export const Overlay = () => {
   return (
     <Portal>
-      <h1>hola</h1>
+      <OverlayWrapper />
     </Portal>
   )
 }
---
+++ src/modules/components/Overlay/Overlay.styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const OverlayWrapper = styled.div`
+  all: initial;
+  position: fixed;
+  z-index: 1000001;
+  font-family: 'Coda', monospace;
+  opacity: 0.5;
+  background-color: #000;
+  height: fill;
+  inset: 0;
+  width: 100%;
+`

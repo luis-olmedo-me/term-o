4d8307c51809de4d7d0f058077a806627ed6a6a0
olmedoluis <olmedoluis@gmail.com>
2022-04-05 22:45:43 -0300

    feat: improve styles

--- src/modules/components/Select/Select.styles.js
+++ src/modules/components/Select/Select.styles.js
@@ -12,8 +12,19 @@ export const SelectOptionsWrapper = styled.div`
   max-height: 150px;
   border-radius: 3px;
   transform: translate(-50%, -50%);
+  font-family: Coda;
+  box-shadow: 0 0 3px 1px #00000020;
 `
 
 export const SelectOption = styled.div`
-  height: 40px;
+  line-height: 40px;
+  vertical-align: middle;
+  text-align: center;
+  border-bottom: 1px solid #eaeaea;
+  cursor: pointer;
+  color: #333;
+
+  &&:last-child {
+    border-bottom: none;
+  }
 `

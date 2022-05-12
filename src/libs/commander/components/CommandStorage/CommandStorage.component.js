43c39b3e3f7ea63e9255a94cbe291b0d8ae575a1
olmedoluis <olmedoluis@gmail.com>
2022-05-12 00:14:07 -0300

    feat: Show local storage as list

--- src/libs/commander/components/CommandStorage/CommandStorage.component.js
+++ src/libs/commander/components/CommandStorage/CommandStorage.component.js
@@ -1,17 +1,39 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { actionTypes, parameterTypes } from '../../constants/commands.constants'
 import { LogWrapper } from '../LogWrapper/LogWrapper.component'
+import { Table } from 'modules/components/Table/Table.component'
 import { getActionType } from './CommandStorage.helpers'
 import { storageMessages } from './CommandStorage.messages'
+import { usePaginationGroups } from 'modules/components/Table/hooks/usePaginationGroups.hook'
+
+const storageHeaders = ['key', 'values']
+
+const turnStorageToTableItems = ({ storage = {} }) => {
+  return Object.keys(storage).map((key) => {
+    const values = storage[key]
+    return [key, values]
+  })
+}
 
 export const CommandStorage = ({ props, terminal: { command } }) => {
   const actionType = getActionType(props)
+
+  const [tableItems, setTableItems] = useState([])
+
+  const { pageData, buttonGroups } = usePaginationGroups({
+    items: tableItems,
+    maxItems: 10
+  })
 
   useEffect(
     function handleActionType() {
       switch (actionType) {
         case actionTypes.LOCAL_STORAGE:
-          console.log('showing LOCAL_STORAGE...', window.localStorage)
+          const localStorageAsTableItems = turnStorageToTableItems({
+            storage: window.localStorage
+          })
+
+          setTableItems(localStorageAsTableItems)
           break
 
         default:
@@ -25,7 +47,9 @@ export const CommandStorage = ({ props, terminal: { command } }) => {
     <>
       <LogWrapper variant={parameterTypes.COMMAND}>{command}</LogWrapper>
 
-      <LogWrapper variant={parameterTypes.SUCCESS}>Worked!</LogWrapper>
+      <LogWrapper variant={parameterTypes.TABLE} buttonGroups={buttonGroups}>
+        <Table headers={storageHeaders} rows={pageData} />
+      </LogWrapper>
     </>
   )
 }

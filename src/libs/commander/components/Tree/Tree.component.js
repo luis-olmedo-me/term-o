fc922cad6a0882845ecb96b17b9677da6f4c4810
olmedoluis <olmedoluis@gmail.com>
2022-05-25 01:47:33 -0300

    feat: Use editable texts

--- src/libs/commander/components/Tree/Tree.component.js
+++ src/libs/commander/components/Tree/Tree.component.js
@@ -62,7 +62,15 @@ export const Tree = ({ content, title, className, hasComa }) => {
   return (
     <div className={className}>
       <span>
-        {title ? `${title}: ${quotedContent}` : quotedContent}
+        {title ? (
+          <>
+            <EditableText title={title} />
+            {':'}
+            <EditableText title={quotedContent} />
+          </>
+        ) : (
+          <EditableText title={quotedContent} />
+        )}
         {hasComa && ','}
       </span>
     </div>
@@ -72,3 +80,32 @@ export const Tree = ({ content, title, className, hasComa }) => {
 const IdentedTree = styled(Tree)`
   margin-left: 20px;
 `
+
+const EditableText = ({ title }) => {
+  const [value, setValue] = React.useState(title)
+  const [isEditing, setIsEditing] = React.useState(false)
+
+  const handleKeyPress = (event) => {
+    event.stopPropagation()
+
+    if (event.key === 'Enter') {
+      setValue(title)
+      setIsEditing(false)
+    }
+    if (event.key === 'Escape') {
+      setIsEditing(false)
+    }
+  }
+
+  return isEditing ? (
+    <input
+      type='text'
+      value={value}
+      onChange={(event) => setValue(event.target.value)}
+      onKeyPress={handleKeyPress}
+      onMouseDown={(event) => event.stopPropagation()}
+    />
+  ) : (
+    <span onClick={() => setIsEditing(true)}>{title}</span>
+  )
+}

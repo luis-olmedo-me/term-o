6336ada3d454b3f23212690f59c1979337101a94
olmedoluis <olmedoluis012@gmail.com>
2022-08-13 17:50:59 -0300

    refactor: Isolate helpers

--- src/libs/commander/components/CommandHelp/CommandHelp.component.js
+++ src/libs/commander/components/CommandHelp/CommandHelp.component.js
@@ -1,7 +1,10 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { parameterTypes } from '../../constants/commands.constants'
 import { helpActionTypes } from './CommandHelp.constants'
-import { getActionType } from './CommandHelp.helpers'
+import {
+  getActionType,
+  getMessagesFromCommandsToCheck
+} from './CommandHelp.helpers'
 import { LogWrapper } from '../LogWrapper/LogWrapper.component'
 import { consoleCommands } from '../../commander.constants'
 import { List } from 'modules/components/Table/List/List.component'
@@ -13,36 +16,7 @@ export const CommandHelp = ({ props, terminal: { command } }) => {
   const [localMessages, setLocalMessages] = useState([])
 
   const handleHelp = useCallback((commands) => {
-    const messagesForHelp = commands.reduce((messages, commandToHelpWith) => {
-      const commandData = consoleCommands[commandToHelpWith]
-
-      if (commandData) {
-        const { props } = commandData
-
-        return [
-          ...messages,
-          {
-            id: commandToHelpWith,
-            title: commandToHelpWith,
-            items: Object.entries(props).map(([propName, propConfig]) => {
-              return {
-                id: propConfig.key,
-                text: `--${propName} <${propConfig.type}> - ${propConfig.description}`
-              }
-            })
-          }
-        ]
-      } else {
-        return [
-          ...messages,
-          {
-            id: commandToHelpWith,
-            title: commandToHelpWith,
-            warning: 'Command not found'
-          }
-        ]
-      }
-    }, [])
+    const messagesForHelp = getMessagesFromCommandsToCheck({ commands })
 
     setLocalMessages(messagesForHelp)
   }, [])

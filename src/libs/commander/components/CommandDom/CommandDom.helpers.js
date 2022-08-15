620ab0b22a27f04a006758d4105793565c9413f5
olmedoluis <63429352+olmedoluis@users.noreply.github.com>
2022-08-15 20:23:33 -0300

    [WBS-101] Improve by text prop from dom command (#87)
    * feat: Check text as node childs
    
    * feat: Expect for input values

--- src/libs/commander/components/CommandDom/CommandDom.helpers.js
+++ src/libs/commander/components/CommandDom/CommandDom.helpers.js
@@ -74,7 +74,18 @@ export const generateFilterBySome = ({
     }
     if (byText.length) {
       validations.push((element) =>
-        byText.some((text) => element.textContent?.includes?.(text))
+        byText.some((text) => {
+          const hasTextMatch = Array.from(element.childNodes).some((child) => {
+            const isTextNode = child.nodeType === 3
+            const isTextMatch = !!child?.data?.includes(text)
+
+            return isTextMatch && isTextNode
+          })
+
+          const hasValueMatch = element.value?.includes?.(text)
+
+          return hasTextMatch || hasValueMatch
+        })
       )
     }
     if (byStyle.length) {

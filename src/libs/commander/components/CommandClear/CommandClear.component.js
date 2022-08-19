f3b8a38465330ed531a70624a0cf1afdbd16df70
olmedoluis <olmedoluis012@gmail.com>
2022-08-18 23:18:46 -0300

    fix: Properly import stuff

--- src/libs/commander/components/CommandClear/CommandClear.component.js
+++ src/libs/commander/components/CommandClear/CommandClear.component.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { resetConfiguration } from 'src/helpers/event.helpers.js'
 import { actionTypes, parameterTypes } from '../../constants/commands.constants'
 import { getActionType } from './CommandClear.helpers'
@@ -8,7 +8,7 @@ import { LogWrapper } from '../LogWrapper/LogWrapper.component'
 
 export const CommandClearWithoutContext = ({
   props,
-  terminal: { clearTerminal, setMessageData },
+  terminal: { clearTerminal, setMessageData, command },
   setHighlitedElement
 }) => {
   const actionType = getActionType(props)

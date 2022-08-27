21ab14ed4a9133a7c3f37071361ef961d175a9f3
olmedoluis <olmedoluis012@gmail.com>
2022-08-27 02:35:38 -0300

    feat: Add hardcoded data

--- src/libs/commander/components/Notification/Notification.styles.js
+++ src/libs/commander/components/Notification/Notification.styles.js
@@ -8,10 +8,17 @@ export const NotificationWrapper = styled.div`
   left: 10px;
   top: 10px;
   width: 400px;
-  height: 150px;
   border-radius: 3px;
   font-family: Coda;
   box-shadow: 0 0 15px 5px #0000002a;
   padding: 10px;
   font-size: 1em;
+  display: flex;
+  gap: 24px;
+  align-items: center;
+  box-sizing: content-box;
+`
+
+export const Metadata = styled.p`
+  width: 70%;
 `

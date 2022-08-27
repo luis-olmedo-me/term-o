93d9fa60fa209f0d918fe75cc45de68e3fe292c6
olmedoluis <olmedoluis012@gmail.com>
2022-08-27 11:52:29 -0300

    feat: Hide notifications after 3 seconds

--- src/modules/components/Notifications/hooks/useNotifications.hook.js
+++ src/modules/components/Notifications/hooks/useNotifications.hook.js
@@ -1,13 +1,26 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 export const useNotifications = () => {
   const [notifications, setNotifications] = useState([])
+  console.log({ notifications })
 
   const addNotification = useCallback((id, message) => {
     setNotifications((oldNotifications) => {
       return [...oldNotifications, { id, message }]
     })
   }, [])
+
+  useEffect(() => {
+    if (!notifications.length) return
+
+    const clearLastNotificationTimeoutId = setTimeout(() => {
+      setNotifications((oldNotifications) => {
+        return oldNotifications.slice(1)
+      })
+    }, 3000)
+
+    return () => clearTimeout(clearLastNotificationTimeoutId)
+  }, [notifications])
 
   return { notifications, addNotification }
 }

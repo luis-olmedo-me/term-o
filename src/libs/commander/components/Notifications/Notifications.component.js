266bfc19963c01fec9c888339b3d4446f8015565
olmedoluis <olmedoluis012@gmail.com>
2022-08-27 11:24:25 -0300

    feat: Change name of component

---
+++ src/libs/commander/components/Notifications/Notifications.component.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Portal } from 'src/modules/components/Portal/Portal.component'
+import { Logo } from 'src/modules/icons/Logo.icon'
+import {
+  Description,
+  NotificationWrapper,
+  LogoWrapper,
+  NotificationsWrapper
+} from './Notifications.styles'
+
+export const Notifications = ({ messages }) => {
+  return (
+    <Portal>
+      <NotificationsWrapper>
+        {messages.map(({ id, message }) => {
+          return (
+            <NotificationWrapper key={id}>
+              <Logo size={50} Wrapper={LogoWrapper} />
+
+              <Description>{message}</Description>
+            </NotificationWrapper>
+          )
+        })}
+      </NotificationsWrapper>
+    </Portal>
+  )
+}
---
+++ src/libs/commander/components/Notifications/Notifications.styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+export const NotificationsWrapper = styled.div`
+  all: initial;
+  position: fixed;
+  z-index: 1000004;
+  left: 10px;
+  top: 10px;
+  width: 400px;
+  font-family: Coda;
+  font-size: 1em;
+`
+
+export const NotificationWrapper = styled.div`
+  margin-bottom: 10px;
+  box-shadow: 0 0 15px 5px #0000002a;
+  background-color: #111;
+  padding: 10px;
+  display: flex;
+  gap: 24px;
+  border-radius: 3px;
+  align-items: center;
+  box-sizing: content-box;
+
+  &:last-child {
+    margin-bottom: none;
+  }
+`
+
+export const Description = styled.p`
+  margin: 0;
+`
+
+export const LogoWrapper = styled.svg`
+  background: #ffffff20;
+  border-radius: 15px;
+  padding: 6px;
+`

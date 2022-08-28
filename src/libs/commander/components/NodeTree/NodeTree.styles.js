9ff0bc6209a830c802fa6f46ec63db8e9a7ba341
olmedoluis <olmedoluis012@gmail.com>
2022-08-28 16:02:55 -0300

    feat: Limit the size of the shown elements

--- src/libs/commander/components/NodeTree/NodeTree.styles.js
+++ src/libs/commander/components/NodeTree/NodeTree.styles.js
@@ -4,4 +4,11 @@ export const TreeWrapper = styled.div`
   padding: 10px;
   background: #00000020;
   border-radius: 3px;
+  overflow: scroll;
+  white-space: nowrap;
+  max-height: 550px;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `

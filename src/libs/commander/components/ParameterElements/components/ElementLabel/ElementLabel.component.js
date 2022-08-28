5071cac04472d01971d30a9bb0f401f2fe6de248
olmedoluis <olmedoluis012@gmail.com>
2022-08-28 02:11:33 -0300

    feat: Show same data as before but stylish

--- src/libs/commander/components/ParameterElements/components/ElementLabel/ElementLabel.component.js
+++ src/libs/commander/components/ParameterElements/components/ElementLabel/ElementLabel.component.js
@@ -1,12 +1,33 @@
 import React from 'react'
 
 export const ElementLabel = ({ element }) => {
+  const { id, classList } = element
+
   const tagName = element.tagName.toLowerCase()
+  const classes = [...classList].join(' ')
 
   return (
     <span>
       <span style={{ color: '#9BA1A6' }}>{'<'}</span>
+
       <span style={{ color: '#0070f3' }}>{tagName}</span>
+
+      {id && (
+        <>
+          <span style={{ color: '#F8C572' }}>{` id`}</span>
+          <span style={{ color: '#F6AD37' }}>{`=`}</span>
+          <span style={{ color: '#F5A524' }}>{`"${id}"`}</span>
+        </>
+      )}
+
+      {classes && (
+        <>
+          <span style={{ color: '#F8C572' }}>{` class`}</span>
+          <span style={{ color: '#F6AD37' }}>{`=`}</span>
+          <span style={{ color: '#F5A524' }}>{`"${classes}"`}</span>
+        </>
+      )}
+
       <span style={{ color: '#9BA1A6' }}>{' />'}</span>
     </span>
   )

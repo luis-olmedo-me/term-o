7b9c042d55e4cc1cd9327323f38623715c49096b
olmedoluis <olmedoluis012@gmail.com>
2022-09-07 23:22:14 -0300

    feat: Use table to show attributes

--- src/libs/commander/modules/ElementEdition/ElementEdition.component.js
+++ src/libs/commander/modules/ElementEdition/ElementEdition.component.js
@@ -1,5 +1,21 @@
 import React from 'react'
+import { getAttributes } from '../../components/CommandDom/CommandDom.helpers'
+import { Table } from 'modules/components/Table/Table.component'
 
 export const ElementEdition = ({ element }) => {
-  return <span>editing {element?.tagName}</span>
+  const attributes = getAttributes(element)
+
+  const attributeRows = Object.entries(attributes).map(
+    ([attributeName, attributeValue]) => {
+      return [attributeName, <input type='text' value={attributeValue} />]
+    }
+  )
+
+  return (
+    <Table
+      headers={['Attribute', 'Value']}
+      rows={attributeRows}
+      widths={[50, 50]}
+    />
+  )
 }

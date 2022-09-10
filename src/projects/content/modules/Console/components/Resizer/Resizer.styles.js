c7ba9da8d8cceaad1e46e04522372c849c2daf02
olmedoluis <olmedoluis012@gmail.com>
2022-09-09 23:03:55 -0300

    feat: Add better borders to resizers

--- src/projects/content/modules/Console/components/Resizer/Resizer.styles.js
+++ src/projects/content/modules/Console/components/Resizer/Resizer.styles.js
@@ -21,12 +21,19 @@ export const ResizerWrapper = styled.button`
       background-color: #333;
       width: 8px;
     }
+
+    &&:active,
+    &&:focus {
+      outline: none;
+    }
   }
   &&.left {
     left: 0;
+    border-radius: 5px 0 0 5px;
   }
   &&.right {
     right: 0;
+    border-radius: 0 5px 5px 0;
   }
 
   &&.top,
@@ -42,11 +49,18 @@ export const ResizerWrapper = styled.button`
       background-color: #333;
       height: 12px;
     }
+
+    &&:active,
+    &&:focus {
+      outline: none;
+    }
   }
   &&.top {
     top: 0;
+    border-radius: 5px 5px 0 0;
   }
   &&.bottom {
     bottom: 0;
+    border-radius: 0 0 5px 5px;
   }
 `

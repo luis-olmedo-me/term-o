058fb4af2c4f6a0f6e18d3d0350fcd25c522cfc7
olmedoluis <olmedoluis012@gmail.com>
2022-09-11 22:03:22 -0300

    feat: Add beter styles for selection

--- src/projects/content/modules/Console/components/Suggestions/Suggestions.styles.js
+++ src/projects/content/modules/Console/components/Suggestions/Suggestions.styles.js
@@ -10,13 +10,20 @@ export const SuggestionsWrapper = styled.div`
 `
 
 export const Suggestion = styled.span`
-  padding: 10px 30px;
   display: block;
   color: ${({ selected }) => (selected ? '#F8C572' : '#d6d6d6')};
   background-color: ${({ selected }) => (selected ? '#222' : '#111')};
+  padding: ${(props) => (props.selected ? '10px 40px' : '10px 30px')};
   display: flex;
   justify-content: space-between;
   height: 40px;
   box-sizing: border-box;
   font-size: 16px;
+  cursor: pointer;
+  transition: padding 0.2s ease-in-out;
+
+  &:hover {
+    background-color: #222;
+    color: #f8c572;
+  }
 `

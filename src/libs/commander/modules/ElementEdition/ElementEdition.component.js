a76ec0cd318834df4973e5bb6dce0b9733e3345f
olmedoluis <olmedoluis012@gmail.com>
2022-09-18 03:02:09 -0300

    feat: Limit table items

--- src/libs/commander/modules/ElementEdition/ElementEdition.component.js
+++ src/libs/commander/modules/ElementEdition/ElementEdition.component.js
@@ -4,6 +4,10 @@ import { Table } from 'modules/components/Table/Table.component'
 import { Input } from './components/AttributeInput/AttributeInput.styles'
 import { parameterTypes } from '../../constants/commands.constants'
 import { turnAttributesIntoTableItems } from './ElementEdition.helpers'
+import { usePaginationGroups } from 'modules/components/Table/hooks/usePaginationGroups.hook'
+import { Carousel } from 'modules/components/Carousel/Carousel.component'
+import { CarouselItem } from 'modules/components/Carousel/Carousel.styles'
+import { LogWrapper } from '../../components/LogWrapper/LogWrapper.component'
 
 export const ElementEdition = ({ element, onGoBack }) => {
   const [newAtributeName, setNewAttributeName] = useState('')
@@ -12,8 +16,14 @@ export const ElementEdition = ({ element, onGoBack }) => {
   const attributes = getAttributes(element)
 
   const attributeRows = turnAttributesIntoTableItems({ attributes, element })
+  const { buttonGroups, pages, pageNumber } = usePaginationGroups({
+    items: attributeRows,
+    maxItems: 2
+  })
 
   const handleNewAttributeKeyUp = ({ key }) => {
+    if (!newAtributeName || !newAtributeValue) return
+
     if (key === 'Enter') {
       element.setAttribute(newAtributeName, newAtributeValue)
 
@@ -22,32 +32,30 @@ export const ElementEdition = ({ element, onGoBack }) => {
     }
   }
 
-  const rows = [
-    ...attributeRows,
-    [
-      <Input
-        type='text'
-        placeholder='New attribute name'
-        value={newAtributeName}
-        onChange={(event) => setNewAttributeName(event.target.value)}
-        onKeyUp={handleNewAttributeKeyUp}
-      />,
-      <Input
-        type='text'
-        placeholder='New attribute value'
-        value={newAtributeValue}
-        onChange={(event) => setNewAttributeValue(event.target.value)}
-        onKeyUp={handleNewAttributeKeyUp}
-      />
-    ]
+  const editableRow = [
+    <Input
+      type='text'
+      placeholder='New attribute name'
+      value={newAtributeName}
+      onChange={(event) => setNewAttributeName(event.target.value)}
+      onKeyUp={handleNewAttributeKeyUp}
+    />,
+    <Input
+      type='text'
+      placeholder='New attribute value'
+      value={newAtributeValue}
+      onChange={(event) => setNewAttributeValue(event.target.value)}
+      onKeyUp={handleNewAttributeKeyUp}
+    />
   ]
 
   const editionPageButtonGroups = [
     {
       id: 'go-back',
-      text: '<',
+      text: '<☰',
       onClick: onGoBack
-    }
+    },
+    ...buttonGroups
   ]
 
   return (
@@ -55,7 +63,20 @@ export const ElementEdition = ({ element, onGoBack }) => {
       variant={parameterTypes.TABLE}
       buttonGroups={editionPageButtonGroups}
     >
-      <Table headers={['Attribute', 'Value']} rows={rows} widths={[50, 50]} />
+      <Carousel itemInView={pageNumber}>
+        {pages.map((page, currentPageNumber) => {
+          return (
+            <CarouselItem key={currentPageNumber}>
+              <Table
+                key={currentPageNumber}
+                headers={['Attribute', 'Value']}
+                rows={[...page, editableRow]}
+                widths={[50, 50]}
+              />
+            </CarouselItem>
+          )
+        })}
+      </Carousel>
     </LogWrapper>
   )
 }

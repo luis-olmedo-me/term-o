ba037c730d797258f42893c74bcf59c74e590907
olmedoluis <olmedoluis012@gmail.com>
2022-09-30 00:45:33 -0300

    feat: restore changes

--- src/libs/commander/modules/NodeTree/NodeTree.styles.js
+++ src/libs/commander/modules/NodeTree/NodeTree.styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const TreeWrapper = styled.div`
   overflow: scroll;
   white-space: nowrap;
-  max-height: 535px;
+  max-height: 537px;
   font-weight: bold;
   border-radius: 3px 0 0 3px;
   scroll-snap-type: block;

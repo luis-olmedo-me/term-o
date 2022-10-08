bbaa54c6c92869128ef7dc17330cecb75d5ab4e4
olmedoluis <olmedoluis012@gmail.com>
2022-10-08 20:30:35 -0300

    feat: Reduce height of header

--- src/modules/components/Table/Table.styles.js
+++ src/modules/components/Table/Table.styles.js
@@ -12,7 +12,7 @@ export const TableRow = styled.div`
   background-color: ${(props) => (props.header ? '#fafafa' : 'transparent')};
   color: ${(props) => (props.header ? '#222' : '#fafafa')};
   font-weight: ${(props) => (props.header ? 'bold' : 'normal')};
-  padding: ${(props) => (props.header ? '5px 20px' : '0 20px')};
+  padding: ${(props) => (props.header ? '3px 20px' : '0 20px')};
   border-radius: ${(props) => (props.header ? '5px' : '0')};
   text-align: ${(props) => (props.header ? 'center' : 'left')};
   font-size: 1em;

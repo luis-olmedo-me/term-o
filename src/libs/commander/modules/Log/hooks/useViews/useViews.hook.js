a12f585e9f7962e734a3ad2ca5299153b5b67e47
olmedoluis <olmedoluis012@gmail.com>
2022-10-09 01:07:11 -0300

    feat: create actions

--- src/libs/commander/modules/Log/hooks/useViews/useViews.hook.js
+++ src/libs/commander/modules/Log/hooks/useViews/useViews.hook.js
@@ -1,3 +1,15 @@
-export const useViews = ({ views }) => {
-  return
+import { useState } from 'react'
+
+export const useViews = ({ views, defaultView }) => {
+  const [itemInView, setItemInView] = useState(defaultView)
+
+  const actions = views.map((view) => {
+    return {
+      id: `head-to-${view.id}`,
+      text: view.text,
+      onClick: () => setItemInView(view.id)
+    }
+  })
+
+  return { itemInView, changeView: setItemInView, actions }
 }

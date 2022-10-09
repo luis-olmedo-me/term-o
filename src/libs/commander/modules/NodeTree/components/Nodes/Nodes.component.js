2570ec5f7a24e200f1cbfe51aee94e7e9fc82988
olmedoluis <olmedoluis012@gmail.com>
2022-10-09 10:14:28 -0300

    feat: Remove useless actions

--- src/libs/commander/modules/NodeTree/components/Nodes/Nodes.component.js
+++ src/libs/commander/modules/NodeTree/components/Nodes/Nodes.component.js
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { useEffect, useRef, useState } from 'react'
 import { ElementLabel } from '../ElementLabel/ElementLabel.component'
 import {
   TagWrapper,
@@ -10,7 +9,6 @@ import {
 } from './Nodes.styles'
 import { withOverlayContext } from 'modules/components/Overlay/Overlay.hoc'
 import { isElementHidden } from '../../../../components/CommandDom/CommandDom.helpers'
-import { createXPathFromElement } from '../../../List/components/Element/Element.helpers'
 
 const supportedNodeTypes = [Node.ELEMENT_NODE, Node.TEXT_NODE]
 
@@ -27,14 +25,6 @@ const NodesWithoutContext = ({
   setEditingElement,
   onStylesOptionClick
 }) => {
-  const nodeWrapperRef = useRef(null)
-  const nodeData = useRef({
-    parent: node?.parentElement,
-    nextSibling: node?.nextElementSibling
-  })
-
-  const [isDead, setIsDead] = useState(false)
-
   const childNodes = [...node.childNodes].filter(
     (node) => !supportedNodeTypes.includes(node)
   )
@@ -52,67 +42,8 @@ const NodesWithoutContext = ({
         : [...openNodes, node]
     )
   }
-  const handleChangeRoot = () => {
-    handleRootChange(node)
-  }
-
-  const handleCopy = () => {
-    const xPath = createXPathFromElement(node)
-
-    navigator.clipboard.writeText(xPath.includes(' ') ? `"${xPath}"` : xPath)
-  }
-
-  const handleLifeToggle = () => {
-    if (isDead) {
-      const { parent, nextSibling } = nodeData.current
-
-      setIsDead(false)
-
-      parent.insertBefore(node, nextSibling)
-    } else {
-      setHighlitedElement(null)
-      setIsDead(true)
-      node.remove()
-    }
-  }
 
   const actions = [
-    {
-      id: 'group',
-      items: [
-        {
-          id: 'copy-node',
-          title: 'Copy xpath',
-          onClick: handleCopy,
-          Component: '📋'
-        },
-        {
-          id: 'change-root',
-          title: 'Change root',
-          onClick: handleChangeRoot,
-          disabled: !hasNodes,
-          Component: '🌱'
-        },
-        {
-          id: 'edit-element',
-          title: 'Edit element',
-          onClick: () => setEditingElement({ element: node }),
-          Component: '✏️'
-        },
-        {
-          id: 'edit-styles',
-          title: 'Edit styles',
-          onClick: (event) => onStylesOptionClick?.({ event, element: node }),
-          Component: '✂️'
-        },
-        {
-          id: 'life-toggle-element',
-          title: isDead ? 'Restore element' : 'Delete element',
-          onClick: handleLifeToggle,
-          Component: isDead ? '↩️' : '💀'
-        }
-      ]
-    },
     {
       id: 'toggle-element',
       onClick: handleToggleElement,
@@ -129,7 +60,7 @@ const NodesWithoutContext = ({
       const isHidden = isElementHidden(node)
 
       return (
-        <GapNodesWrapper ref={nodeWrapperRef} isRoot={isNodeRoot}>
+        <GapNodesWrapper isRoot={isNodeRoot}>
           <ElementLabel
             element={node}
             Wrapper={TagWrapper}
@@ -173,7 +104,7 @@ const NodesWithoutContext = ({
 
       return (
         node.textContent.trim() && (
-          <GapNodesWrapper ref={nodeWrapperRef}>
+          <GapNodesWrapper>
             <TagWrapper
               onClick={handleElementClick}
               isNodeObjetive={isNodeObjetive}

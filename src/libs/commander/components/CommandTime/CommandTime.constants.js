aae741d954bb08c8fc9b9837fc38c07d517ec0b7
olmedoluis <olmedoluis012@gmail.com>
2022-10-29 14:13:17 -0300

    feat: Add props

--- src/libs/commander/components/CommandTime/CommandTime.constants.js
+++ src/libs/commander/components/CommandTime/CommandTime.constants.js
@@ -4,7 +4,22 @@ import { optionTypes } from '../../constants/commands.constants'
 import { CommandTime } from './CommandTime.component'
 
 export const timeConfig = {
-  props: {},
+  props: {
+    delay: {
+      key: 'delay',
+      description: 'Set a delay time before running the next command.',
+      type: optionTypes.NUMBER,
+      defaultValue: 0,
+      alias: 'd'
+    },
+    interval: {
+      key: 'interval',
+      description: 'Set a interval time before running the next command.',
+      type: optionTypes.NUMBER,
+      defaultValue: 0,
+      alias: 'i'
+    }
+  },
   output: (props) => <CommandTime key={props.id} {...props} />
 }
 

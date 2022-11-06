6e0cd1fba5af4e60cc8b9e82e09f172b7d9cfa60
olmedoluis <olmedoluis012@gmail.com>
2022-11-06 15:55:38 -0300

    feat: Create index file

---
+++ src/modules/icons/index.js
@@ -0,0 +1,33 @@
+import { Chevron } from './Chevron.icon'
+import { Copy } from './Copy.icon'
+import { DoubleChevron } from './DoubleChevron.icon'
+import { Error } from './Error.icon'
+import { Eye } from './Eye.icon'
+import { Flag } from './Flag.icon'
+import { Home } from './Home.icon'
+import { Info } from './Info.icon'
+import { Loader } from './Loader.icon'
+import { Logo } from './Logo.icon'
+import { Palette } from './Palette.icon'
+import { Pencil } from './Pencil.icon'
+import { Skull } from './Skull.icon'
+import { Tag } from './Tag.icon'
+import { Tick } from './Tick.icon'
+
+export {
+  Chevron,
+  Copy,
+  DoubleChevron,
+  Error,
+  Eye,
+  Flag,
+  Home,
+  Info,
+  Loader,
+  Logo,
+  Palette,
+  Pencil,
+  Skull,
+  Tag,
+  Tick
+}

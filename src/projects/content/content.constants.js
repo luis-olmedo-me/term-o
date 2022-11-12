0f7a885bd63513f0e3d77dbea896823543c7e6c9
olmedoluis <olmedoluis012@gmail.com>
2022-11-12 12:57:58 -0300

    feat: Apply fonts

--- src/projects/content/content.constants.js
+++ src/projects/content/content.constants.js
@@ -1,7 +1,3 @@
-const appContainer = document.createElement('div')
-const styleContainer = document.createElement('div')
-styleContainer.appendChild(appContainer)
-
 const root = document.createElement('div')
 root.id = 'term-o-root'
 root.dataset.isOpen = 'false'
@@ -13,8 +9,7 @@ termoBody.setAttribute('id', 'term-o-body')
 termoBody.append(root)
 
 const shadow = root.attachShadow({ mode: 'open' })
-shadow.appendChild(styleContainer)
 
 document.documentElement.append(termoBody)
 
-export { appContainer, styleContainer }
+export { shadow }
--- src/projects/content/content.js
+++ src/projects/content/content.js
@@ -3,21 +3,23 @@ import * as React from 'preact'
 import { OverlayProvider } from 'modules/components/Overlay/Overlay.provider'
 import { PortalProvider } from 'modules/components/Portal/Portal.provider'
 import { StyleSheetManager, ThemeProvider } from 'styled-components'
-import { appContainer, styleContainer } from './content.constants'
+import { appContainer, shadow } from './content.constants'
 import { FontFamilies } from './fonts/Fonts.styles'
 import { Console } from './modules/Console/Console.component'
 
 React.render(
-  <StyleSheetManager target={styleContainer}>
-    <ThemeProvider theme={{}}>
-      <FontFamilies />
+  <>
+    <FontFamilies />
 
-      <PortalProvider>
-        <OverlayProvider>
-          <Console />
-        </OverlayProvider>
-      </PortalProvider>
-    </ThemeProvider>
-  </StyleSheetManager>,
-  appContainer
+    <StyleSheetManager target={shadow}>
+      <ThemeProvider theme={{}}>
+        <PortalProvider>
+          <OverlayProvider>
+            <Console />
+          </OverlayProvider>
+        </PortalProvider>
+      </ThemeProvider>
+    </StyleSheetManager>
+  </>,
+  shadow
 )

86036a761b80ad8f87e20dc9bfd3836e3bf4871e
olmedoluis <olmedoluis012@gmail.com>
2022-11-13 02:50:53 -0300

    feat: Implement new scheme on help command

--- src/libs/commander/components/CommandHelp/CommandHelp.component.js
+++ src/libs/commander/components/CommandHelp/CommandHelp.component.js
@@ -2,86 +2,92 @@ import * as React from 'preact'
 import { useCallback, useEffect, useState } from 'preact/hooks'
 import { removeDuplicatedFromArray } from 'src/helpers/utils.helpers.js'
 import { consoleCommands } from '../../commander.constants'
+import { commanderMessages } from '../../commander.messages.js'
 import { parameterTypes } from '../../constants/commands.constants'
-import { Log } from '../../modules/Log'
+import { Log, useMessageLog } from '../../modules/Log'
 import { helpActionTypes } from './CommandHelp.constants'
-import {
-  getActionType,
-  getMessagesFromCommandsToCheck
-} from './CommandHelp.helpers'
+import { getActionType, getMessagesFromCommandsToCheck } from './CommandHelp.helpers'
 import { ListItem, UnsortedList } from './CommandHelp.styles'
 
 export const CommandHelp = ({ props, terminal: { command, finish } }) => {
   const actionType = getActionType(props)
-  const { about } = props
   const [localMessages, setLocalMessages] = useState([])
 
-  const handleHelp = useCallback(
-    (commands) => {
-      const commandsNoRepeated = removeDuplicatedFromArray(commands)
+  const { log: messageLog, setMessage } = useMessageLog()
 
-      const messagesForHelp = getMessagesFromCommandsToCheck({
-        commands: commandsNoRepeated
-      })
+  const handleHelp = useCallback(
+    (commands = props.about) => {
+      const commandsNoRepeated = removeDuplicatedFromArray(commands)
+      const messagesForHelp = getMessagesFromCommandsToCheck({ commands: commandsNoRepeated })
 
       setLocalMessages(messagesForHelp)
-      finish()
     },
-    [finish]
+    [props]
   )
 
   const handleAllHelp = useCallback(() => {
     const allCommandNames = Object.keys(consoleCommands)
 
     handleHelp(allCommandNames)
-    finish()
-  }, [handleHelp, finish])
+  }, [handleHelp])
+
+  const doAction = useCallback(async () => {
+    switch (actionType) {
+      case helpActionTypes.HELP:
+        return handleHelp()
+
+      case helpActionTypes.NONE:
+        return handleAllHelp()
+    }
+  }, [actionType, handleHelp, handleAllHelp])
 
   useEffect(
     function handleActionType() {
-      switch (actionType) {
-        case helpActionTypes.HELP:
-          handleHelp(about)
-          break
-
-        case helpActionTypes.NONE: {
-          handleAllHelp()
-          break
-        }
+      const handleError = error => {
+        setMessage(commanderMessages[error.message] || commanderMessages.unexpectedError)
+        finish({ break: true })
       }
+
+      doAction()
+        .then(finish)
+        .catch(handleError)
     },
-    [actionType, handleHelp, about]
+    [doAction, setMessage, finish]
   )
 
   return (
     <>
       <Log variant={parameterTypes.COMMAND}>{command}</Log>
 
-      <Log variant={parameterTypes.HELP} hasScroll hasShadow>
-        {localMessages.map(({ id, title, items }) => {
-          return (
-            <UnsortedList key={id}>
-              <ListItem>
-                {title}
+      {messageLog && <Log variant={messageLog.type}>{messageLog.message}</Log>}
 
-                <UnsortedList>
-                  {items.map(({ id, title, description }) => {
-                    return (
-                      <ListItem key={id}>
-                        {title}
+      {!messageLog && (
+        <Log variant={parameterTypes.HELP} hasScroll hasShadow>
+          {localMessages.map(({ id, title, items }) => {
+            return (
+              <UnsortedList key={id}>
+                <ListItem>
+                  {title}
 
-                        <UnsortedList>
-                          <ListItem>{description}</ListItem>
-                        </UnsortedList>
-                      </ListItem>
-                    )
-                  })}
-                </UnsortedList>
-              </ListItem>
-            </UnsortedList>
-          )
-        })}
-      </Log>
+                  <UnsortedList>
+                    {items.map(({ id, title, description }) => {
+                      return (
+                        <ListItem key={id}>
+                          {title}
+
+                          <UnsortedList>
+                            <ListItem>{description}</ListItem>
+                          </UnsortedList>
+                        </ListItem>
+                      )
+                    })}
+                  </UnsortedList>
+                </ListItem>
+              </UnsortedList>
+            )
+          })}
+        </Log>
+      )}
     </>
   )
 }

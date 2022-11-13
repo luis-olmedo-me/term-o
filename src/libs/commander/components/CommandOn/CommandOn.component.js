9541e1557f9d1f2b39802c1a8fd6d1e2c133d51e
olmedoluis <olmedoluis012@gmail.com>
2022-11-13 03:15:12 -0300

    feat: Use new scheme on ON command

--- src/libs/commander/components/CommandOn/CommandOn.component.js
+++ src/libs/commander/components/CommandOn/CommandOn.component.js
@@ -1,65 +1,63 @@
 import * as React from 'preact'
+
 import { useCallback, useEffect } from 'preact/hooks'
 import { addPageEvents } from 'src/helpers/event.helpers.js'
 import { generateUUID } from 'src/helpers/utils.helpers'
-import {
-  customPageEventNames,
-  parameterTypes
-} from '../../constants/commands.constants'
+import { customPageEventNames, parameterTypes } from '../../constants/commands.constants'
 import { Log, useMessageLog } from '../../modules/Log'
 import { onActionTypes } from './CommandOn.constants'
 import { checkIfRegExpIsValid, getActionType } from './CommandOn.helpers'
 import { onMessages } from './CommandOn.messages'
 
 export const CommandOn = ({ props, terminal: { command, finish } }) => {
-  const { url, run, event } = props
-
   const actionType = getActionType(props)
 
   const { log: messageLog, setMessage } = useMessageLog()
 
-  const handleAddEvent = useCallback(() => {
-    if (!run.length) return setMessage(onMessages.missingCommand)
+  const handleAddEvent = useCallback(async () => {
+    const areURLsValid = props.url.every(checkIfRegExpIsValid)
 
-    const areURLsValid = url.every(checkIfRegExpIsValid)
+    if (!props.run.length) throw new Error('missingCommand')
+    if (!areURLsValid) throw new Error('invalidURLRegularExpressions')
+    if (props.event && !customPageEventNames.includes(props.event))
+      throw new Error('invalidEventType')
 
-    if (!areURLsValid) {
-      return setMessage(onMessages.invalidURLRegularExpressions)
-    }
-
-    if (event && !customPageEventNames.includes(event)) {
-      return setMessage(onMessages.invalidEventType)
-    }
-
-    const urlForEvent = url.join('|')
-    const commandsToRun = run.map((commandToRun) => {
+    const urlForEvent = props.url.join('|')
+    const commandsToRun = props.run.map(commandToRun => {
       return {
         id: generateUUID(),
         command: commandToRun,
         url: urlForEvent,
-        event
+        event: props.event
       }
     })
 
-    addPageEvents(commandsToRun)
-      .catch(() => setMessage(onMessages.unexpectedError))
-      .then(() => setMessage(onMessages.eventSaveSuccess))
-      .then(() => finish())
-  }, [url, run, setMessage, finish])
+    await addPageEvents(commandsToRun)
+    setMessage(onMessages.eventSaveSuccess)
+  }, [props, setMessage])
+
+  const doAction = useCallback(async () => {
+    switch (actionType) {
+      case onActionTypes.ADD_EVENT:
+        return await handleAddEvent()
+
+      case onActionTypes.NONE:
+        throw new Error('unexpectedError')
+    }
+  }, [actionType, handleAddEvent])
 
   useEffect(
     function handleActionType() {
-      switch (actionType) {
-        case onActionTypes.ADD_EVENT:
-          handleAddEvent()
-          break
-
-        case onActionTypes.NONE:
-          setMessage(onMessages.unexpectedError)
-          break
+      const handleError = error => {
+        setMessage(onMessages[error.message] || onMessages.unexpectedError)
+        finish({ break: true })
       }
+
+      doAction()
+        .then(finish)
+        .catch(handleError)
     },
-    [actionType, handleAddEvent, setMessage]
+    [doAction, setMessage, finish]
   )
 
   return (

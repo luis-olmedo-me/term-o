205f50ca54835b3d22a2d943afbcff56ee7d91be
olmedoluis <olmedoluis012@gmail.com>
2022-11-21 23:12:34 -0300

    feat: Implement new log structure in time command

--- src/libs/commander/components/CommandTime/CommandTime.component.js
+++ src/libs/commander/components/CommandTime/CommandTime.component.js
@@ -1,8 +1,9 @@
 import * as React from 'preact'
 import { useCallback, useEffect, useState } from 'preact/hooks'
+
 import { parameterTypes } from '../../constants/commands.constants'
 import { Clock } from '../../modules/Clock/Clock.component'
-import { Log, useMessageLog } from '../../modules/Log'
+import { LogCard, LogContainer, useMessageLog } from '../../modules/Log'
 import { timeActionTypes } from './CommandTime.constants'
 import { getActionType } from './CommandTime.helpers'
 import { timeMessages } from './CommandTime.messages'
@@ -50,16 +51,18 @@ export const CommandTime = ({ props, terminal: { command, finish } }) => {
   const hasDelay = Boolean(delay)
 
   return (
-    <>
-      <Log variant={parameterTypes.COMMAND}>{command}</Log>
-
-      {messageLog && <Log variant={messageLog.type}>{messageLog.message}</Log>}
+    <LogContainer>
+      {messageLog && (
+        <LogCard variant={messageLog.type} command={command}>
+          {messageLog.message}
+        </LogCard>
+      )}
 
       {hasDelay && !messageLog && (
-        <Log variant={parameterTypes.TABLE}>
+        <LogCard variant={parameterTypes.TABLE} command={command}>
           <Clock time={delay} onFinish={handleFinishTimer} />
-        </Log>
+        </LogCard>
       )}
-    </>
+    </LogContainer>
   )
 }

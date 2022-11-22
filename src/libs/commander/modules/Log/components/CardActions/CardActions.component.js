81607564c9f2e349a696075121d779b2a28d4b88
olmedoluis <olmedoluis012@gmail.com>
2022-11-21 21:44:03 -0300

    feat: Create actions

---
+++ src/libs/commander/modules/Log/components/CardActions/CardActions.component.js
@@ -0,0 +1,43 @@
+import * as React from 'preact'
+import { DateTimePickerAction } from '../DateTimePickerAction/DateTimePickerAction.component'
+import { Action, Groups } from './CardActions.styles'
+
+export const CardActions = ({ actions }) => {
+  return (
+    <Groups>
+      {actions.map(
+        ({ id, text, onClick, onChange, disabled, selected, invalid, type, label, title }) => {
+          const commonProps = {
+            key: id,
+            className: `
+                ${selected ? 'selected' : ''}
+                ${disabled ? 'disabled' : ''}
+                ${invalid ? 'invalid' : ''}
+              `,
+            disabled,
+            title
+          }
+
+          switch (type) {
+            case 'datetime':
+              return (
+                <DateTimePickerAction
+                  {...commonProps}
+                  onChange={onChange}
+                  label={label}
+                  value={text}
+                />
+              )
+
+            default:
+              return (
+                <Action {...commonProps} onClick={onClick} hasIcon={typeof text !== 'string'}>
+                  {text}
+                </Action>
+              )
+          }
+        }
+      )}
+    </Groups>
+  )
+}

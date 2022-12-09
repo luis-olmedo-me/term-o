dbe1cd348a4dabb826f4e67e0025170134121408
olmedoluis <olmedoluis012@gmail.com>
2022-12-09 01:39:58 -0300

    refactor: Isolate constants

--- src/libs/commander/modules/Log/components/AttributeEditionLog/AttributeEditionLog.component.js
+++ src/libs/commander/modules/Log/components/AttributeEditionLog/AttributeEditionLog.component.js
@@ -1,42 +1,9 @@
 import * as React from 'preact'
-import { useState } from 'preact/hooks'
 
-import { Carousel, CarouselItem } from '@modules/components/Carousel'
-import { Table } from '@modules/components/Table/Table.component'
 import { getAttributes } from '@src/helpers/dom.helpers'
-import { parameterTypes } from '../../../../constants/commands.constants'
-import usePaginationActions from '../../hooks/usePaginationActions'
-import AttributeInput, { Input } from '../AttributeInput'
-import LogCard from '../LogCard'
 import TableLog from '../TableLog'
-import { attributeTableOptions } from './AttributeEditionLog.constants'
+import { attributeTableOptions, tableComponents } from './AttributeEditionLog.constants'
 import { turnAttributesIntoTableItems } from './AttributeEditionLog.helpers'
-
-const components = {
-  attrNameEditor: ({ value, row }) => {
-    const handleNameEnter = newName => {
-      row.element.setAttribute(newName, row.attributeValue)
-      row.element.removeAttribute(value)
-    }
-
-    return (
-      <AttributeInput placeholder="Attribute name" onEnter={handleNameEnter} defaultValue={value} />
-    )
-  },
-  attrValueEditor: ({ value, row }) => {
-    const handleValueEnter = newValue => {
-      row.element.setAttribute(row.attributeName, newValue)
-    }
-
-    return (
-      <AttributeInput
-        placeholder="Attribute value"
-        onEnter={handleValueEnter}
-        defaultValue={value}
-      />
-    )
-  }
-}
 
 export const AttributeEditionLog = ({ element, leftOptions = [], rightOptions = [], command }) => {
   const attributes = getAttributes(element)
@@ -51,7 +18,7 @@ export const AttributeEditionLog = ({ element, leftOptions = [], rightOptions = 
       hasSelection={false}
       leftActions={leftOptions}
       rightActions={rightOptions}
-      components={components}
+      components={tableComponents}
     />
   )
 }
--- src/libs/commander/modules/Log/components/AttributeEditionLog/AttributeEditionLog.constants.js
+++ src/libs/commander/modules/Log/components/AttributeEditionLog/AttributeEditionLog.constants.js
@@ -1,3 +1,6 @@
+import * as React from 'preact'
+import AttributeInput from '../AttributeInput'
+
 const attributeHeaderIds = {
   ATTRIBUTE: 'attribute',
   VALUE: 'value'
@@ -24,4 +27,30 @@ export const attributeTableOptions = {
       cellRenderer: 'attrValueEditor'
     }
   ]
+}
+
+export const tableComponents = {
+  attrNameEditor: ({ value, row }) => {
+    const handleNameEnter = newName => {
+      row.element.setAttribute(newName, row.attributeValue)
+      row.element.removeAttribute(value)
+    }
+
+    return (
+      <AttributeInput placeholder="Attribute name" onEnter={handleNameEnter} defaultValue={value} />
+    )
+  },
+  attrValueEditor: ({ value, row }) => {
+    const handleValueEnter = newValue => {
+      row.element.setAttribute(row.attributeName, newValue)
+    }
+
+    return (
+      <AttributeInput
+        placeholder="Attribute value"
+        onEnter={handleValueEnter}
+        defaultValue={value}
+      />
+    )
+  }
 }

8120c9fb0ef1cddee077e51a9215ba7d3ef5ab8b
olmedoluis <olmedoluis012@gmail.com>
2024-02-21 00:42:49 -0300

    feat: Dont allow to disable all columns

--- src/libs/commander/modules/Log/hooks/useTableConfig/useTableConfig.helpers.js
+++ src/libs/commander/modules/Log/hooks/useTableConfig/useTableConfig.helpers.js
@@ -12,27 +12,33 @@ export const createTableLogActionsCreatorPerRow = ({
 }) => {
   const firstColumn = columns.at(0)
   const lastColumn = columns.at(-1)
+  const isAboutToUnselectAll = activeColumnIds.length === 1
 
-  return row => [
-    {
-      id: tableConfigActionIds.MOVE_UP,
-      title: 'Move up',
-      onClick: onMoveUpClick,
-      Component: <Chevron direction="top" />,
-      disabled: row.id === lastColumn.id
-    },
-    {
-      id: tableConfigActionIds.MOVE_DOWN,
-      title: 'Move down',
-      onClick: onMoveDownClick,
-      Component: <Chevron direction="bottom" />,
-      disabled: row.id === firstColumn.id
-    },
-    {
-      id: tableConfigActionIds.STATE,
-      title: 'State',
-      onClick: onToggleColumn,
-      Component: <Live active={activeColumnIds.includes(row.id)} />
-    }
-  ]
+  return row => {
+    const isActive = activeColumnIds.includes(row.id)
+
+    return [
+      {
+        id: tableConfigActionIds.MOVE_UP,
+        title: 'Move up',
+        onClick: onMoveUpClick,
+        Component: <Chevron direction="top" />,
+        disabled: row.id === lastColumn.id
+      },
+      {
+        id: tableConfigActionIds.MOVE_DOWN,
+        title: 'Move down',
+        onClick: onMoveDownClick,
+        Component: <Chevron direction="bottom" />,
+        disabled: row.id === firstColumn.id
+      },
+      {
+        id: tableConfigActionIds.STATE,
+        title: 'State',
+        onClick: onToggleColumn,
+        Component: <Live active={isActive} />,
+        disabled: isActive && isAboutToUnselectAll
+      }
+    ]
+  }
 }

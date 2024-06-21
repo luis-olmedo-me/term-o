039847aabf6e1432550a3acc03e28224b331bb97
LuisMi <olmedoluis012@gmail.com>
2024-06-21 18:25:40 -0300

    feat: Set up global reset

--- src-new/scripts/sidepanel/sidepanel.js
+++ src-new/scripts/sidepanel/sidepanel.js
@@ -3,9 +3,12 @@ import * as React from 'preact'
 import { defaultTheme } from '@src/theme/theme.colors'
 import { ThemeProvider } from 'styled-components'
 import Terminal from './modules/Terminal'
+import GlobalStyle from './styles/Global.styles'
 
 React.render(
   <ThemeProvider theme={defaultTheme}>
+    <GlobalStyle />
+
     <Terminal />
   </ThemeProvider>,
   document.getElementById('root')

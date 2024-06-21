84132f056f1b7d2ab23faef30cfdd202c40b6703
LuisMi <olmedoluis012@gmail.com>
2024-06-21 18:58:49 -0300

    feat: Hanlde value

--- src-new/scripts/sidepanel/modules/Terminal/Terminal.module.js
+++ src-new/scripts/sidepanel/modules/Terminal/Terminal.module.js
@@ -3,15 +3,19 @@ import { Input } from '../../components/Input/Input.component'
 import Logger from '../../components/Logger'
 
 export const Terminal = () => {
-  const handleChange = () => {
-    console.log('change')
+  const [value, setValue] = useState('')
+
+  const handleChange = event => {
+    const value = event.target.value
+
+    setValue(value)
   }
 
   return (
     <div>
       <Logger logs={[]} />
 
-      <Input type="text" onChange={handleChange} />
+      <Input type="text" onChange={handleChange} value={value} />
     </div>
   )
 }

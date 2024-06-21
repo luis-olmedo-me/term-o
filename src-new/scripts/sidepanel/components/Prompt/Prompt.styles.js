8d6ea18663ca9f09bedbb076476768146abaa51e
LuisMi <olmedoluis012@gmail.com>
2024-06-21 20:40:41 -0300

    feat: Create prompt component

---
+++ src-new/scripts/sidepanel/components/Prompt/Prompt.styles.js
@@ -0,0 +1,32 @@
+import { theme as t } from '@src/theme/theme.helpers'
+import styled from 'styled-components'
+
+export const Prompt = styled.div`
+  font-family: ${t('font.primary')};
+  width: 100%;
+  box-sizing: border-box;
+  white-space: nowrap;
+  font-size: ${t('font-size.50')};
+  border: none;
+  display: block;
+  caret-color: ${t('green.50')};
+  line-height: ${t('line-height.300')};
+  background-color: ${t('grey.900')};
+  color: ${t('grey.50')};
+  letter-spacing: ${t('space.50')};
+
+  &::selection {
+    color: ${t('green.A200')};
+    background-color: ${t('grey.800')};
+  }
+`
+
+export const PromptWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${t('space.400')};
+  background-color: ${t('grey.900')};
+  color: ${t('grey.50')};
+  padding: ${t('space.300')} ${t('space.500')};
+  cursor: text;
+`

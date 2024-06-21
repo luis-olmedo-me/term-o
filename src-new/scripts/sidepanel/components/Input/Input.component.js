b45b54a227ed6c4e6b69e5cfded59cf3c445409a
LuisMi <olmedoluis012@gmail.com>
2024-06-21 19:53:03 -0300

    feat: Focus on input at first time

--- src-new/scripts/sidepanel/components/Input/Input.component.js
+++ src-new/scripts/sidepanel/components/Input/Input.component.js
@@ -2,12 +2,13 @@ import * as React from 'preact'
 
 import * as S from './Input.styles'
 
-export const Input = ({ onChange, onKeyDown, placeholder, value, prefix }) => {
+export const Input = ({ onChange, onKeyDown, placeholder, value, prefix, inputRef }) => {
   return (
     <S.InputWrapper>
       <span>{prefix}</span>
 
       <S.Input
+        ref={inputRef}
         spellCheck="false"
         type="text"
         value={value}
--- src-new/scripts/sidepanel/modules/Terminal/Terminal.module.js
+++ src-new/scripts/sidepanel/modules/Terminal/Terminal.module.js
@@ -1,5 +1,5 @@
 import * as React from 'preact'
-import { useState } from 'preact/hooks'
+import { useEffect, useRef, useState } from 'preact/hooks'
 
 import Input from '../../components/Input'
 import Logger from '../../components/Logger'
@@ -7,6 +7,7 @@ import * as S from './Terminal.styles'
 
 export const Terminal = () => {
   const [value, setValue] = useState('')
+  const inputRef = useRef(null)
 
   const handleChange = event => {
     const value = event.target.value
@@ -14,11 +15,19 @@ export const Terminal = () => {
     setValue(value)
   }
 
+  const focusOnInput = () => {
+    inputRef.current.focus()
+  }
+
+  useEffect(function focusOnInputAtFirstTime() {
+    focusOnInput()
+  }, [])
+
   return (
-    <S.TerminalWrapper>
+    <S.TerminalWrapper onClick={focusOnInput}>
       <Logger logs={['test', 'test1']} />
 
-      <Input type="text" onChange={handleChange} value={value} prefix="$" />
+      <Input type="text" onChange={handleChange} value={value} prefix="$" inputRef={inputRef} />
     </S.TerminalWrapper>
   )
 }

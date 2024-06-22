40b1cb818451138da7b4b5b8fd4d4a178a6d8341
LuisMi <olmedoluis012@gmail.com>
2024-06-22 02:21:00 -0300

    feat: Set up quick example

--- src-new/scripts/sidepanel/libs/CommandParser/commands/Command.service.js
+++ src-new/scripts/sidepanel/libs/CommandParser/commands/Command.service.js
@@ -1,10 +1,23 @@
+const defaultValues = {
+  string: '',
+  boolean: false,
+  number: 0,
+  array: [],
+  object: {},
+  none: null
+}
+
 export class Command {
   constructor() {
     this.propTypes = {}
+    this.defaults = {}
   }
 
-  expectProp(name, type) {
+  expectProp({ name, type, defaultValue }) {
+    const value = defaultValue || defaultValues[type] || defaultValues.none
+
     this.propTypes = { ...this.propTypes, [name]: type }
+    this.defaults = { ...this.defaults, [name]: value }
 
     return this
   }
--- src-new/scripts/sidepanel/libs/CommandParser/commands/clear/clear.command.js
+++ src-new/scripts/sidepanel/libs/CommandParser/commands/clear/clear.command.js
@@ -1,5 +1,8 @@
 import { Command } from '../Command.service'
 
-export class Clear extends Command {
-  constructor() {}
-}
+export const clear = new Command()
+  .expectProp({ name: 'test', type: 'string' })
+  .expectProp({ name: 'test-1', type: 'boolean' })
+  .expectProp({ name: 'test-2', type: 'number' })
+  .expectProp({ name: 'test-3', type: 'array' })
+  .expectProp({ name: 'test-4', type: 'object' })

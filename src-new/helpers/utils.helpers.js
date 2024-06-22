756d8532b44fd953e34ea7740090b765ad330448
LuisMi <olmedoluis012@gmail.com>
2024-06-21 22:06:32 -0300

    feat: Create uuid

--- src-new/helpers/utils.helpers.js
+++ src-new/helpers/utils.helpers.js
@@ -7,3 +7,14 @@ export const debounce = (callback, wait) => {
     timerId = setTimeout(() => callback(...args), wait)
   }
 }
+
+export const createUUIDv4 = () => {
+  const uuidTemplate = '10000000-1000-4000-8000-100000000000'
+
+  return uuidTemplate.replace(/[018]/g, character => {
+    const randomValue = crypto.getRandomValues(new Uint8Array(1))[0]
+    const newValue = character === '0' ? randomValue & 0x0f : (randomValue & 0x3) | 0x8
+
+    return newValue.toString(16)
+  })
+}
--- src-new/scripts/sidepanel/libs/Log/Log.service.js
+++ src-new/scripts/sidepanel/libs/Log/Log.service.js
@@ -1,5 +1,8 @@
+import { createUUIDv4 } from 'helpers/utils.helpers'
+
 class Log {
   constructor(command) {
+    this.id = createUUIDv4()
     this.command = command
   }
 }

e52837c4b2bf16bab5185edcfbfd5899bf64045e
LuisMi <olmedoluis012@gmail.com>
2024-06-24 21:43:43 -0300

    feat: Show example

--- src-new/scripts/sidepanel/command-handlers/dom/dom.handler.js
+++ src-new/scripts/sidepanel/command-handlers/dom/dom.handler.js
@@ -1,3 +1,7 @@
 export const handleDOM = command => {
-  command.update('Searching for DOM elements')
+  command.update('Searching for DOM elements.')
+
+  setTimeout(() => {
+    command.update('Searching for DOM elements twice.')
+  }, 1000)
 }

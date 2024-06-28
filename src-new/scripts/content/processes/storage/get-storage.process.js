625a3432c13ee6c5e2a9679a49835a987406e453
LuisMi <olmedoluis012@gmail.com>
2024-06-27 22:38:18 -0300

    feat: Silent quotes

--- src-new/scripts/content/processes/storage/get-storage.process.js
+++ src-new/scripts/content/processes/storage/get-storage.process.js
@@ -8,12 +8,20 @@ export const getCookies = () => {
   }, {})
 }
 
+function silentQuotes(storageObject) {
+  return Object.keys(storageObject).reduce((newStorageObject, key) => {
+    newStorageObject[key] = storageObject[key].replace(/"/g, '\\"')
+
+    return newStorageObject
+  }, {})
+}
+
 export const getStorage = (resolve, data) => {
   let storages = {}
 
-  if (data.includeLocal) storages = { ...storages, local: { ...window.localStorage } }
-  if (data.includeSession) storages = { ...storages, session: { ...window.localStorage } }
-  if (data.includeCookies) storages = { ...storages, cookies: getCookies() }
+  if (data.includeLocal) storages = { ...storages, local: silentQuotes(window.localStorage) }
+  if (data.includeSession) storages = { ...storages, session: silentQuotes(window.sessionStorage) }
+  if (data.includeCookies) storages = { ...storages, cookies: silentQuotes(getCookies()) }
 
   resolve(storages)
 }

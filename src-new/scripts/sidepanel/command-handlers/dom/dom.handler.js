fe619e2f56443fb5357866f4ea57a04b06dec43b
LuisMi <olmedoluis012@gmail.com>
2024-06-28 01:17:56 -0300

    feat: Isolate helpers

--- src-new/scripts/sidepanel/command-handlers/dom/dom.handler.js
+++ src-new/scripts/sidepanel/command-handlers/dom/dom.handler.js
@@ -1,15 +1,6 @@
 import { getDOMElements } from '@sidepanel/proccesses/workers'
 import { getColor as C } from '@src/theme/theme.helpers'
-
-function prependCounters(array) {
-  const counters = array.reduce((acc, value) => {
-    const oldValue = acc[value] || 0
-
-    return { ...acc, [value]: oldValue + 1 }
-  }, {})
-
-  return Object.entries(counters).map(([value, count]) => `${count} ${value}`)
-}
+import { prependCounters } from '../command-handlers.helpers'
 
 export const handleDOM = async command => {
   const { tab } = command.data

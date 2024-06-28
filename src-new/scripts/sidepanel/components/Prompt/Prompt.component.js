fffd1cde333ae03f792c9d59f80f563a1ae3b361
LuisMi <63429352+luis-olmedo-me@users.noreply.github.com>
2024-06-28 12:33:32 -0300

    [WBS-180] Show historial index (#173)
    * feat: Remember commands
    
    * feat: Allow to edit
    
    * feat: Show historial index
    
    * feat: Limit index
    
    * feat: Handle enter directly
    
    * feat: Dont repeat logic

--- src-new/scripts/sidepanel/components/Prompt/Prompt.component.js
+++ src-new/scripts/sidepanel/components/Prompt/Prompt.component.js
@@ -5,21 +5,50 @@ import Input from '../Input'
 import { createPSO } from './Prompt.helpers'
 import * as S from './Prompt.styles'
 
+const HISTORIAL_SIZE = 40
+
 export const Prompt = ({ onEnter, inputRef, tab, pso }) => {
   const [value, setValue] = useState('')
+  const [historialIndex, setHistorialIndex] = useState(0)
+  const [historial, setHistorial] = useState([])
 
   const handleChange = event => {
-    const value = event.target.value
+    const targetValue = event.target.value
 
-    setValue(value)
+    setValue(targetValue)
+    if (historialIndex) setHistorialIndex(0)
   }
 
   const handleKeyDown = event => {
     const key = event.key
+    const targetValue = event.target.value
 
     if (key === 'Enter') {
-      onEnter(value)
+      onEnter(targetValue)
+      setHistorial(history => [...history, value].slice(0, HISTORIAL_SIZE))
       setValue('')
+    }
+
+    if (key === 'ArrowUp') {
+      event.preventDefault()
+
+      setHistorialIndex(index => {
+        const newIndex = index - 1
+        const canBeStored = newIndex * -1 <= historial.length
+
+        return canBeStored ? newIndex : index
+      })
+    }
+
+    if (key === 'ArrowDown') {
+      event.preventDefault()
+
+      setHistorialIndex(index => {
+        const newIndex = index + 1
+        const canBeStored = newIndex <= 0
+
+        return canBeStored ? newIndex : index
+      })
     }
   }
 
@@ -29,10 +58,10 @@ export const Prompt = ({ onEnter, inputRef, tab, pso }) => {
 
       <Input
         inputRef={inputRef}
-        value={value}
+        value={historialIndex ? historial.at(historialIndex) : value}
         onChange={handleChange}
         onKeyDown={handleKeyDown}
-        prefix="$"
+        prefix={historialIndex || '$'}
       />
     </S.PromptWrapper>
   )

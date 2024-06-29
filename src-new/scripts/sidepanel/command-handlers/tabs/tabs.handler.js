5b9d46814c47699ffcb4cbe4d8ba3fdcb6d00886
LuisMi <olmedoluis012@gmail.com>
2024-06-29 20:54:47 -0300

    feat: Allow to switch tabs

--- src-new/scripts/sidepanel/command-handlers/tabs/tabs.handler.js
+++ src-new/scripts/sidepanel/command-handlers/tabs/tabs.handler.js
@@ -3,6 +3,16 @@ import { getColor as C } from '@src/theme/theme.helpers'
 export const handleTABS = async command => {
   const P = name => command.props[name]
 
+  if (P`switch`) {
+    const tabIdString = P`switch`.replace(/^T/, '')
+    const tabId = Number(tabIdString)
+    const tab = await chrome.tabs.get(tabId)
+
+    await chrome.tabs.update(tabId, { selected: true })
+    command.update(`You are on ${C`blue`}T${tab.id}${C`foreground`} now.`)
+    command.update(`${C`blue`}W${tab.windowId} T${tab.id} ${C`yellow`}"${tab.title}" "${tab.url}"`)
+  }
+
   if (P`list`) {
     const tabs = await chrome.tabs.query({})
 

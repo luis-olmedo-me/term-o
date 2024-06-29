013360ca3678d7f6538cf2d0d3e0082c3067aef3
LuisMi <olmedoluis012@gmail.com>
2024-06-29 20:15:08 -0300

    feat: Show more tab url

---
+++ src-new/scripts/sidepanel/command-handlers/tabs/tabs.handler.js
@@ -0,0 +1,15 @@
+import { getStorage } from '@sidepanel/proccesses/workers'
+import { getColor as C } from '@src/theme/theme.helpers'
+
+export const handleTABS = async command => {
+  // const { tab } = command.data
+  const P = name => command.props[name]
+
+  if (P`list`) {
+    const tabs = await chrome.tabs.query({})
+
+    tabs.forEach(tab => {
+      command.update(`#W${tab.windowId} #T${tab.id} "${tab.title}" "${tab.url}"`)
+    })
+  }
+}

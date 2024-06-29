74a57bb19bd9319916d532828e383bfe42964859
LuisMi <olmedoluis012@gmail.com>
2024-06-29 20:33:30 -0300

    refactor: Remove useless data

--- src-new/scripts/sidepanel/command-handlers/tabs/tabs.handler.js
+++ src-new/scripts/sidepanel/command-handlers/tabs/tabs.handler.js
@@ -1,8 +1,6 @@
-import { getStorage } from '@sidepanel/proccesses/workers'
 import { getColor as C } from '@src/theme/theme.helpers'
 
 export const handleTABS = async command => {
-  // const { tab } = command.data
   const P = name => command.props[name]
 
   if (P`list`) {

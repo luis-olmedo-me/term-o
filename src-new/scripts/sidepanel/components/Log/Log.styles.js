f9f327d17db1473dd0c5bbe822947106f5f5b2a0
LuisMi <olmedoluis012@gmail.com>
2024-07-01 00:09:42 -0300

    feat: Use Colored Text component

--- src-new/scripts/sidepanel/components/Log/Log.styles.js
+++ src-new/scripts/sidepanel/components/Log/Log.styles.js
@@ -8,12 +8,3 @@ export const LogWrapper = styled.div`
 export const LogItem = styled.p`
   word-break: break-all;
 `
-
-export const ColoredText = styled.span`
-  color: ${({ color }) => color};
-
-  &::selection {
-    color: ${t('colors.color-selection')};
-    background-color: ${t('colors.background-selection')};
-  }
-`

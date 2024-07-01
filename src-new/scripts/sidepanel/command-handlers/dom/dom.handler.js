9e546aca0f150137262e177bd6e00c8e126bc23b
LuisMi <olmedoluis012@gmail.com>
2024-07-01 02:04:01 -0300

    feat: Make dom handler follow the pattern

--- src-new/scripts/sidepanel/command-handlers/dom/dom.handler.js
+++ src-new/scripts/sidepanel/command-handlers/dom/dom.handler.js
@@ -6,25 +6,27 @@ export const handleDOM = async command => {
   const { tab } = command.data
   const P = name => command.props[name]
 
-  command.update('Getting elements.')
-  const elements = await getDOMElements(tab.id, {
-    searchByTag: P`tag`,
-    searchByAttributeName: P`attr`
-  })
+  if (P`get`) {
+    command.update('Getting elements.')
+    const elements = await getDOMElements(tab.id, {
+      searchByTag: P`tag`,
+      searchByAttributeName: P`attr`
+    })
 
-  let textElements = elements.map(({ tagName, attributes }) => {
-    const attrs = Object.entries(attributes)
-      .map(
-        ([name, value]) =>
-          `${C`bright-black`}[${C`cyan`}${name}${C`white`}=${C`yellow`}"${value}"${C`bright-black`}]`
-      )
-      .join(' ')
+    let textElements = elements.map(({ tagName, attributes }) => {
+      const attrs = Object.entries(attributes)
+        .map(
+          ([name, value]) =>
+            `${C`bright-black`}[${C`cyan`}${name}${C`white`}=${C`yellow`}"${value}"${C`bright-black`}]`
+        )
+        .join(' ')
 
-    return attrs ? `${C`red`}${tagName} ${attrs}` : `${C`red`}${tagName}`
-  })
+      return attrs ? `${C`red`}${tagName} ${attrs}` : `${C`red`}${tagName}`
+    })
 
-  if (P`group`) textElements = prependCounters(textElements)
+    if (P`group`) textElements = prependCounters(textElements)
 
-  command.reset()
-  textElements.forEach(element => command.update(element))
+    command.reset()
+    textElements.forEach(element => command.update(element))
+  }
 }

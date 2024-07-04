704626885d76b8ad939d3f88daf8920591626c29
LuisMi <olmedoluis012@gmail.com>
2024-07-04 18:21:02 -0300

    feat: Create handler for alias command

---
+++ src-new/scripts/sidepanel/command-handlers/alias/alias.handler.js
@@ -0,0 +1,14 @@
+import { getColor as C } from '@src/theme/theme.helpers'
+
+export const handleALIAS = async command => {
+  const P = name => command.props[name]
+
+  if (P`list`) {
+  }
+
+  if (P`add`) {
+  }
+
+  if (P`delete`) {
+  }
+}

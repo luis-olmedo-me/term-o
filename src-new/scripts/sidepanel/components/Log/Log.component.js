6b24f6e26d8bbe7bfb3a8e78554ae3ff9376bb4d
LuisMi <olmedoluis012@gmail.com>
2024-07-04 17:41:13 -0300

    feat: Limit the logs shown

--- src-new/scripts/sidepanel/components/Log/Log.component.js
+++ src-new/scripts/sidepanel/components/Log/Log.component.js
@@ -4,16 +4,20 @@ import { useEffect, useRef, useState } from 'preact/hooks'
 import ColoredText from '../ColoredText'
 import * as S from './Log.styles'
 
+const MAX_LINES_PER_COMMAND = 50
+
 export const Log = ({ command, prefix, onUpdate }) => {
   const [updates, setUpdates] = useState(command.updates)
+  const [visiblesCount, setVisiblesCount] = useState(MAX_LINES_PER_COMMAND)
   const wrapper = useRef(null)
 
   useEffect(
     function listenUpdates() {
       const handleUpdate = ({ updates: newUpdates }) => {
         if (onUpdate) onUpdate()
+        const limitedUpdates = newUpdates.slice(visiblesCount * -1)
 
-        setUpdates(newUpdates)
+        setUpdates(limitedUpdates)
       }
 
       handleUpdate(command)

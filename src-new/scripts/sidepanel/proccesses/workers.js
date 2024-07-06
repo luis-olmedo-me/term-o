6ee8efcce4ab213c4a2d9053053ebe322c8f63b1
LuisMi <olmedoluis012@gmail.com>
2024-07-06 19:31:23 -0300

    feat: Isolate and use helpers

--- src-new/scripts/sidepanel/proccesses/workers.js
+++ src-new/scripts/sidepanel/proccesses/workers.js
@@ -27,24 +27,3 @@ export const getStorage = (tabId, data) => {
     data
   })
 }
-
-export const getTab = async tabIdRaw => {
-  const tabIdString = tabIdRaw.replace(/^T/, '')
-  const tabId = Number(tabIdString)
-  const isValidId = !Number.isNaN(tabId) && tabIdRaw.startsWith('T')
-
-  if (!isValidId) throw 'The tab id provided is not valid.'
-
-  return await chrome.tabs.get(tabId)
-}
-
-export const createTab = async options => {
-  let tab = await chrome.tabs.create(options)
-
-  while (tab.status !== 'complete') {
-    await delay(100)
-    tab = await chrome.tabs.get(tab.id)
-  }
-
-  return tab
-}

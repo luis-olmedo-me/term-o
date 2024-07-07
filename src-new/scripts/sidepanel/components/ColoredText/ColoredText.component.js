851f6bfbac75eaa89749c6e9e766175fda673ce7
LuisMi <olmedoluis012@gmail.com>
2024-07-07 03:06:56 -0300

    feat: Update history when theme changes

--- src-new/scripts/sidepanel/components/ColoredText/ColoredText.component.js
+++ src-new/scripts/sidepanel/components/ColoredText/ColoredText.component.js
@@ -1,11 +1,12 @@
 import * as React from 'preact'
-
 import { useMemo } from 'preact/hooks'
+
+import colorSet from '@src/libs/color-set'
 import { convertStringToObjects } from './ColoredText.helpers'
 import * as S from './ColoredText.styles'
 
 export const ColoredText = ({ value }) => {
-  const sections = useMemo(() => convertStringToObjects(value), [value])
+  const sections = useMemo(() => convertStringToObjects(value), [value, colorSet.name])
 
   return (
     <>

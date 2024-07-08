14d1d77f1a318ed639615c6de3c2a4e2477f5d41
LuisMi <olmedoluis012@gmail.com>
2024-07-08 08:28:24 -0300

    fix: Properly set up handler

--- src-new/scripts/sidepanel/command-handlers/index.js
+++ src-new/scripts/sidepanel/command-handlers/index.js
@@ -2,6 +2,7 @@ import { commandNames } from '@src/libs/command-parser'
 import { handleALIAS } from './alias/alias.handler'
 import { handleDOM } from './dom/dom.handler'
 import { handleSTORAGE } from './storage/storage.handler'
+import { handleSTYLES } from './style/style.handler'
 import { handleTABS } from './tabs/tabs.handler'
 import { handleTHEME } from './theme/theme.handler'
 
@@ -10,5 +11,6 @@ export default {
   [commandNames.STORAGE]: handleSTORAGE,
   [commandNames.TABS]: handleTABS,
   [commandNames.ALIAS]: handleALIAS,
-  [commandNames.THEME]: handleTHEME
+  [commandNames.THEME]: handleTHEME,
+  [commandNames.STYLE]: handleSTYLES
 }

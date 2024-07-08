4e0e1ef8f3f28c2b5f08bcf1e746bbff3c69a498
LuisMi <olmedoluis012@gmail.com>
2024-07-08 01:28:41 -0300

    feat: Properly validate prop

--- src-new/libs/command-parser/commands/style/style.command.js
+++ src-new/libs/command-parser/commands/style/style.command.js
@@ -1,10 +1,13 @@
 import { commandNames } from '../../command-parser.constants'
 import Command from '../../sub-services/command'
+import { isXpath } from '../validators'
 
 export const createSTYLE = () => {
   return new Command({ name: commandNames.STYLE }).expect({
     name: 'on',
-    type: 'boolean',
-    abbreviation: 'o'
+    type: 'string',
+    abbreviation: 'o',
+    worksWith: [],
+    validate: [isXpath]
   })
 }

dd77f98b7535d9a804f31de0fa33f902b1159700
LuisMi <olmedoluis012@gmail.com>
2024-07-10 01:44:50 -0300

    feat: Isolate each validation

--- src-new/libs/command-parser/sub-services/Options/Options.helpers.js
+++ src-new/libs/command-parser/sub-services/Options/Options.helpers.js
@@ -1,40 +1,47 @@
 import { getColor as C } from '@src/theme/theme.helpers'
 
+const validatePropDependencies = (propDependencies, propNames, propName) => {
+  const propsCollapsing = propNames.filter(
+    name => !propDependencies.includes(name) && propName !== name
+  )
+
+  if (propsCollapsing.length) {
+    const useless = propsCollapsing.map(name => `${C`brightRed`}--${name}${C`red`}`).join(' or ')
+
+    throw `${C`brightRed`}--${propName}${C`red`} can not work with ${useless}`
+  }
+}
+
+const validateAsDependency = (dependencies, newProps, propName) => {
+  const missedDependencyFrom = Object.entries(dependencies).reduce(
+    (deps, [dependencyName, dependencyValues]) => {
+      return dependencyValues.includes(propName) ? [...deps, dependencyName] : deps
+    },
+    []
+  )
+  const missingDependencies = missedDependencyFrom.filter(
+    dependency => typeof newProps[dependency] === 'undefined'
+  )
+
+  const isValidSkiping = missedDependencyFrom.length - 1 === missingDependencies.length
+
+  if (missingDependencies.length && !isValidSkiping) {
+    const matches = missingDependencies
+      .map(name => `${C`brightRed`}--${name}${C`red`}`)
+      .join(' or ')
+
+    throw `${C`brightRed`}--${propName}${C`red`} requires to be executed with ${matches}.`
+  }
+}
+
 export const validate = (dependencies, newProps) => {
   const propNames = Object.keys(newProps)
 
   for (const propName of propNames) {
     const propDependencies = dependencies[propName]
 
-    const missedDependencyFrom = Object.entries(dependencies).reduce(
-      (deps, [dependencyName, dependencyValues]) => {
-        return dependencyValues.includes(propName) ? [...deps, dependencyName] : deps
-      },
-      []
-    )
-    const missingDependencies = missedDependencyFrom.filter(
-      dependency => typeof newProps[dependency] === 'undefined'
-    )
+    validateAsDependency(dependencies, newProps, propName)
 
-    const isValidSkiping = missedDependencyFrom.length - 1 === missingDependencies.length
-
-    if (missingDependencies.length && !isValidSkiping) {
-      const matches = missingDependencies
-        .map(name => `${C`brightRed`}--${name}${C`red`}`)
-        .join(' or ')
-
-      throw `${C`brightRed`}--${propName}${C`red`} requires to be executed with ${matches}.`
-    }
-
-    if (!propDependencies) continue
-    const propsCollapsing = propNames.filter(
-      name => !propDependencies.includes(name) && propName !== name
-    )
-
-    if (propsCollapsing.length) {
-      const useless = propsCollapsing.map(name => `${C`brightRed`}--${name}${C`red`}`).join(' or ')
-
-      throw `${C`brightRed`}--${propName}${C`red`} can not work with ${useless}`
-    }
+    if (propDependencies) validatePropDependencies(propDependencies, propNames, propName)
   }
 }

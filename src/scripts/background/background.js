1076e207cc9dfd34a47859e4ab2fae9d7b6d4e27
LuisMi <olmedoluis012@gmail.com>
2024-07-17 20:49:23 -0300

    feat: Use command parser on background

--- src/scripts/background/background.js
+++ src/scripts/background/background.js
@@ -1,4 +1,9 @@
+import commandHandlers, { getCurrentTab } from '@sidepanel/command-handlers'
+import commandParser from '@src/libs/command-parser'
+
 chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true })
+
+commandParser.setHandlers(commandHandlers)
 
 let userEvents = []
 
@@ -7,5 +12,28 @@ const getUserEventsFromLS = async () => {
 
   userEvents = events
 }
+const executeEvents = async events => {
+  const tab = await getCurrentTab()
+
+  events.forEach(event => {
+    const command = commandParser.read(event.command)
+
+    command.appendsData({ tab })
+
+    command.execute()
+  })
+}
 
 getUserEventsFromLS()
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status !== 'complete') return
+
+  const pendingEvents = userEvents.filter(event => new RegExp(event.url).test(tab.url))
+
+  if (pendingEvents.length === 0) return
+
+  executeEvents(pendingEvents)
+
+  console.log(tab, tab, changeInfo)
+})

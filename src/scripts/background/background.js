a7dea58466ae07a97de00b6d27ed9e7309379ba8
LuisMi <olmedoluis012@gmail.com>
2024-07-17 01:48:03 -0300

    feat: Get events on background script

--- src/scripts/background/background.js
+++ src/scripts/background/background.js
@@ -1,1 +1,11 @@
 chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true })
+
+let userEvents = []
+
+const getUserEventsFromLS = async () => {
+  const { events = [] } = await chrome.storage.local.get('events')
+
+  userEvents = events
+}
+
+getUserEventsFromLS()

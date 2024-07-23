2855c76c5599ed6286b612ccb64a21d77d081f32
LuisMi <olmedoluis012@gmail.com>
2024-07-22 23:10:59 -0300

    feat: Use dynamic default theme name

--- src/scripts/background/command-handlers/theme/theme.handler.js
+++ src/scripts/background/command-handlers/theme/theme.handler.js
@@ -1,4 +1,4 @@
-import { defaultSets } from '@src/libs/color-set/color-set.constants'
+import { defaultlDarkMode, defaultSets } from '@src/libs/color-set'
 import { displayHelp, formatTheme } from '../command-handlers.helpers'
 import { getStorageValue, setStorageValue } from '../storage/storage.helpers'
 
@@ -44,7 +44,7 @@ export const handleTHEME = async command => {
   if (P`delete`) {
     const name = P`delete`
     const currentName = theme.colors.name
-    const defaultName = 'Default Dark Mode'
+    const defaultName = defaultlDarkMode.name
 
     const colorSetsFromLS = await getStorageValue('local', 'color-sets')
     const colorSets = colorSetsFromLS || defaultSets

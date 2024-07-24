82b623b1179a4ae7683273bc2259c2b5c6779d5e
LuisMi <olmedoluis012@gmail.com>
2024-07-24 01:24:24 -0300

    feat: Properly export constants

--- src/libs/themer/index.js
+++ src/libs/themer/index.js
@@ -1,5 +1,5 @@
-import { defaultColorMode, defaultSets } from './themer.constants'
+import { defaultColorTheme, defaultTheme } from './themer.constants'
 import { themer } from './themer.service'
 
 export default themer
-export { defaultColorMode, defaultSets }
+export { defaultColorTheme, defaultTheme }

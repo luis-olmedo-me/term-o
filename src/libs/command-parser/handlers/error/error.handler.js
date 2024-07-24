2fc82e8121ee34506304740302090a276c8a05db
LuisMi <olmedoluis012@gmail.com>
2024-07-24 02:59:56 -0300

    fix: Properly show error messages

--- src/libs/command-parser/handlers/error/error.handler.js
+++ src/libs/command-parser/handlers/error/error.handler.js
@@ -4,7 +4,7 @@ export const handleERROR = async command => {
   const P = name => command.props[name]
 
   if (P`title`) {
-    const errorUpdate = formatError({ message: P`title` })
+    const errorUpdate = formatError({ title: P`title` })
 
     command.throw(errorUpdate)
   }

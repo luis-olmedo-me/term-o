5e12bf5cc2c587a7d9d0e8e1883327c27af5f51e
LuisMi <olmedoluis012@gmail.com>
2024-07-23 22:44:24 -0300

    feat: Save default data

--- src/libs/themer/themer.service.js
+++ src/libs/themer/themer.service.js
@@ -3,29 +3,41 @@ import { defaultColorTheme, defaultTheme } from './themer.constants'
 import { handleStorageChanges } from './themer.helpers'
 
 class Themer {
-  constructor(defaultColors, defaultTheme) {
-    this.colors = defaultColors
-    this.colorSets = [defaultColors]
+  constructor(defaultColorTheme, defaultTheme) {
+    this.defaultColorThemes = [defaultColorTheme]
+    this.defaultColorTheme = defaultColorTheme
+
     this.staticTheme = defaultTheme
+    this.colorTheme = defaultColorTheme
+    this.colorThemes = this.defaultColorThemes
+
     this.handleStorageChanges = handleStorageChanges.bind(this)
 
     this.getThemesFromLS()
     chrome.storage.onChanged.addListener(this.handleStorageChanges)
   }
 
-  theme() {
-    return { ...this.staticTheme, colors: this.colors }
+  isDefault(name) {
+    return this.defaultColorThemes.some(set => set.name === name)
+  }
+
+  get defaultColorName() {
+    return this.defaultColorTheme.name
+  }
+
+  get theme() {
+    return { ...this.staticTheme, colors: this.colorTheme }
   }
 
   async getThemesFromLS() {
     const newThemes = await getStorageValue('local', 'color-sets')
     const newThemeName = await getStorageValue('local', 'color-set-name')
 
-    if (newThemes) this.colorSets = newThemes
+    if (newThemes) this.colorThemes = newThemes
 
-    const newTheme = this.colorSets.find(theme => theme.name === newThemeName)
+    const newTheme = this.colorThemes.find(theme => theme.name === newThemeName)
 
-    if (newTheme) this.colors = newTheme
+    if (newTheme) this.colorTheme = newTheme
   }
 
   destroy() {

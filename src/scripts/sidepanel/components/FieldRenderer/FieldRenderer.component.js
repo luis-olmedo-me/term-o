2cd32d7ad3a122383f27b4ee231b6bb4ea41d47f
LuisMi <olmedoluis012@gmail.com>
2024-07-29 01:18:52 -0300

    feat: properly save checkbox changes

--- src/scripts/sidepanel/components/FieldRenderer/FieldRenderer.component.js
+++ src/scripts/sidepanel/components/FieldRenderer/FieldRenderer.component.js
@@ -1,5 +1,5 @@
 import * as React from 'preact'
-import { useState } from 'preact/hooks'
+import { useEffect, useState } from 'preact/hooks'
 
 import useConfig from '@src/hooks/useConfig'
 import Input from '../Input'
@@ -7,7 +7,17 @@ import Input from '../Input'
 export const FieldRenderer = ({ value, sectionId, inputId, type }) => {
   const [localValue, setLocalValue] = useState(value)
 
+  const isDifferentValue = localValue !== value
+  const isCheckbox = type === 'checkbox'
+
   const { changeConfig } = useConfig()
+
+  useEffect(
+    function updateLocalValue() {
+      if (isDifferentValue && isCheckbox) setLocalValue(value)
+    },
+    [isDifferentValue, isCheckbox]
+  )
 
   const inputs = {
     text: (
@@ -29,7 +39,7 @@ export const FieldRenderer = ({ value, sectionId, inputId, type }) => {
       <Input
         type="checkbox"
         checked={localValue}
-        onChange={({ target }) => changeConfig(sectionId, inputId, target.value)}
+        onChange={({ target }) => changeConfig(sectionId, inputId, target.checked)}
         endText={localValue ? 'Yes' : 'No'}
       />
     ),

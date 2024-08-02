bdbf351966120a94f2e1307aad4479d083f06dc4
LuisMi <olmedoluis012@gmail.com>
2024-08-02 01:56:24 -0300

    feat: Add options and conditionals

--- src/scripts/sidepanel/components/Prompt/Prompt.helpers.js
+++ src/scripts/sidepanel/components/Prompt/Prompt.helpers.js
@@ -1,8 +1,37 @@
+const conditionPattern = /^\s*([\w]+)\s*==\s*(".*?"|\w+)\s*\?\s*(.*)\s*:\s*(.*)\s*$/
 export const createPSO = (value, tab) => {
+  if (!tab) return value
+
   const url = tab?.url && new URL(tab.url)
 
-  const origin = url?.origin || ''
-  const pathname = url?.pathname || ''
+  const replacements = {
+    origin: url.origin || '',
+    pathname: url.pathname || '',
+    host: url.host || '',
+    hostname: url.hostname || '',
+    status: tab.status || '',
+    title: tab.title || '',
+    tab_id: `T${tab.id || ''}`,
+    window_id: `T${tab.windowId || ''}`,
+    is_incognito: `${tab.incognito}`
+  }
 
-  return value.replace('{origin}', origin).replace('{pathname}', pathname)
+  return value.replace(/{([^}]+)}/g, (match, expression) => {
+    const result = conditionPattern.exec(expression)
+
+    if (result) {
+      const [, lhs, rhs, trueValue, falseValue] = result
+
+      const lhsValue = replacements[lhs] ?? lhs
+      const rhsValue = rhs.replace(/"/g, '').trim()
+
+      const isConditionMatch = lhsValue === rhsValue
+
+      return isConditionMatch
+        ? trueValue.replace(/"/g, '').trim()
+        : falseValue.replace(/"/g, '').trim()
+    } else {
+      return replacements[expression] || match
+    }
+  })
 }

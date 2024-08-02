d9e05a2643aac1d2bb4a2fa177a76d6e77125df7
LuisMi <olmedoluis012@gmail.com>
2024-08-02 01:02:51 -0300

    refactor: Add sanity check

--- src/scripts/sidepanel/components/Prompt/Prompt.helpers.js
+++ src/scripts/sidepanel/components/Prompt/Prompt.helpers.js
@@ -1,5 +1,5 @@
 export const createPSO = (value, tab) => {
-  const url = tab && new URL(tab.url)
+  const url = tab?.url && new URL(tab.url)
 
   const origin = url?.origin || ''
   const pathname = url?.pathname || ''

bc85c436fdbd2d8678a8a2a3955a3b12239adbc6
LuisMi <olmedoluis012@gmail.com>
2024-08-06 00:25:11 -0300

    feat: Handle help prop

--- src/libs/command-parser/handlers/request/request.handler.js
+++ src/libs/command-parser/handlers/request/request.handler.js
@@ -1,3 +1,9 @@
+import { displayHelp } from '../handlers.helpers'
+
 export const handleREQUEST = async command => {
+  const P = name => command.props[name]
+
   command.update('testing..')
+
+  if (P`help`) displayHelp(command)
 }

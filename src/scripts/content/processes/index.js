880673f4987fdc8bd55e752bea04c1be6b430a9d
LuisMi <olmedoluis012@gmail.com>
2024-08-14 00:49:18 -0300

    feat: Create set storage process

--- src/scripts/content/processes/index.js
+++ src/scripts/content/processes/index.js
@@ -2,6 +2,7 @@ import { clickElement } from './dom/click-element.process'
 import { findDOMElement } from './dom/find-dom-element.process'
 import { getDOMElements } from './dom/get-dom-elements.process'
 import { getStorage } from './storage/get-storage.process'
+import { setStorage } from './storage/set-storage.process'
 import { applyElementStyles } from './style/apply-element-styles.process'
 import { getElementStyles } from './style/get-element-styles.process'
 
@@ -9,6 +10,7 @@ export default {
   'get-dom-elements': getDOMElements,
   'find-dom-element': findDOMElement,
   'get-storage': getStorage,
+  'set-storage': setStorage,
   'get-element-styles': getElementStyles,
   'apply-element-styles': applyElementStyles,
   'click-element': clickElement

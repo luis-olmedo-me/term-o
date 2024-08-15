468e3ea5d19fd7bf4b41ef6a2074f14968eda2c8
LuisMi <olmedoluis012@gmail.com>
2024-08-15 01:23:24 -0300

    feat: handle dialog cancel

--- src/libs/command-parser/handlers/scripts/scripts.helpers.js
+++ src/libs/command-parser/handlers/scripts/scripts.helpers.js
@@ -1,10 +1,25 @@
 export const uploadFile = () => {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     const fileInput = document.createElement('input')
     fileInput.setAttribute('type', 'file')
 
+    const receiveFile = event => {
+      const [firstFile] = Array.from(event.currentTarget.files)
+
+      if (firstFile) resolve(firstFile)
+      else reject('No file was uploaded.')
+    }
+
+    const cancel = () => {
+      fileInput.remove()
+
+      reject('No file was uploaded.')
+    }
+
+    fileInput.addEventListener('change', receiveFile)
+    fileInput.addEventListener('cancel', cancel)
+
     fileInput.click()
-    resolve()
   })
 
   //   if (fileInput.files.length === 0) {

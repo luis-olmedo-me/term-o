8da57636b057bfb7f5eb882212ddc18effd74d14
LuisMi <olmedoluis012@gmail.com>
2024-10-16 21:01:59 -0300

    refator: Remove event when it is not needed

--- src/scripts/sandbox/sandbox.js
+++ src/scripts/sandbox/sandbox.js
@@ -1,23 +1,28 @@
 const safeEval = event => {
   const code = event.data.code
+
   const storage = props => {
     return new Promise(resolve => {
       const handleSandboxCommand = event => {
         if (event.data?.type !== 'sandbox-command-return') return
 
+        window.removeEventListener('message', handleSandboxCommand)
         resolve(event.data.updates)
       }
+
+      window.addEventListener('message', handleSandboxCommand)
 
       event.source.window.postMessage(
         { type: 'sandbox-command', data: { props, name: 'storage' } },
         event.origin
       )
-
-      window.addEventListener('message', handleSandboxCommand)
     })
   }
 
+  const update = () => {}
+
   const restrictedEval = new Function(
+    'update',
     'storage',
     `
         "use strict";
@@ -29,7 +34,7 @@ const safeEval = event => {
   )
 
   try {
-    return restrictedEval(storage)
+    return restrictedEval(storage, update)
   } catch (error) {
     return `Error: ${error.message}`
   }

89f0cc31a07235e5bd2baa77dc4f53d09e49c521
LuisMi <olmedoluis012@gmail.com>
2025-09-21 01:13:53 -0300

    refactor: Use constants for storage

--- src/scripts/background/packages/event-manager.package.js
+++ src/scripts/background/packages/event-manager.package.js
@@ -1,20 +1,22 @@
+import { storageKeys } from '@src/constants/storage.constants'
+import { namespaces } from '@src/hooks/useStorage'
 import { getStorageValue } from '@src/libs/command-parser/handlers/storage/storage.helpers'
 
 const eventManager = (function() {
   let values = []
 
   const handleStorageChanges = (changes, currentChanges) => {
-    if (currentChanges !== 'local') return
+    if (currentChanges !== namespaces.LOCAL) return
 
     for (let [storageKey, { newValue }] of Object.entries(changes)) {
-      if (storageKey === 'events') {
+      if (storageKey === storageKeys.EVENTS) {
         values = newValue
       }
     }
   }
 
   const getUserEventsFromLS = async () => {
-    const events = await getStorageValue('local', 'events')
+    const events = await getStorageValue(namespaces.LOCAL, storageKeys.EVENTS)
 
     values = events || []
   }

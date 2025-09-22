5126500b780f741a8ec28ac74dad4354792525ac
LuisMi <olmedoluis012@gmail.com>
2025-09-22 09:20:47 -0300

    Use storage constants

--- src/scripts/content/processes/storage/set-storage.process.js
+++ src/scripts/content/processes/storage/set-storage.process.js
@@ -1,14 +1,16 @@
+import { storageNamespaces } from '@src/constants/storage.constants'
+
 export const setStorage = async (resolve, data) => {
   let { namespace, key, value } = data
 
   switch (namespace) {
-    case 'local':
+    case storageNamespaces.LOCAL:
       window.localStorage.setItem(key, value)
       break
-    case 'session':
+    case storageNamespaces.SESSION:
       window.sessionStorage.setItem(key, value)
       break
-    case 'cookie':
+    case storageNamespaces.COOKIE:
       document.cookie = `${key}=${value}`
       break
   }

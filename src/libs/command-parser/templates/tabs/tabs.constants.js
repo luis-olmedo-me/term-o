fa847885af9c1b758a5f2cc5910ba3ebc9dfb916
LuisMi <olmedoluis012@gmail.com>
2025-09-24 14:35:04 -0300

    feat: Add help with correct format for tabs

---
+++ src/libs/command-parser/templates/tabs/tabs.constants.js
@@ -0,0 +1,11 @@
+export const tabsHelpSections = {
+  GENERAL: 'general',
+  FILTERS: 'filters',
+  TAB_ACTIONS: 'tab_actions'
+}
+
+export const tabsHelpSectionTitles = {
+  [tabsHelpSections.GENERAL]: 'General',
+  [tabsHelpSections.FILTERS]: 'Filters',
+  [tabsHelpSections.TAB_ACTIONS]: 'Tab Actions'
+}

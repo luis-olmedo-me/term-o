79b193f81ecfb82df53de009c5097e9e602c4e34
LuisMi <olmedoluis012@gmail.com>
2025-09-24 15:13:53 -0300

    feat: Apply new help scheme for events command

---
+++ src/libs/command-parser/templates/events/events.constants.js
@@ -0,0 +1,11 @@
+export const eventsHelpSections = {
+  GENERAL: 'general',
+  CREATION: 'creation',
+  MANAGEMENT: 'management'
+}
+
+export const eventsHelpSectionTitles = {
+  [eventsHelpSections.GENERAL]: 'General',
+  [eventsHelpSections.CREATION]: 'Creation',
+  [eventsHelpSections.MANAGEMENT]: 'Management'
+}

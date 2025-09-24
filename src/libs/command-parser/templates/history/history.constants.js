6b911259d9b7b056058b8f56940596a32bff5151
LuisMi <olmedoluis012@gmail.com>
2025-09-24 15:16:21 -0300

    feat: Apply new help scheme for history command

---
+++ src/libs/command-parser/templates/history/history.constants.js
@@ -0,0 +1,11 @@
+export const historyHelpSections = {
+  GENERAL: 'general',
+  FILTERS: 'filters',
+  MANAGEMENT: 'management'
+}
+
+export const historyHelpSectionTitles = {
+  [historyHelpSections.GENERAL]: 'General',
+  [historyHelpSections.FILTERS]: 'Filters',
+  [historyHelpSections.MANAGEMENT]: 'Management'
+}

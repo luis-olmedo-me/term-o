2171674252fc2d84c3e59e4a348b0c9e098d6a3b
LuisMi <olmedoluis012@gmail.com>
2025-09-29 13:16:58 -0300

    feat: Display logo

--- src/scripts/configuration/components/SidePanel/SidePanel.component.js
+++ src/scripts/configuration/components/SidePanel/SidePanel.component.js
@@ -1,11 +1,14 @@
 import * as React from 'preact'
 
+import { Logo } from '@src/icons/Logo.component'
 import Button from '@src/scripts/sidepanel/components/Button'
 import * as S from './SidePanel.styles'
 
 export const SidePanel = ({ options }) => {
   return (
     <S.SidePanelWrapper>
+      <Logo />
+
       {options.map(option => {
         return <Button key={option.id} text={option.name} fullWidth />
       })}
--- src/scripts/sidepanel/components/Button/index.js
+++ src/scripts/sidepanel/components/Button/index.js
@@ -1,3 +1,6 @@
 import { Button } from './Button.component'
+import { ButtonWrapper } from './Button.styles'
 
 export default Button
+
+export { ButtonWrapper }

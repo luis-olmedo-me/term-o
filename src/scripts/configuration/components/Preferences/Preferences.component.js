ca29b72c786eed396b909c630e290d0a7835f2b9
LuisMi <olmedoluis012@gmail.com>
2025-09-29 09:14:05 -0300

    feat: Encapsulate preferences in div

--- src/scripts/configuration/components/Preferences/Preferences.component.js
+++ src/scripts/configuration/components/Preferences/Preferences.component.js
@@ -7,33 +7,37 @@ import * as S from './Preferences.styles'
 export const Preferences = () => {
   const { config } = useConfig()
 
-  return config.map(section => {
-    return (
-      <S.SectionWrapper key={section.id}>
-        <S.SectionTitle>{section.name}</S.SectionTitle>
-        <S.Description>{section.description}</S.Description>
+  return (
+    <div>
+      {config.map(section => {
+        return (
+          <S.SectionWrapper key={section.id}>
+            <S.SectionTitle>{section.name}</S.SectionTitle>
+            <S.Description>{section.description}</S.Description>
 
-        {section.inputs.map(input => {
-          return (
-            <S.InputsWrapper key={input.id}>
-              <S.InputTitle>
-                {input.name} [{input.type}]
-              </S.InputTitle>
+            {section.inputs.map(input => {
+              return (
+                <S.InputsWrapper key={input.id}>
+                  <S.InputTitle>
+                    {input.name} [{input.type}]
+                  </S.InputTitle>
 
-              <S.Description>{input.description}</S.Description>
+                  <S.Description>{input.description}</S.Description>
 
-              <FieldRenderer
-                value={input.value}
-                sectionId={section.id}
-                inputId={input.id}
-                type={input.type}
-              />
-            </S.InputsWrapper>
-          )
-        })}
-      </S.SectionWrapper>
-    )
-  })
+                  <FieldRenderer
+                    value={input.value}
+                    sectionId={section.id}
+                    inputId={input.id}
+                    type={input.type}
+                  />
+                </S.InputsWrapper>
+              )
+            })}
+          </S.SectionWrapper>
+        )
+      })}
+    </div>
+  )
 }
 
 Preferences.propTypes = {

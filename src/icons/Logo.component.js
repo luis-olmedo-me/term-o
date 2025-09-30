c9cd887496ddf92c22fdeb5a039d6839c508d200
LuisMi <olmedoluis012@gmail.com>
2025-09-30 18:20:08 -0300

    feat: Make logo be smaller

--- src/icons/Logo.component.js
+++ src/icons/Logo.component.js
@@ -6,8 +6,8 @@ export const Logo = () => {
 
   return (
     <svg
-      width={128}
-      height={128}
+      width={100}
+      height={100}
       viewBox="0 0 512 512"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"

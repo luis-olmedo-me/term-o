4ac7a41cce1e81c8d682007e4ddcf011dd6a885f
LuisMi <olmedoluis012@gmail.com>
2025-10-02 15:35:48 -0300

    feat: Create switch

---
+++ src/scripts/configuration/components/Switch/Switch.styles.js
@@ -0,0 +1,59 @@
+import { theme as t } from '@src/libs/themer'
+import styled from 'styled-components'
+
+export const Input = styled.input`
+  font-family: ${t('font.primary')};
+  width: 100%;
+  box-sizing: border-box;
+  white-space: nowrap;
+  font-size: ${t('fontSize.50')};
+  border: none;
+  display: block;
+  caret-color: ${t('colors.cursorColor')};
+  line-height: ${t('lineHeight.300')};
+  background-color: ${t('colors.background')};
+  color: ${t('colors.foreground')};
+  letter-spacing: ${t('space.50')};
+
+  &:active,
+  &:focus,
+  &:focus-visible {
+    outline: none;
+  }
+
+  &::selection {
+    color: ${t('colors.green')};
+    background-color: ${t('colors.selectionBackground')};
+  }
+`
+
+export const SwitchWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${t('space.400')};
+  background-color: ${t('colors.background')};
+  color: ${t('colors.foreground')};
+  padding: ${t('space.300')} ${t('space.600')};
+  cursor: text;
+
+  &[aria-disabled='true'],
+  &[aria-disabled='true'] ${Input}:disabled {
+    cursor: text;
+  }
+
+  &:has(${Input}[type="checkbox"]) {
+    width: fit-content;
+  }
+
+  input[type='checkbox'] {
+    margin: 0;
+    accent-color: ${t('colors.brightGreen')};
+  }
+`
+
+export const Prefix = styled.span`
+  &::selection {
+    color: ${t('colors.green')};
+    background-color: ${t('colors.selectionBackground')};
+  }
+`
---
+++ src/scripts/configuration/components/Switch/index.js
@@ -0,0 +1,6 @@
+import { Switch } from './Switch.component'
+import { SwitchWrapper } from './Switch.styles'
+
+export default Switch
+
+export { SwitchWrapper as InputWrapper }

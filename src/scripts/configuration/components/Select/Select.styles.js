c3f1f88edbd3cc6a2c1d3b0a734c79a94b09b4c9
LuisMi <olmedoluis012@gmail.com>
2025-10-02 15:04:24 -0300

    feat: Add loading state for Select input

--- src/scripts/configuration/components/Select/Select.styles.js
+++ src/scripts/configuration/components/Select/Select.styles.js
@@ -1,5 +1,45 @@
 import { theme as t } from '@src/libs/themer'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const slide = keyframes`
+  from {
+    left: 0;
+    opacity: 0;
+  }
+
+  40%{
+    opacity: 1;
+  }
+  60%{
+    opacity: 1;
+  }
+
+  to {
+    left: 95%;
+    opacity: 0;
+  }
+`
+
+export const SelecterWrapper = styled.div`
+  &.loading {
+    position: relative;
+    width: fit-content;
+
+    &::after {
+      content: '';
+      position: absolute;
+      animation: ${slide} 1s linear infinite alternate;
+      animation-timing-function: ease-in-out;
+      width: 5%;
+      height: ${t('space.100')};
+      background: ${t('colors.blue')};
+      bottom: 0;
+      left: 0;
+      border-radius: ${t('radius.100')} ${t('radius.100')} 0 0;
+      box-shadow: 0 -10px 25px 5px ${t('colors.blue')};
+    }
+  }
+`
 
 export const Selecter = styled.select`
   width: 300px;

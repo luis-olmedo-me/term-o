57d7011cbe3416a581c9c4e5813517ed151ec96b
LuisMi <olmedoluis012@gmail.com>
2025-10-03 12:45:06 -0300

    feat: Increase size

--- src/scripts/configuration/components/ColorDot/ColorDot.styles.js
+++ src/scripts/configuration/components/ColorDot/ColorDot.styles.js
@@ -2,10 +2,10 @@ import { theme as t } from '@src/libs/themer'
 import styled from 'styled-components'
 
 export const Dot = styled.span`
-  width: 0.5rem;
-  height: 0.5rem;
+  width: 0.6rem;
+  height: 0.6rem;
   display: inline-block;
-  border-radius: 100%;
+  border-radius: ${t('radius.200')};
 
   &.red {
     background-color: ${t('colors.red')};

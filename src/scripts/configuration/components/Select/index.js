1ac0903dd99eaec277491993c7f1f80da7b3076a
LuisMi <olmedoluis012@gmail.com>
2025-10-03 12:56:35 -0300

    refactor: Remove useless import

--- src/scripts/configuration/components/Select/index.js
+++ src/scripts/configuration/components/Select/index.js
@@ -1,7 +1,6 @@
 import { ColorDot } from './prefixes/ColorDot/ColorDot.component'
-import { Dot } from './prefixes/ColorDot/ColorDot.styles'
 import { Select } from './Select.component'
 
 export default Select
 
-export { ColorDot, Dot }
+export { ColorDot }

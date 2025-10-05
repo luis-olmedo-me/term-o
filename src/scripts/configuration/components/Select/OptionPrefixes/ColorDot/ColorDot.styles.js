ceae9109e51e423d84bdc520380f0103f3899922
LuisMi <olmedoluis012@gmail.com>
2025-10-05 00:23:09 -0300

    refactor: Remove repeated code

--- src/scripts/configuration/components/Select/OptionPrefixes/ColorDot/ColorDot.styles.js
+++ src/scripts/configuration/components/Select/OptionPrefixes/ColorDot/ColorDot.styles.js
@@ -6,29 +6,24 @@ export const Dot = styled.span`
   height: 0.6rem;
   display: inline-block;
   border-radius: ${t('radius.200')};
+  border: ${t('space.50')} solid currentColor;
 
   &.blue {
     background-color: ${t('colors.blue')};
-    border: ${t('space.50')} solid currentColor;
   }
   &.cyan {
     background-color: ${t('colors.cyan')};
-    border: ${t('space.50')} solid currentColor;
   }
   &.green {
     background-color: ${t('colors.green')};
-    border: ${t('space.50')} solid currentColor;
   }
   &.purple {
     background-color: ${t('colors.purple')};
-    border: ${t('space.50')} solid currentColor;
   }
   &.red {
     background-color: ${t('colors.red')};
-    border: ${t('space.50')} solid currentColor;
   }
   &.yellow {
     background-color: ${t('colors.yellow')};
-    border: ${t('space.50')} solid currentColor;
   }
 `

82f5238c753a0914101424605e630e5aec02d2ac
LuisMi <olmedoluis012@gmail.com>
2025-10-05 00:06:08 -0300

    refactor: Remove useless limit

--- src/scripts/configuration/components/TextArea/TextArea.component.js
+++ src/scripts/configuration/components/TextArea/TextArea.component.js
@@ -1,30 +1,16 @@
 import * as React from 'preact'
-import { useRef } from 'preact/hooks'
 import * as S from './TextArea.styles'
 
 export const TextArea = ({ onChange, onBlur, value, name, maxLines, disabled = false }) => {
-  const inputRef = useRef(null)
-
-  const handleChange = event => {
-    var lines = inputRef.current.value.split('\n')
-
-    if (lines.length > maxLines) {
-      inputRef.current.value = lines.slice(0, maxLines).join('\n')
-    } else {
-      onChange(event)
-    }
-  }
-
   return (
     <S.TextAreaInput
-      ref={inputRef}
       name={name}
       disabled={disabled}
       onBlur={onBlur}
-      onChange={handleChange}
-      spellCheck="false"
+      onChange={onChange}
       rows={maxLines}
       value={value}
+      spellCheck="false"
     ></S.TextAreaInput>
   )
 }

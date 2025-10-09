ca3f586c20e9c62441f831fd67d6ca15ad3b5487
LuisMi <olmedoluis012@gmail.com>
2025-10-09 17:14:18 -0300

    refactor: Use messages helpers from route

--- src/constants/messages.constants.js
+++ src/constants/messages.constants.js
@@ -2,5 +2,9 @@ export const messages = [
   {
     original: /^Error: No tab with id: \d+\.$/g,
     new: 'No tab id found.'
+  },
+  {
+    original: /^Could not establish connection. Receiving end does not exist\./,
+    new: 'Term-o cannot establish a connection to the tab. Please refresh the page or ensure it is open. Note that Term-o cannot execute commands on extension or browser built-in pages.'
   }
 ]

3f5043360c501133c092f5493bffd2b9a4d35ec0
LuisMi <olmedoluis012@gmail.com>
2025-10-09 19:43:33 -0300

    feat: Expect for storage changes of events

--- src/libs/storage/storage.service.js
+++ src/libs/storage/storage.service.js
@@ -1,8 +1,30 @@
+import { storageKeys, storageNamespaces } from '@src/constants/storage.constants'
+import { getStorageValue } from '@src/helpers/storage.helpers'
 import EventListener from '@src/templates/event-listener'
 
 class Storage extends EventListener {
   constructor() {
     super()
+
+    this.events = []
+
+    this.init()
+
+    chrome.storage.onChanged.addListener(this.handleStorageChanges.bind(this))
+  }
+
+  async init() {
+    this.events = await getStorageValue(storageNamespaces.LOCAL, storageKeys.EVENTS, [])
+  }
+
+  handleStorageChanges(changes, changesNamespace) {
+    if (changesNamespace !== storageNamespaces.LOCAL) return
+
+    for (let [storageKey, { newValue }] of Object.entries(changes)) {
+      if (storageKey === storageKeys.EVENTS) {
+        this.events = newValue
+      }
+    }
   }
 }
 

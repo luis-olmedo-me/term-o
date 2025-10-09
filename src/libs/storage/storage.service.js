1b805f7adfb9240fa1dc4b8ac63b28262eaec043
LuisMi <olmedoluis012@gmail.com>
2025-10-09 20:51:15 -0300

    feat: expect for history changes

--- src/libs/storage/storage.service.js
+++ src/libs/storage/storage.service.js
@@ -7,22 +7,25 @@ class Storage extends EventListener {
     super()
 
     this.events = []
+    this.history = []
 
     this.init()
   }
 
   async init() {
     this.events = await getStorageValue(storageNamespaces.LOCAL, storageKeys.EVENTS, [])
+    this.history = await getStorageValue(storageNamespaces.SESSION, storageKeys.HISTORY, [])
 
     chrome.storage.onChanged.addListener(this.handleStorageChanges.bind(this))
   }
 
-  handleStorageChanges(changes, changesNamespace) {
-    if (changesNamespace !== storageNamespaces.LOCAL) return
-
+  handleStorageChanges(changes) {
     for (let [storageKey, { newValue }] of Object.entries(changes)) {
       if (storageKey === storageKeys.EVENTS) {
         this.events = newValue
+      }
+      if (storageKey === storageKeys.HISTORY) {
+        this.history = newValue
       }
     }
   }

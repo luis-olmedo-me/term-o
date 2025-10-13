165976040b12a9574a2e75286bbb4a063bd5c3c7
LuisMi <olmedoluis012@gmail.com>
2025-10-13 18:22:44 -0300

    feat: Add methods for config

--- src/templates/Configuration/Configuration.template.js
+++ src/templates/Configuration/Configuration.template.js
@@ -1,6 +1,33 @@
+import { storageKeys } from '@src/constants/storage.constants'
+import {
+  buildDetailedConfig,
+  getConfigValueByInputId,
+  updateConfigValueIn
+} from '@src/helpers/config.helpers'
+
 export class Configuration {
   constructor(storageService, value) {
     this.storageService = storageService
     this.value = value
+    this.details = buildDetailedConfig(value)
+  }
+
+  set(value) {
+    this.value = value
+    this.details = buildDetailedConfig(value)
+  }
+
+  get(inputId) {
+    return getConfigValueByInputId(this.value, inputId)
+  }
+
+  change(inputId, newValue) {
+    const newConfig = updateConfigValueIn(this.value, inputId, newValue)
+
+    this.storageService.set(storageKeys.CONFIG, newConfig)
+  }
+
+  reset() {
+    this.storageService.set(storageKeys.CONFIG, [])
   }
 }

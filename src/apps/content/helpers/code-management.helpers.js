5a3b0296462a0ce12c80f350c212c86475fc7123
LuisMi <olmedoluis012@gmail.com>
2025-10-20 09:52:55 -0300

    refactor: Delete deprecated file

--- src/apps/content/helpers/code-management.helpers.js
+++
@@ -1,8 +0,0 @@
-export const inject = customFunction => {
-  const script = document.createElement('script')
-  script.textContent = `(${customFunction})();`
-
-  document.documentElement.appendChild(script)
-
-  script.remove()
-}

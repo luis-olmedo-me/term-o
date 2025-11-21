9143a8c358ee299f5d6e86a4464f6e70cff05fb0
LuisMi <olmedoluis012@gmail.com>
2025-11-21 17:45:23 -0300

    feat: Add small delay between clicking elements

--- src/apps/content/process-handlers/trigger-event.process.js
+++ src/apps/content/process-handlers/trigger-event.process.js
@@ -1,6 +1,7 @@
 import { getElementByXPath } from '@content/helpers/dom-management.helpers'
 import { highlightElement, humanClick } from '@content/pure-js'
 import { eventsAvailable } from '@src/constants/options.constants'
+import { delay } from '@src/helpers/utils.helpers'
 
 export default async (resolve, reject, data) => {
   const element = getElementByXPath(data.xpath)
@@ -9,6 +10,7 @@ export default async (resolve, reject, data) => {
 
   if (data.event === eventsAvailable.CLICK) {
     highlightElement(element, data.theme)
+    await delay(100)
     humanClick(element)
   }
 

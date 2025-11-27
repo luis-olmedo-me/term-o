2493102279e035a9af8e0dc46c55611830b648d4
LuisMi <olmedoluis012@gmail.com>
2025-11-27 20:58:04 -0300

    feat: Apply format at term-highlight component

---
+++ src/apps/web-components/components/term-highlight/term-highlight.constants.js
@@ -0,0 +1,1 @@
+export const highlightPropNames = ['color', 'radius', 'duration']
--- src/apps/web-components/components/term-highlight/term-highlight.js
+++ src/apps/web-components/components/term-highlight/term-highlight.js
@@ -1,73 +1,43 @@
+import HighlightCss from './term-highlight.raw.css'
+import HighlightHtml from './term-highlight.raw.html'
+
 import { delay } from '@src/helpers/utils.helpers'
+import { getPropsFromAttrs } from '@web-components/helpers/props.helpers'
+import { highlightPropNames } from './term-highlight.constants'
 
 class TermHighlight extends HTMLElement {
   constructor() {
     super()
 
     this._shadow = this.attachShadow({ mode: 'closed' })
+    this._shadow.innerHTML = HighlightHtml
+    this._elements.styles.innerHTML = HighlightCss
+  }
+
+  get _elements() {
+    return {
+      overlay: this._shadow.querySelector('#overlay'),
+      styles: this._shadow.querySelector('#styles')
+    }
   }
 
   connectedCallback() {
-    const color = this.getAttribute('color')
-    const radius = this.getAttribute('radius')
-    const duration = Number(this.getAttribute('duration'))
+    this._props = getPropsFromAttrs(this, highlightPropNames)
+    const duration = Number(this._props.duration)
 
-    this._shadow.innerHTML = `
-      <style>
-        .overlay {
-          position: absolute;
-          inset: 0;
-          border-radius: ${radius};
-          background: transparent;
-          border: 1px solid transparent;
-          opacity: 0;
-          transform: scale(1.4);
-          box-shadow: 0 0 0 transparent;
+    this.style.setProperty('--color', this._props.color)
+    this.style.setProperty('--radius', this._props.radius)
 
-          transition:
-            opacity .2s ease-in-out,
-            transform .2s ease-in-out,
-            box-shadow .2s ease-in-out,
-            border-color .2s ease-in-out,
-            background-color .2s ease-in-out;
-        }
-
-        .overlay.active {
-          opacity: 1;
-          transform: scale(1);
-          background-color: ${color};
-          border-color: ${color};
-          box-shadow: 0 0 15px ${color};
-        }
-
-        .overlay.fade-out {
-          transition:
-            opacity .3s ease-in-out,
-            transform .5s ease-in-out,
-            box-shadow .6s ease-in-out,
-            border-color .5s ease-in-out,
-            background-color .5s ease-in-out;
-
-          opacity: 0;
-          transform: scale(1.3);
-          box-shadow: 0 0 10px ${color};
-        }
-      </style>
-
-      <div class="overlay"></div>
-    `
-
-    this._overlay = this._shadow.querySelector('.overlay')
     this._runAnimation(duration)
   }
 
   async _runAnimation(duration) {
     await delay(20)
-    this._overlay.classList.add('active')
+    this._elements.overlay.classList.add('active')
 
     await delay(400)
-    this._overlay.classList.remove('active')
-    this._overlay.classList.add('fade-out')
+    this._elements.overlay.classList.remove('active')
+    this._elements.overlay.classList.add('fade-out')
 
     await delay(duration)
     this.remove()

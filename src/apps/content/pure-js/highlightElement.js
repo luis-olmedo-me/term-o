ade397c65f0be28f1db5e08b59d6b44723d6413a
LuisMi <olmedoluis012@gmail.com>
2025-11-27 21:46:26 -0300

    feat: Use createWebElement helper to create highlight

--- src/apps/content/pure-js/highlightElement.js
+++ src/apps/content/pure-js/highlightElement.js
@@ -1,18 +1,18 @@
 import { customColorThemeKeys } from '@src/constants/themes.constants'
 import { webElements } from '@src/constants/web-elements.constants'
+import { createWebElement } from './createWebElement'
 
 export const highlightElement = async (element, theme) => {
   const rect = element.getBoundingClientRect()
   const radius = window.getComputedStyle(element).borderRadius
   const color = theme.colors[customColorThemeKeys.ACCENT]
 
-  const host = document.createElement(webElements.HIGHLIGHT)
-  host.setAttribute('color', color)
-  host.setAttribute('radius', radius)
-  host.setAttribute('left', `${rect.left}px`)
-  host.setAttribute('top', `${rect.top}px`)
-  host.setAttribute('width', `${rect.width}px`)
-  host.setAttribute('height', `${rect.height}px`)
-
-  document.body.appendChild(host)
+  return createWebElement(webElements.HIGHLIGHT, {
+    color: color,
+    radius: radius,
+    left: `${rect.left}px`,
+    top: `${rect.top}px`,
+    width: `${rect.width}px`,
+    height: `${rect.height}px`
+  })
 }

96ef7c9635a17b80c77d45fc5fce01fbf0811dbf
LuisMi <olmedoluis012@gmail.com>
2025-11-28 14:13:45 -0300

    feat: Import web-elements using helper

--- src/apps/content/index.js
+++ src/apps/content/index.js
@@ -1,12 +1,8 @@
+import { importWebComponents } from '@content/helpers/web-components.helpers'
+import processHandlers from '@content/process-handlers'
 import { setUpHandlers } from '@src/helpers/process.helpers'
-import processHandlers from './process-handlers'
 
 const contentHandler = setUpHandlers(processHandlers)
 
 chrome.runtime.onMessage.addListener(contentHandler)
-
-const script = document.createElement('script')
-script.src = chrome.runtime.getURL('assets/js/web-components.js')
-
-document.documentElement.appendChild(script)
-script.remove()
+importWebComponents()

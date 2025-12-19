da7ee20e72e0c30b1bd267b0abd433998880016c
LuisMi <olmedoluis012@gmail.com>
2025-12-19 13:34:41 -0300

    feat: Define scripts as modules

--- src/helpers/web-components.helpers.js
+++ src/helpers/web-components.helpers.js
@@ -3,6 +3,7 @@ import { webElements } from '@src/constants/web-elements.constants'
 export const importWebComponents = () => {
   const script = document.createElement('script')
   script.src = chrome.runtime.getURL('assets/js/web-components.js')
+  script.type = 'module'
 
   document.documentElement.appendChild(script)
   script.remove()

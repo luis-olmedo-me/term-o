4033c02164216cd1fc63ee2e137b68a82b71dd27
LuisMi <olmedoluis012@gmail.com>
2025-12-22 00:12:13 -0300

    feat: Use scss at ColoredText

--- src/apps/sidepanel/components/ColoredText/ColoredText.component.jsx
+++ src/apps/sidepanel/components/ColoredText/ColoredText.component.jsx
@@ -2,18 +2,23 @@ import * as React from 'preact'
 import { useMemo } from 'preact/hooks'
 
 import { convertStringToObjects } from './ColoredText.helpers'
-import * as S from './ColoredText.styles'
+import { text } from './ColoredText.module.scss'
 
 export const ColoredText = ({ value }) => {
   const sections = useMemo(() => convertStringToObjects(value), [value])
 
   return (
     <>
-      {sections.map(({ color, bgcolor, content }, index) => (
-        <S.Text key={index} aria-color={color} aria-bg-color={bgcolor}>
-          {content}
-        </S.Text>
-      ))}
+      {sections.map(({ color, bgcolor, content }, index) => {
+        const colorClass = `termo-color-${color ?? 'none'}`
+        const bgcolorClass = `termo-bgcolor-${bgcolor ?? 'none'}`
+
+        return (
+          <span className={`${text} ${bgcolorClass} ${colorClass}`} key={index}>
+            {content}
+          </span>
+        )
+      })}
     </>
   )
 }

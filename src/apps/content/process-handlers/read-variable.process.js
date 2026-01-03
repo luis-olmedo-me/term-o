9e9e365f7463e270e2183281f81a041b2e76012d
LuisMi <olmedoluis012@gmail.com>
2026-01-03 01:31:15 -0300

    feat: Create process listener for reading variables

---
+++ src/apps/content/process-handlers/read-variable.process.js
@@ -0,0 +1,50 @@
+export default async (resolve, reject, data) => {
+  window.addEventListener('message', event => {
+    if (event.source !== window) return
+    if (event.data?.source !== 'MY_EXTENSION') return
+
+    if (event.data.type === 'GLOBAL_VALUE') {
+      console.log('Valor obtenido:', event.data.value)
+
+      const json = event.data.value !== null ? JSON.stringify(event.data.value) : null
+
+      resolve(json)
+    }
+
+    if (event.data.type === 'GLOBAL_ERROR') {
+      reject(event.data.value)
+    }
+  })
+
+  const script = document.createElement('script')
+  const path = data.path
+
+  script.textContent = `
+    (function() {
+      try {
+        const path = "${path}".split(".");
+        let value = window;
+
+        for (const key of path) {
+          value = value?.[key];
+        }
+
+        window.postMessage({
+          source: "MY_EXTENSION",
+          type: "GLOBAL_VALUE",
+          path: "${path}",
+          value: value ?? null
+        }, "*");
+      } catch (e) {
+        window.postMessage({
+          source: "MY_EXTENSION",
+          type: "GLOBAL_ERROR",
+          error: e.message
+        }, "*");
+      }
+    })();
+  `
+
+  document.documentElement.appendChild(script)
+  script.remove()
+}

0924eb825e0fb1271f230466e564447093866ba0
LuisMi <olmedoluis012@gmail.com>
2026-01-15 23:10:09 -0300

    feat: Match bgcolor border radius

--- src/apps/sidepanel/components/ColoredText/ColoredText.component.jsx
+++ src/apps/sidepanel/components/ColoredText/ColoredText.component.jsx
@@ -1,22 +1,38 @@
 import { useMemo } from 'preact/hooks'
 
 import { getBackgroundedSections } from './ColoredText.helpers'
-import { text } from './ColoredText.module.scss'
+import { nextColored, previousColored, text, uniqueColored } from './ColoredText.module.scss'
+
+const getBgBorderMod = (sections, sectionIndex) => {
+  const nextSection = sections[sectionIndex + 1]
+  const previousSection = sections[sectionIndex - 1]
+
+  const isNextColored = !!nextSection && nextSection?.bgcolor !== 'reset'
+  const isPreviousColored = !!previousSection && previousSection?.bgcolor !== 'reset'
+
+  if (isNextColored && !isPreviousColored) return nextColored
+  if (isPreviousColored && !isNextColored) return previousColored
+  return uniqueColored
+}
 
 export const ColoredText = ({ value }) => {
   const sections = useMemo(() => getBackgroundedSections(value), [value])
 
   return (
     <>
-      {sections.map(({ bgcolor, content }, sectionIndex) => (
-        <span className={text} key={sectionIndex} data-bgcolor={bgcolor}>
-          {content.map(({ color, content }, index) => (
-            <span className={text} key={`${sectionIndex}-${index}`} data-color={color}>
-              {content}
-            </span>
-          ))}
-        </span>
-      ))}
+      {sections.map(({ bgcolor, content }, sectionIndex) => {
+        const sectionMod = getBgBorderMod(sections, sectionIndex)
+
+        return (
+          <span className={sectionMod} key={sectionIndex} data-bgcolor={bgcolor}>
+            {content.map(({ color, content }, index) => (
+              <span className={text} key={`${sectionIndex}-${index}`} data-color={color}>
+                {content}
+              </span>
+            ))}
+          </span>
+        )
+      })}
     </>
   )
 }

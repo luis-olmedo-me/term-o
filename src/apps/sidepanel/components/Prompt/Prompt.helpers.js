9491c740d0536ed3ffef6e10928a5ef734327f97
LuisMi <olmedoluis012@gmail.com>
2026-02-05 00:38:56 -0300

    feat: Expect for amps command suggestions

--- src/apps/sidepanel/components/Prompt/Prompt.helpers.js
+++ src/apps/sidepanel/components/Prompt/Prompt.helpers.js
@@ -5,27 +5,28 @@ export const createSuggestion = (value, caret) => {
   const start = caret !== null ? value.slice(0, caret) : ''
   const end = caret !== null ? value.slice(caret) : ''
 
-  const lastFragmentStart = start.split('&&').at(-1) ?? ''
+  const lastFragmentStart = (start.split('&&').at(-1) ?? '').trimLeft()
   const lastFragmentEnd = end.split('&&').at(0) ?? ''
   const argsStart = getArgs(lastFragmentStart)
   const argsEnd = getArgs(lastFragmentEnd)
 
   if (argsStart.length === 0) return ''
   if (argsStart.length === 1) {
+    const lastArgStart = argsStart.at(-1) ?? ''
     const firstArgEnd = argsEnd.at(0) ?? ''
-    const match = Object.values(commandNames).find(name => {
-      if (!name.startsWith(start)) return false
-      if (!firstArgEnd) return true
 
-      const nameEnd = name.slice(start.length)
+    const match = Object.values(commandNames).find(name => {
+      if (!name.startsWith(lastArgStart)) return false
+      if (!firstArgEnd) return true
+      const nameEnd = name.slice(lastArgStart.length)
 
       return nameEnd.endsWith(firstArgEnd)
     })
 
     if (!match) return ''
     return firstArgEnd
-      ? match.slice(start.length, firstArgEnd.length * -1)
-      : match.slice(start.length)
+      ? match.slice(lastArgStart.length, firstArgEnd.length * -1)
+      : match.slice(lastArgStart.length)
   }
 
   return ''

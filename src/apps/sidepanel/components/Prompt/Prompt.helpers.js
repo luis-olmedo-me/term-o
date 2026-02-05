ad7c239dad54785b3741051dc7a68ca554684739
LuisMi <olmedoluis012@gmail.com>
2026-02-05 10:40:21 -0300

    feat: Add suggestions by options

--- src/apps/sidepanel/components/Prompt/Prompt.helpers.js
+++ src/apps/sidepanel/components/Prompt/Prompt.helpers.js
@@ -1,9 +1,11 @@
 import { commandNames } from '@src/constants/command.constants'
 import { getArgs } from '@src/helpers/arguments.helpers'
+import commandParser from '@src/libs/command-parser'
 
 export const createSuggestion = (value, caret, aliases) => {
   const aliasNames = aliases.map(alias => alias.key)
-  const names = [...aliasNames, ...Object.values(commandNames)]
+  const commandValueNames = Object.values(commandNames)
+  const names = [...aliasNames, ...commandValueNames]
 
   const start = caret !== null ? value.slice(0, caret) : ''
   const end = caret !== null ? value.slice(caret) : ''
@@ -14,11 +16,10 @@ export const createSuggestion = (value, caret, aliases) => {
   if (!lastFragmentStart && !lastFragmentEnd) return ''
   const argsStart = getArgs(lastFragmentStart)
   const argsEnd = getArgs(lastFragmentEnd)
+  const lastArgStart = argsStart.at(-1) ?? ''
+  const firstArgEnd = argsEnd.at(0) ?? ''
 
   if (argsStart.length <= 1) {
-    const lastArgStart = argsStart.at(-1) ?? ''
-    const firstArgEnd = argsEnd.at(0) ?? ''
-
     const match = names.find(name => {
       if (!name.startsWith(lastArgStart)) return false
       if (!firstArgEnd) return true
@@ -32,6 +33,26 @@ export const createSuggestion = (value, caret, aliases) => {
       ? match.slice(lastArgStart.length, firstArgEnd.length * -1)
       : match.slice(lastArgStart.length)
   }
+  const firstArgStart = argsStart.at(0) ?? ''
+  const isCommand = commandValueNames.some(name => name === firstArgStart)
 
-  return ''
+  if (!isCommand) return ''
+  const command = commandParser.bases.find(base => base.name === firstArgStart)
+
+  const options = command.options.values.map(option => `--${option.name}`)
+  const abbreviations = command.options.values.map(option => `-${option.abbreviation}`)
+  const optionNames = [...abbreviations, ...options]
+
+  const match = optionNames.find(option => {
+    if (!option.startsWith(lastArgStart)) return false
+    if (!firstArgEnd) return true
+    const optionEnd = option.slice(lastArgStart.length)
+
+    return optionEnd.endsWith(firstArgEnd)
+  })
+
+  if (!match) return ''
+  return firstArgEnd
+    ? match.slice(lastArgStart.length, firstArgEnd.length * -1)
+    : match.slice(lastArgStart.length)
 }

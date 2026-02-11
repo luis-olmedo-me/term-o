62e28c1b149e0799eda9179657371e5c98616e8b
LuisMi <olmedoluis012@gmail.com>
2026-02-11 10:21:45 -0300

    feat: Remove the use of a notification to upload a file

--- src/apps/sidepanel/process-handlers/upload-file.process.js
+++ src/apps/sidepanel/process-handlers/upload-file.process.js
@@ -1,31 +1,21 @@
-import { durations } from '@src/constants/web-elements.constants'
 import { readFileContent, uploader } from '@src/helpers/file.helpers'
-import { createNotification } from '@src/helpers/web-components.helpers'
 
 export default async (resolve, reject, data) => {
-  const notification = createNotification({
-    title: 'Term-O | Action required to proceed',
-    message: 'Click to start uploading a file',
-    theme: data.theme,
-    duration: durations.EXTENDED
-  })
+  try {
+    const input = uploader({
+      extensions: data.extensions,
+      onError: reject,
+      onUpload: async file =>
+        resolve({
+          name: file.name,
+          content: await readFileContent(file),
+          lastVisitTime: file.lastModifiedDate.toISOString(),
+          size: `${file.size}B`
+        })
+    })
 
-  const input = uploader({
-    extensions: data.extensions,
-    onError: reject,
-    onUpload: async file =>
-      resolve({
-        name: file.name,
-        content: await readFileContent(file),
-        lastVisitTime: file.lastModifiedDate.toISOString(),
-        size: `${file.size}B`
-      })
-  })
-
-  const openFileDialog = () => {
     input.open()
+  } catch (error) {
+    reject('Unexpected error when trying to upload a file.')
   }
-
-  notification.addEventListener('click', openFileDialog)
-  notification.addEventListener('timeout', event => reject(event.detail))
 }

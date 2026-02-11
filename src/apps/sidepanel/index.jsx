1c65232da0e4b0a4f372dfe85e6914e18c484b12
LuisMi <olmedoluis012@gmail.com>
2026-02-11 15:52:00 -0300

    feat: Isolate sidepanel helpers

--- src/apps/sidepanel/index.jsx
+++ src/apps/sidepanel/index.jsx
@@ -1,19 +1,20 @@
 import { render } from 'preact'
 
 import Terminal from '@sidepanel/components/Terminal'
+import processHandlers from '@sidepanel/process-handlers'
 import commandParser from '@src/libs/command-parser'
 import StorageProvider from '@src/providers/StorageProvider'
 import ThemeProvider from '@src/providers/ThemeProvider'
-import './index.scss'
 
+import { connectToBackground } from '@sidepanel/helpers/conection.helpers'
 import { origins } from '@src/constants/command.constants'
 import { setUpHandlers } from '@src/helpers/process.helpers'
-import processHandlers from './process-handlers'
+import './index.scss'
 
 const sidepanelHandler = setUpHandlers(processHandlers)
 
+connectToBackground()
 commandParser.setOrigin(origins.MANUAL)
-chrome.runtime.connect({ name: 'sidepanel' })
 chrome.runtime.onMessage.addListener(sidepanelHandler)
 
 render(

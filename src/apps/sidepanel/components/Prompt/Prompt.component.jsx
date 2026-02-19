87aa61a16c27f45206db4c80c5e0b95c0784f8f3
LuisMi <olmedoluis012@gmail.com>
2026-02-19 15:03:16 -0300

    refactor: Compact padding in prompt

--- src/apps/sidepanel/components/Prompt/Prompt.component.jsx
+++ src/apps/sidepanel/components/Prompt/Prompt.component.jsx
@@ -41,7 +41,6 @@ export const Prompt = ({
   const [config] = useStorage({ key: storageKeys.CONFIG })
 
   const historialSize = config.getValueById(configInputIds.HISTORIAL_SIZE)
-  const statusIndicator = config.getValueById(configInputIds.STATUS_INDICATOR)
   const isTruncated = config.getValueById(configInputIds.LINE_TRUNCATION)
 
   const calculateSuggestion = useCallback((value, caret, aliases, addons) => {
@@ -187,11 +186,7 @@ export const Prompt = ({
   const end = caret !== null ? value.slice(caret) : ''
 
   return (
-    <div
-      data-loading={loading && !isRequesting}
-      data-indicator={statusIndicator}
-      className={promptWrapper}
-    >
+    <div data-loading={loading && !isRequesting} className={promptWrapper}>
       {contextLines.map((contextLine, index) => (
         <p key={`${contextLine}-${index}`} className={promptLine} data-truncated={isTruncated}>
           <ColoredText value={contextLine} />

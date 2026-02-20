49bd9cc8543e59417993ad9cb39b0f3cd3687012
LuisMi <olmedoluis012@gmail.com>
2026-02-20 19:03:49 -0300

    feat: Adjust styles on text area

--- src/components/TextArea/TextArea.component.jsx
+++ src/components/TextArea/TextArea.component.jsx
@@ -1,42 +1,12 @@
-import { useCallback, useEffect, useRef, useState } from 'preact/hooks'
+import { useRef, useState } from 'preact/hooks'
 
-import {
-  getBackgroundedSections,
-  getBgBorderMod
-} from '@src/apps/sidepanel/components/ColoredText/ColoredText.helpers'
-import { debounce } from '@src/helpers/utils.helpers'
-import { text, textAreaInput, textAreaOverlay, textAreaWrapper } from './TextArea.module.scss'
+import { textAreaInput, textAreaOverlay, textAreaWrapper } from './TextArea.module.scss'
 
 export const TextArea = ({ onBlur, value, name, maxLines }) => {
   const overlayRef = useRef(null)
   const textAreaRef = useRef(null)
 
-  const [sections, setSections] = useState([])
   const [localValue, setLocalValue] = useState(value)
-
-  const calculateSections = useCallback(value => {
-    const newSections = getBackgroundedSections(value)
-
-    setSections(newSections)
-  }, [])
-  const debouncedcalculateSections = useCallback(debounce(calculateSections, 200), [
-    calculateSections
-  ])
-
-  useEffect(
-    function changeSections() {
-      let debounceTimeoutId = null
-
-      const calculate = async () => {
-        debounceTimeoutId = debouncedcalculateSections(localValue)
-      }
-
-      calculate()
-
-      return () => clearTimeout(debounceTimeoutId)
-    },
-    [localValue]
-  )
 
   const syncScroll = () => {
     overlayRef.current.scrollLeft = textAreaRef.current.scrollLeft
@@ -53,19 +23,7 @@ export const TextArea = ({ onBlur, value, name, maxLines }) => {
   return (
     <div className={textAreaWrapper}>
       <div ref={overlayRef} className={textAreaOverlay}>
-        {sections.map(({ bgcolor, content }, sectionIndex) => {
-          const sectionMod = getBgBorderMod(sections, sectionIndex)
-
-          return (
-            <span className={sectionMod} key={sectionIndex} data-bgcolor={bgcolor}>
-              {content.map(({ color, content }, index) => (
-                <span className={text} key={`${sectionIndex}-${index}`} data-color={color}>
-                  {content}
-                </span>
-              ))}
-            </span>
-          )
-        })}
+        <span>{localValue}</span>
       </div>
 
       <textarea

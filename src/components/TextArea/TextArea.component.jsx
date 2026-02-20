9c384f7ebd3c68b624b72aae5728315da3871101
LuisMi <olmedoluis012@gmail.com>
2026-02-20 14:04:38 -0300

    refactor: Display overlay over text area

--- src/components/TextArea/TextArea.component.jsx
+++ src/components/TextArea/TextArea.component.jsx
@@ -1,16 +1,71 @@
-import { textAreaInput } from './TextArea.module.scss'
+import { useCallback, useEffect, useRef, useState } from 'preact/hooks'
+
+import { debounce } from '@src/helpers/utils.helpers'
+import { textAreaInput, textAreaOverlay, textAreaWrapper } from './TextArea.module.scss'
+
+const createColorPositions = value => {
+  console.log('💬 ~ value:', value)
+  return []
+}
 
 export const TextArea = ({ onChange, onBlur, value, name, maxLines }) => {
+  const overlayRef = useRef(null)
+  const textAreaRef = useRef(null)
+
+  const [colorPositions, setColorPositions] = useState([])
+
+  const calculateColorPositions = useCallback(value => {
+    const newSuggestion = createColorPositions(value)
+
+    setColorPositions(newSuggestion)
+  }, [])
+  const debouncedcalculateColorPositions = useCallback(debounce(calculateColorPositions, 200), [
+    calculateColorPositions
+  ])
+
+  useEffect(
+    function changeColorPositions() {
+      let debounceTimeoutId = null
+
+      const calculate = async () => {
+        debounceTimeoutId = debouncedcalculateColorPositions(value)
+      }
+
+      calculate()
+
+      return () => clearTimeout(debounceTimeoutId)
+    },
+    [value]
+  )
+
+  const syncScroll = () => {
+    overlayRef.current.scrollLeft = textAreaRef.current.scrollLeft
+  }
+
+  const handleBlur = event => {
+    console.log('💬 ~ colorPositions:', colorPositions)
+    onBlur(event)
+  }
+
   return (
-    <textarea
-      className={textAreaInput}
-      name={name}
-      onBlur={onBlur}
-      onInput={onChange}
-      rows={maxLines}
-      value={value}
-      spellCheck="false"
-    />
+    <div className={textAreaWrapper}>
+      <div ref={overlayRef} className={textAreaOverlay}>
+        <span>{value}</span>
+      </div>
+
+      <textarea
+        ref={textAreaRef}
+        className={textAreaInput}
+        rows={maxLines}
+        name={name}
+        value={value}
+        onBlur={handleBlur}
+        onInput={onChange}
+        onKeyUp={syncScroll}
+        onScroll={syncScroll}
+        spellCheck="false"
+      />
+    </div>
   )
 }
 

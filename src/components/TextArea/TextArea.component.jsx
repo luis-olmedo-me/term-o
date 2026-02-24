c5775ac9353aea800e06870799fd42277441a144
LuisMi <olmedoluis012@gmail.com>
2026-02-24 17:14:43 -0300

    feat: Create parser for runtime coloring

--- src/components/TextArea/TextArea.component.jsx
+++ src/components/TextArea/TextArea.component.jsx
@@ -2,9 +2,47 @@ import { useRef } from 'preact/hooks'
 
 import { textAreaInput, textAreaOverlay, textAreaWrapper } from './TextArea.module.scss'
 
+const colorPattern = /\[termo\.(color|bgcolor)\.([A-Za-z]+)\]/g
+
+const parser = value => {
+  const matches = value.matchAll(colorPattern)?.toArray() || []
+  let results = []
+  let lastColor = 'reset'
+  let lastBGColor = 'reset'
+
+  for (const match of matches) {
+    const matchValue = match.at(0)
+    const category = match.at(1)
+    const color = match.at(2)
+
+    const isColorKey = category === 'color'
+
+    if (isColorKey) lastColor = color
+    else lastBGColor = color
+
+    results.push({
+      value: matchValue,
+      color: 'brightBlack',
+      bgcolor: 'white'
+    })
+
+    results.push({
+      value: 'testing the content',
+      color: lastColor,
+      bgcolor: lastBGColor
+    })
+  }
+
+  console.log('💬 ~ results:', results)
+
+  return 'test'
+}
+
 export const TextArea = ({ onBlur, value, name, maxLines, onChange }) => {
   const overlayRef = useRef(null)
   const textAreaRef = useRef(null)
+
+  parser(value)
 
   const syncScroll = () => {
     overlayRef.current.scrollLeft = textAreaRef.current.scrollLeft

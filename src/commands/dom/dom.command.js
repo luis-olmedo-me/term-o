20c983267380857bbb18e4b383d5e704d90977e3
LuisMi <olmedoluis012@gmail.com>
2026-03-17 11:46:58 -0300

    feat: Add command option for html injection

--- src/commands/dom/dom.command.js
+++ src/commands/dom/dom.command.js
@@ -33,8 +33,16 @@ export default new CommandBase({
     type: commandTypes.STRING,
     helpSection: domHelpSections.SEARCH,
     description: 'Find elements with an XPath query',
-    worksWith: ['tab-id', 'sibling', 'parent', 'child', 'xpath', 'below'],
+    worksWith: ['tab-id', 'sibling', 'parent', 'child', 'xpath', 'below', 'inject'],
     validate: [isXpath]
+  })
+  .expect({
+    name: 'inject',
+    abbreviation: 'I',
+    type: commandTypes.STRING,
+    helpSection: domHelpSections.ACTIONS_AND_UTILITIES,
+    description: 'Inject HTML as text',
+    validate: []
   })
   .expect({
     name: 'sibling',

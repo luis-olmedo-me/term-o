ec1585c6c51d5289bda7f28408058e7c45237e56
LuisMi <olmedoluis012@gmail.com>
2026-04-03 01:40:56 -0300

    refactor: Remove usecase of ColoredText webcomponent

--- src/apps/web-components/components/ElementPicker/index.js
+++ src/apps/web-components/components/ElementPicker/index.js
@@ -2,11 +2,11 @@ import WebElement from '@web-components/templates/WebElement'
 import elementPickerCss from './ElementPicker.raw.css?raw'
 import elementPickerHtml from './ElementPicker.raw.html?raw'
 
-import { embedWebElements, webElements } from '@src/constants/web-elements.constants'
+import { getPaintedFragments } from '@src/components/ColoredText/ColoredText.helpers'
+import { webElements } from '@src/constants/web-elements.constants'
 import { stringifyFragments } from '@src/helpers/command.helpers'
 import { convertElementToJSON } from '@src/helpers/converter.helpers'
 import { formatElement } from '@src/helpers/format.helpers'
-import { createWebElement } from '@src/helpers/web-components.helpers'
 
 class ElementPicker extends WebElement {
   constructor() {
@@ -37,17 +37,34 @@ class ElementPicker extends WebElement {
       const elementAsLog = formatElement({ ...elementAsJSON, textContent: null, xpath: null })
       const elementAsTextLog = stringifyFragments(elementAsLog)
 
+      const coloredTextElement = this._createPaintedElement(elementAsTextLog)
+
       const textElement = document.createElement('li')
       textElement.addEventListener('click', () => thisRef.$dispatch('pickedup', elementAsJSON))
       textElement.setAttribute('class', 'list-option')
       textElement.setAttribute('role', 'option')
 
-      const coloredTextElement = createWebElement(embedWebElements.COLORED_TEXT, {}, textElement)
-      coloredTextElement.$dispatch('textchange', elementAsTextLog)
-
       textElement.append(coloredTextElement)
       listElement.append(textElement)
     })
+  }
+
+  _createPaintedElement(text) {
+    const lineElement = document.createElement('p')
+    const fragments = getPaintedFragments(text)
+
+    lineElement.setAttribute('class', 'line')
+    for (const fragment of fragments) {
+      const spanElement = document.createElement('span')
+      spanElement.setAttribute('data-bgcolor', fragment.bgcolor)
+      spanElement.setAttribute('data-color', fragment.color)
+      spanElement.setAttribute('class', 'colored')
+
+      spanElement.innerText = fragment.value
+      lineElement.append(spanElement)
+    }
+
+    return lineElement
   }
 }
 

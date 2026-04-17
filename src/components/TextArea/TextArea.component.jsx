b186edf3198adffb19aac2465d5d0f9a276891fe
LuisMi <olmedoluis012@gmail.com>
2026-04-16 22:43:13 -0300

    refactor: Show keywords when textarea is focus

--- src/components/TextArea/TextArea.component.jsx
+++ src/components/TextArea/TextArea.component.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'preact/hooks'
+import { useRef, useState } from 'preact/hooks'
 
 import ColoredText from '@src/components/ColoredText'
 
@@ -13,9 +13,16 @@ export const TextArea = ({ onBlur, value, name, maxLines, onChange }) => {
   const overlayRef = useRef(null)
   const textAreaRef = useRef(null)
 
+  const [showKeywords, setShowKeywords] = useState(false)
+
   const syncScroll = () => {
     overlayRef.current.scrollLeft = textAreaRef.current.scrollLeft
     overlayRef.current.scrollTop = textAreaRef.current.scrollTop
+  }
+
+  const handleBlur = event => {
+    setShowKeywords(false)
+    onBlur(event)
   }
 
   return (
@@ -26,10 +33,11 @@ export const TextArea = ({ onBlur, value, name, maxLines, onChange }) => {
         rows={maxLines}
         name={name}
         value={value}
-        onBlur={onBlur}
         onInput={onChange}
         onKeyDown={syncScroll}
         onScroll={syncScroll}
+        onBlur={handleBlur}
+        onFocus={() => setShowKeywords(true)}
         spellCheck="false"
       />
 
@@ -37,7 +45,7 @@ export const TextArea = ({ onBlur, value, name, maxLines, onChange }) => {
         <ColoredText
           fragmentClassName={textarea__colored_fragment}
           value={value}
-          keywordsEnabled
+          keywordsEnabled={showKeywords}
           hideArtificialSpaces
         />
       </div>

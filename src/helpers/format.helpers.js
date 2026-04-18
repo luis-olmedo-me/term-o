17697dd5dde1208dc583be512d6352b6f39b779a
LuisMi <olmedoluis012@gmail.com>
2026-04-18 11:12:25 -0300

    refactor: Update format placements

--- src/helpers/format.helpers.js
+++ src/helpers/format.helpers.js
@@ -123,14 +123,14 @@ export const formatAddon = ({ name, version }) => {
 }
 
 export const formatRegisteredEvent = ({ type, url, line, id }) => {
-  const quotedType = getQuotedString(type)
   const quotedId = getQuotedString(id)
+  const quotedType = getQuotedString(type)
   const quotedURL = getQuotedString(url)
   const quotedLine = getQuotedString(line)
 
   return [
-    `${C`green`}${quotedType}${C`reset`}`,
     `${C`purple`}${quotedId}${C`reset`}`,
+    `${C`green`}${quotedType}${C`reset`}`,
     `${C`yellow`}${quotedURL}${C`reset`}`,
     `${C`yellow`}${quotedLine}${C`reset`}`
   ]

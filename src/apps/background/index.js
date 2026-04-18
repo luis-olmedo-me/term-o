b44a5becac92093d971cba2fe719b6ccbce57848
LuisMi <olmedoluis012@gmail.com>
2026-04-18 16:21:17 -0300

    feat: Always display a title

--- src/apps/background/index.js
+++ src/apps/background/index.js
@@ -97,9 +97,10 @@ chrome.tabs.onActivated.addListener(async activeInfo => {
 
   if (!switchTabAutomatically) return
   const tab = await getTab({ tabId: activeInfo.tabId })
-  const internalTab = createInternalTab(tab)
+  const url = tab.url || tab.pendingUrl
+  const formattedTab = tab.title ? tab : { ...tab, title: url.hostname }
 
-  storage.set(storageKeys.TAB, internalTab)
+  storage.set(storageKeys.TAB, formattedTab)
 })
 
 chrome.runtime.onInstalled.addListener(async () => {

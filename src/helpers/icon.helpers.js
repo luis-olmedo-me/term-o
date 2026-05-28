4c45dabf7aeb765271224ebd5dab89bae044345b
LuisMi <olmedoluis012@gmail.com>
2026-05-27 21:30:58 -0300

    feat: Use a helper to define svg content

--- src/helpers/icon.helpers.js
+++ src/helpers/icon.helpers.js
@@ -1,10 +1,50 @@
 import { themeModes, themeVariants } from '@src/constants/themes.constants'
 
-export const getStrokeOpacityByTheme = ({ theme }) => {
+export const getColorsByTheme = ({ theme }) => {
   const isDarkMode = theme.mode === themeModes.DARK
   const isFlatVariant = theme.variant === themeVariants.FLAT
+  const isFlatDark = isDarkMode && isFlatVariant
 
-  if (isFlatVariant && isDarkMode) return 0
-  if (isFlatVariant && !isDarkMode) return 0.1
-  return 0.5
+  if (isFlatVariant && isDarkMode) {
+    return {
+      primary: {
+        fill: isFlatDark ? theme.colors.accent : `rgb(from ${theme.colors.accent} r g b / 0.7)`
+      },
+      secondary: {
+        fill: isFlatDark ? theme.colors.white : `rgb(from ${theme.colors.white} r g b / 0.7)`
+      }
+    }
+  }
+
+  if (isFlatVariant && !isDarkMode) {
+    return {
+      primary: {
+        fill: isFlatDark ? theme.colors.accent : `rgb(from ${theme.colors.accent} r g b / 0.7)`,
+        stroke: isDarkMode ? theme.colors.accent : theme.colors.brightBlack,
+        strokeOpacity: 0.1,
+        strokeWidth: '1px'
+      },
+      secondary: {
+        fill: isFlatDark ? theme.colors.white : `rgb(from ${theme.colors.white} r g b / 0.7)`,
+        stroke: theme.colors.brightBlack,
+        strokeOpacity: 0.1,
+        strokeWidth: '1px'
+      }
+    }
+  }
+
+  return {
+    primary: {
+      fill: isFlatDark ? theme.colors.accent : `rgb(from ${theme.colors.accent} r g b / 0.7)`,
+      stroke: isDarkMode ? theme.colors.accent : theme.colors.brightBlack,
+      strokeOpacity: 0.5,
+      strokeWidth: '1px'
+    },
+    secondary: {
+      fill: isFlatDark ? theme.colors.white : `rgb(from ${theme.colors.white} r g b / 0.7)`,
+      stroke: theme.colors.brightBlack,
+      strokeOpacity: 0.5,
+      strokeWidth: '1px'
+    }
+  }
 }
--- src/icons/Logo.icon.jsx
+++ src/icons/Logo.icon.jsx
@@ -2,18 +2,13 @@ import useStorage from '@src/hooks/useStorage'
 
 import { iconPropType } from '@src/constants/icon.constants'
 import { storageKeys } from '@src/constants/storage.constants'
-import { themeModes, themeVariants } from '@src/constants/themes.constants'
-import { getStrokeOpacityByTheme } from '@src/helpers/icon.helpers'
+import { getColorsByTheme } from '@src/helpers/icon.helpers'
 
 const Logo = ({ size, className }) => {
   const [config] = useStorage({ key: storageKeys.CONFIG })
 
   const theme = config.theme
-  const isDarkMode = theme.mode === themeModes.DARK
-  const isFlatVariant = theme.variant === themeVariants.FLAT
-  const isFlatDark = isDarkMode && isFlatVariant
-
-  const strokeOpacityByTheme = getStrokeOpacityByTheme({ theme })
+  const colors = getColorsByTheme({ theme })
 
   return (
     <svg
@@ -25,10 +20,10 @@ const Logo = ({ size, className }) => {
     >
       <path
         d="M18.284 10.406a1.99 1.99 0 0 1 0 2.825l-7.232 7.346a1.95 1.95 0 0 1-2.782 0l-.834-.848a1.99 1.99 0 0 1 0-2.825l5.006-5.085-.278-.283a2.36 2.36 0 0 1 0-3.39l.278-.282a2.33 2.33 0 0 1 3.338 0z"
-        fill={isFlatDark ? theme.colors.accent : `rgb(from ${theme.colors.accent} r g b / 0.7)`}
-        stroke={isDarkMode ? theme.colors.accent : theme.colors.brightBlack}
-        stroke-opacity={strokeOpacityByTheme}
-        stroke-width="1px"
+        fill={colors.primary.fill}
+        stroke={colors.primary.stroke}
+        stroke-opacity={colors.primary.strokeOpacity}
+        stroke-width={colors.primary.strokeWidth}
       />
 
       <rect
@@ -38,10 +33,10 @@ const Logo = ({ size, className }) => {
         y="5.7"
         rx="2.5"
         transform="rotate(-45 6.12 5.7)"
-        fill={isFlatDark ? theme.colors.white : `rgb(from ${theme.colors.white} r g b / 0.7)`}
-        stroke={theme.colors.brightBlack}
-        stroke-opacity={strokeOpacityByTheme}
-        stroke-width="1px"
+        fill={colors.secondary.fill}
+        stroke={colors.secondary.stroke}
+        stroke-opacity={colors.secondary.strokeOpacity}
+        stroke-width={colors.secondary.strokeWidth}
       />
     </svg>
   )

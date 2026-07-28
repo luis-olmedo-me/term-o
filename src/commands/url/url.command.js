48555a6fda4e5be3eb8a1e292c62d7652234ce89
LuisMi <olmedoluis012@gmail.com>
2026-07-27 22:18:06 -0300

    feat: Expect to show current URL

--- src/commands/url/url.command.js
+++ src/commands/url/url.command.js
@@ -1,9 +1,26 @@
 import CommandBase from '@src/templates/CommandBase'
 
-import { commandNames } from '@src/constants/command.constants'
+import { commandNames, commandTypes, helpSections } from '@src/constants/command.constants'
+import { options, value } from '@src/helpers/validation-command.helpers'
 import { urlHandler } from './url.handler'
 
 export default new CommandBase({
   name: commandNames.URL,
   handler: urlHandler
 })
+  .expect({
+    name: 'current',
+    abbreviation: 'C',
+    type: commandTypes.BOOLEAN,
+    helpSection: helpSections.ACTIONS,
+    description: 'Show the current URL',
+    validate: [options.allow('tab-id')]
+  })
+  .expect({
+    name: 'tab-id',
+    abbreviation: 'i',
+    type: commandTypes.STRING,
+    helpSection: helpSections.DETAILS,
+    description: 'Define a Tab ID where apply an action',
+    validate: [value.isTabId, options.requireAnyOf('current')]
+  })
--- src/commands/url/url.handler.js
+++ src/commands/url/url.handler.js
@@ -1,1 +1,25 @@
-export const urlHandler = async command => {}
+import { getTab } from '@src/browser-api/tabs.api'
+import { storageKeys } from '@src/constants/storage.constants'
+import { createHelpView } from '@src/helpers/command.helpers'
+import { cleanTabId } from '@src/helpers/tabs.helpers'
+
+export const urlHandler = async command => {
+  const storage = command.get('storage')
+  const P = name => command.props[name]
+
+  let tabId = storage.get(storageKeys.TAB).id
+  console.log('o-test', tabId)
+
+  if (P`tab-id`) {
+    command.log(['"Connecting to the tab."'])
+    const validTab = await getTab({ tabId: cleanTabId(P`tab-id`) })
+
+    tabId = validTab.id
+  }
+
+  if (P`current`) {
+    console.log('test', tabId)
+  }
+
+  if (P`help`) createHelpView(command)
+}
